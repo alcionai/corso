2db63ad1efb6c57f6945418857ea2088d28e14a8
ryanfkeepers <ryanfkeepers@gmail.com>
2022-05-09 13:08:24 -0600

    add coba and cli to corso
    Adds the spf13/cobra cli handling package.  Main() now
    processes cli commands defined by the /cli package.

---
+++ src/cli/cli.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// The root-level command.
+// `corso <command> [<subcommand>] [<service>] [<flag>...]`
+var corsoCmd = &cobra.Command{
+	Use:   "corso",
+	Short: "Protect your Microsoft 365 data.",
+	Long:  `Reliable, secure, and efficient data protection for Microsoft 365.`,
+	Run:   handleCorsoCmd,
+}
+
+// the root-level flags
+var (
+	version bool
+)
+
+// Handler for flat calls to `corso`.
+// Produces the same output as `corso --help`.
+func handleCorsoCmd(cmd *cobra.Command, args []string) {
+	if version {
+		fmt.Printf("Corso\nversion:\tpre-alpha\n")
+	} else {
+		cmd.Help()
+	}
+}
+
+// Handle builds and executes the cli processor.
+func Handle() {
+	corsoCmd.Flags().BoolP("version", "v", version, "current version info")
+	if err := corsoCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
