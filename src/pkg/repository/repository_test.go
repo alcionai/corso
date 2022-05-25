548eb1be72684d09e87e0884ef5fd8881a2152b8
Keepers <104464746+ryanfkeepers@users.noreply.github.com>
2022-05-25 14:06:05 -0600

    add e2e wiring of cli to kopia (#77)
    * add e2e wiring of cli to kopia
    
    Now that pkg/storage and internal/kopia are in place, we
    can wire up the init flow from the cli all the way to kopia.  Testing
    harness for this functionality still needs investigation afterward.
    
    * factor out awsVars struct for s3Cfg

--- src/pkg/repository/repository_test.go
+++ src/pkg/repository/repository_test.go
@@ -2,37 +2,55 @@ package repository_test
 
 import (
 	"context"
+	"strings"
 	"testing"
 
-	"github.com/kopia/kopia/repo/blob"
-
 	"github.com/alcionai/corso/pkg/repository"
+	"github.com/alcionai/corso/pkg/storage"
 )
 
-type testConfig struct{}
-
-func (tc testConfig) KopiaStorage(ctx context.Context, create bool) (blob.Storage, error) {
-	return nil, nil
-}
-
 func TestInitialize(t *testing.T) {
-	_, err := repository.Initialize(
-		context.Background(),
-		repository.ProviderUnknown,
-		repository.Account{},
-		testConfig{})
-	if err != nil {
-		t.Fatalf("didn't expect initialize to error, got [%v]", err)
+	table := []struct {
+		storage     storage.Storage
+		account     repository.Account
+		expectedErr string
+	}{
+		{
+			storage.NewStorage(storage.ProviderUnknown),
+			repository.Account{},
+			"provider details are required",
+		},
+	}
+	for _, test := range table {
+		t.Run(test.expectedErr, func(t *testing.T) {
+			_, err := repository.Initialize(context.Background(), test.account, test.storage)
+			if err == nil || !strings.Contains(err.Error(), test.expectedErr) {
+				t.Fatalf("expected error with [%s], got [%v]", test.expectedErr, err)
+			}
+		})
 	}
 }
 
+// repository.Connect involves end-to-end communication with kopia, therefore this only
+// tests expected error cases from
 func TestConnect(t *testing.T) {
-	_, err := repository.Connect(
-		context.Background(),
-		repository.ProviderUnknown,
-		repository.Account{},
-		testConfig{})
-	if err != nil {
-		t.Fatalf("didn't expect connect to error, got [%v]", err)
+	table := []struct {
+		storage     storage.Storage
+		account     repository.Account
+		expectedErr string
+	}{
+		{
+			storage.NewStorage(storage.ProviderUnknown),
+			repository.Account{},
+			"provider details are required",
+		},
+	}
+	for _, test := range table {
+		t.Run(test.expectedErr, func(t *testing.T) {
+			_, err := repository.Connect(context.Background(), test.account, test.storage)
+			if err == nil || !strings.Contains(err.Error(), test.expectedErr) {
+				t.Fatalf("expected error with [%s], got [%v]", test.expectedErr, err)
+			}
+		})
 	}
 }
