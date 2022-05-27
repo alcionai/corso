eaf6018996d2ca373d513c594631f7ef739074ae
Keepers <104464746+ryanfkeepers@users.noreply.github.com>
2022-05-27 17:22:50 -0600

    add prefix flag for s3 (#94)
    * add prefix flag for s3
    
    Adds the flag --prefix for s3 repo init and creation.
    
    * fix missing prefix flag in repo s3 cli

--- src/pkg/storage/s3.go
+++ src/pkg/storage/s3.go
@@ -4,6 +4,7 @@ type S3Config struct {
 	AccessKey    string
 	Bucket       string
 	Endpoint     string
+	Prefix       string
 	SecretKey    string
 	SessionToken string
 }
@@ -12,6 +13,7 @@ const (
 	keyS3AccessKey    = "s3_accessKey"
 	keyS3Bucket       = "s3_bucket"
 	keyS3Endpoint     = "s3_endpoint"
+	keyS3Prefix       = "s3_prefix"
 	keyS3SecretKey    = "s3_secretKey"
 	keyS3SessionToken = "s3_sessionToken"
 )
@@ -21,6 +23,7 @@ func (c S3Config) Config() config {
 		keyS3AccessKey:    c.AccessKey,
 		keyS3Bucket:       c.Bucket,
 		keyS3Endpoint:     c.Endpoint,
+		keyS3Prefix:       c.Prefix,
 		keyS3SecretKey:    c.SecretKey,
 		keyS3SessionToken: c.SessionToken,
 	}
@@ -33,6 +36,7 @@ func (s Storage) S3Config() S3Config {
 		c.AccessKey = s.Config[keyS3AccessKey].(string)
 		c.Bucket = s.Config[keyS3Bucket].(string)
 		c.Endpoint = s.Config[keyS3Endpoint].(string)
+		c.Prefix = s.Config[keyS3Prefix].(string)
 		c.SecretKey = s.Config[keyS3SecretKey].(string)
 		c.SessionToken = s.Config[keyS3SessionToken].(string)
 	}
--- src/pkg/storage/s3_test.go
+++ src/pkg/storage/s3_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestS3Config_Config(t *testing.T) {
-	s3 := storage.S3Config{"ak", "bkt", "end", "sk", "tkn"}
+	s3 := storage.S3Config{"ak", "bkt", "end", "pre", "sk", "tkn"}
 	c := s3.Config()
 	table := []struct {
 		key    string
@@ -16,6 +16,7 @@ func TestS3Config_Config(t *testing.T) {
 		{"s3_bucket", s3.Bucket},
 		{"s3_accessKey", s3.AccessKey},
 		{"s3_endpoint", s3.Endpoint},
+		{"s3_prefix", s3.Prefix},
 		{"s3_secretKey", s3.SecretKey},
 		{"s3_sessionToken", s3.SessionToken},
 	}
@@ -29,7 +30,7 @@ func TestS3Config_Config(t *testing.T) {
 }
 
 func TestStorage_S3Config(t *testing.T) {
-	in := storage.S3Config{"ak", "bkt", "end", "sk", "tkn"}
+	in := storage.S3Config{"ak", "bkt", "end", "pre", "sk", "tkn"}
 	s := storage.NewStorage(storage.ProviderS3, in)
 	out := s.S3Config()
 	if in.Bucket != out.Bucket {
@@ -40,6 +41,9 @@ func TestStorage_S3Config(t *testing.T) {
 	}
 	if in.Endpoint != out.Endpoint {
 		t.Errorf("expected S3Config.Endpoint to be [%s], got [%s]", in.Endpoint, out.Endpoint)
+	}
+	if in.Prefix != out.Prefix {
+		t.Errorf("expected S3Config.Prefix to be [%s], got [%s]", in.Prefix, out.Prefix)
 	}
 	if in.SecretKey != out.SecretKey {
 		t.Errorf("expected S3Config.SecretKey to be [%s], got [%s]", in.SecretKey, out.SecretKey)
