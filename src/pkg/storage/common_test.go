1532c919db1c42e68e71b9f8e0c050651d131753
ryanfkeepers <ryanfkeepers@gmail.com>
2022-06-02 14:45:46 -0600

    add validation failure tests to storage

--- src/pkg/storage/common_test.go
+++ src/pkg/storage/common_test.go
@@ -17,8 +17,10 @@ func TestCommonCfgSuite(t *testing.T) {
 	suite.Run(t, new(CommonCfgSuite))
 }
 
+var goodCommonConfig = storage.CommonConfig{"passwd"}
+
 func (suite *CommonCfgSuite) TestCommonConfig_Config() {
-	cfg := storage.CommonConfig{"passwd"}
+	cfg := goodCommonConfig
 	c, err := cfg.Config()
 	assert.NoError(suite.T(), err)
 
@@ -38,11 +40,49 @@ func (suite *CommonCfgSuite) TestCommonConfig_Config() {
 func (suite *CommonCfgSuite) TestStorage_CommonConfig() {
 	t := suite.T()
 
-	in := storage.CommonConfig{"passwd"}
+	in := goodCommonConfig
 	s, err := storage.NewStorage(storage.ProviderUnknown, in)
 	assert.NoError(t, err)
 	out, err := s.CommonConfig()
 	assert.NoError(t, err)
 
 	assert.Equal(t, in.CorsoPassword, out.CorsoPassword)
+}
+
+func (suite *CommonCfgSuite) TestStorage_CommonConfig_InvalidCases() {
+	// missing required properties
+	table := []struct {
+		name string
+		cfg  storage.CommonConfig
+	}{
+		{"missing password", storage.CommonConfig{}},
+	}
+	for _, test := range table {
+		suite.T().Run(test.name, func(t *testing.T) {
+			_, err := storage.NewStorage(storage.ProviderUnknown, test.cfg)
+			assert.Error(t, err)
+		})
+	}
+
+	// required property not populated in storage
+	table2 := []struct {
+		name  string
+		amend func(storage.Storage)
+	}{
+		{
+			"missing password",
+			func(s storage.Storage) {
+				s.Config["common_corsoPassword"] = ""
+			},
+		},
+	}
+	for _, test := range table2 {
+		suite.T().Run(test.name, func(t *testing.T) {
+			st, err := storage.NewStorage(storage.ProviderUnknown, goodCommonConfig)
+			assert.NoError(t, err)
+			test.amend(st)
+			_, err = st.CommonConfig()
+			assert.Error(t, err)
+		})
+	}
 }
