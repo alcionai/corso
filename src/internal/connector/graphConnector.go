01c275707ac9b71951ee16c493a0e81992d2d604
Danny <danny@alcion.ai>
2022-06-03 02:46:15 -0400

    GraphConnector initialization (#104)
    * Issue #68: Updates to dependency files.
    
    * Issue #68: Added GraphConnector module and test suite.
    
    * Issue #68: ErrorList module and test added to package to support GraphConnector.
    
    * Issue #68: Updates made to address comments.
    
    * Issue #68: Added additonal comments to structs and corrected library changes.
    
    * Issue #68: Added additonal comments to structs and corrected library changes.
    
    * Issue #68: Updates made to package.
    
    * Update errorList.go
    
    Fix typo
    
    * Issue #68: ErrorList struct removal required
    The structure of the errorList is not required for the first
    initialization of the GraphConnector. In the case that this structure or
    addtional functionality needs to be added in the future, an issue will
    be created.
    
    * Issue #68: Simplification and structure change
    The errorList has been changed to []errors. Additionally, the
    credentials and authentication have been changed to inline. Provided
    early exits for creating NewGraphConnectors. Note, adapter objects can
    be created with strings that fail authentication. Reduced the amount of logs.
    
    * Issue #68: Prepend
    [errors](https://github.com/alcionai/corso/pull/104#discussion_r887268576)
    This fixes the bug that identified in the PR.
    
    * Update src/internal/connector/graphConnector.go
    
    Adjustment for styling.
    
    Co-authored-by: Keepers <104464746+ryanfkeepers@users.noreply.github.com>
    
    * Update src/internal/connector/graphConnector_test.go
    
    Issue #68: PR Resolution
    Test environmental value changed to be consistent with the rest of the package.
    
    Co-authored-by: Keepers <104464746+ryanfkeepers@users.noreply.github.com>
    
    * Issue #68: Resolving Open Questions from reviewers
    Prior to commit, Issue #116. Additionally, the entire error package was
    requested to be removed from the component. GraphConnector tests are
    preliminary set-up to investigate the Issue #116. The code will be
    redacted after the new functionality is verified. Stashing for merge for
    the changes from the reviewers.
    
    * Issue #116: Bug fix. Response verified not to be nil prior to using it in next step.
    
    * Issue #116: Added table structure to verify GraphConnector can handle being given an empty set of parameters.
    
    * Issue #68: PR Changes
    List of changes:
    - Added check to ensure conversion of user object passed
    - setTenant not exported
    - TODO added user query during initialization
    - Iteration flow adjusted for paging.
    
    Co-authored-by: Keepers <104464746+ryanfkeepers@users.noreply.github.com>

---
+++ src/internal/connector/graphConnector.go
@@ -0,0 +1,121 @@
+// Package connector uploads and retrieves data from M365 through
+// the msgraph-go-sdk.
+package connector
+
+import (
+	"errors"
+	"fmt"
+
+	az "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	ka "github.com/microsoft/kiota-authentication-azure-go"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	msgraphgocore "github.com/microsoftgraph/msgraph-sdk-go-core"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	msuser "github.com/microsoftgraph/msgraph-sdk-go/users"
+)
+
+// GraphConnector is a struct used to wrap the GraphServiceClient and
+// GraphRequestAdapter from the msgraph-sdk-go. Additional fields are for
+// bookkeeping and interfacing with other component.
+type GraphConnector struct {
+	tenant  string
+	adapter msgraphsdk.GraphRequestAdapter
+	client  msgraphsdk.GraphServiceClient
+	Users   map[string]string //key<email> value<id>
+	Streams string            //Not implemented for ease of code check-in
+}
+
+func NewGraphConnector(tenantId, clientId, secret string) (*GraphConnector, error) {
+	// Client Provider: Uses Secret for access to tenant-level data
+	cred, err := az.NewClientSecretCredential(tenantId, clientId, secret, nil)
+	if err != nil {
+		return nil, err
+	}
+	auth, err := ka.NewAzureIdentityAuthenticationProviderWithScopes(cred, []string{"https://graph.microsoft.com/.default"})
+	if err != nil {
+		return nil, err
+	}
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(auth)
+	if err != nil {
+		return nil, err
+	}
+	gc := GraphConnector{
+		tenant:  tenantId,
+		adapter: *adapter,
+		client:  *msgraphsdk.NewGraphServiceClient(adapter),
+		Users:   make(map[string]string, 0),
+	}
+	// TODO: Revisit Query all users.
+	err = gc.setTenantUsers()
+	if err != nil {
+		return nil, err
+	}
+	return &gc, nil
+}
+
+// setTenantUsers queries the M365 to identify the users in the
+// workspace. The users field is updated during this method
+// iff the return value is true
+func (gc *GraphConnector) setTenantUsers() error {
+	selecting := []string{"id, mail"}
+	errorList := make([]error, 0)
+	requestParams := &msuser.UsersRequestBuilderGetQueryParameters{
+		Select: selecting,
+	}
+	options := &msuser.UsersRequestBuilderGetRequestConfiguration{
+		QueryParameters: requestParams,
+	}
+	response, err := gc.client.Users().GetWithRequestConfigurationAndResponseHandler(options, nil)
+	if err != nil {
+		return err
+	}
+	if response == nil {
+		return errors.New("connector unable to complete queries. Verify credentials")
+	}
+	userIterator, err := msgraphgocore.NewPageIterator(response, &gc.adapter, models.CreateUserCollectionResponseFromDiscriminatorValue)
+	if err != nil {
+		return err
+	}
+	var hasFailed error
+	callbackFunc := func(userItem interface{}) bool {
+		user, ok := userItem.(models.Userable)
+		if !ok {
+			errorList = append(errorList, errors.New("Unable to iterable to user"))
+			return true
+		}
+		gc.Users[*user.GetMail()] = *user.GetId()
+		return true
+	}
+	hasFailed = userIterator.Iterate(callbackFunc)
+	if len(errorList) > 0 {
+		return errors.New(ConvertErrorList(errorList))
+	}
+	return hasFailed
+}
+
+// ConvertsErrorList takes a list of errors and converts returns
+// a string
+func ConvertErrorList(errorList []error) string {
+	errorLog := ""
+	for idx, err := range errorList {
+		errorLog = errorLog + fmt.Sprintf("Error# %d\t%v\n", idx, err)
+	}
+	return errorLog
+}
+
+// GetUsers returns the email address of users within tenant.
+func (gc *GraphConnector) GetUsers() []string {
+	keys := make([]string, 0)
+	for k := range gc.Users {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (gc *GraphConnector) GetUsersIds() []string {
+	values := make([]string, 0)
+	for _, v := range gc.Users {
+		values = append(values, v)
+	}
+	return values
+}
---
+++ src/internal/connector/graphConnector_test.go
@@ -0,0 +1,67 @@
+package connector_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	graph "github.com/alcionai/corso/internal/connector"
+)
+
+type GraphConnectorTestSuite struct {
+	suite.Suite
+	connector *graph.GraphConnector
+}
+
+type DiconnectedGraphConnectorTestSuite struct {
+	suite.Suite
+}
+
+func (suite *GraphConnectorTestSuite) SetupSuite() {
+	tenant := os.Getenv("TENANT_ID")
+	client := os.Getenv("CLIENT_ID")
+	secret := os.Getenv("CLIENT_SECRET")
+	if os.Getenv("CI") == "" {
+		var err error
+		suite.connector, err = graph.NewGraphConnector(tenant, client, secret)
+		assert.Nil(suite.T(), err)
+	}
+}
+
+func TestGraphConnectorSuite(t *testing.T) {
+	suite.Run(t, new(GraphConnectorTestSuite))
+}
+
+func TestDisconnectedGraphSuite(t *testing.T) {
+	suite.Run(t, new(DiconnectedGraphConnectorTestSuite))
+}
+
+func (suite *DiconnectedGraphConnectorTestSuite) TestBadConnection() {
+	table := []struct {
+		name   string
+		params []string
+	}{
+		{
+			name:   "Invalid Credentials",
+			params: []string{"Test", "without", "data"},
+		},
+		{
+			name:   "Empty Credentials",
+			params: []string{"", "", ""},
+		},
+	}
+	for _, test := range table {
+		gc, err := graph.NewGraphConnector(test.params[0], test.params[1], test.params[2])
+		assert.Nil(suite.T(), gc, test.name+" failed")
+		assert.NotNil(suite.T(), err, test.name+"failed")
+	}
+}
+
+func (suite *GraphConnectorTestSuite) TestGraphConnector() {
+	if os.Getenv("INTEGRATION_TESTING") != "" {
+		suite.T().Skip("Environmental Variables not set")
+	}
+	suite.True(suite.connector != nil)
+}
