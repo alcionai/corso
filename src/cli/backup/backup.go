216c7f344e460dd523ed37e22f37d98908cb035f
ryanfkeepers <ryanfkeepers@gmail.com>
2022-06-06 13:31:17 -0600

    add backup to cli cmds, fix verbiage

--- src/cli/backup/backup.go
+++ src/cli/backup/backup.go
@@ -4,12 +4,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// AddCommands attaches all `corso backup * *` commands to the parent.
+func AddCommands(parent *cobra.Command) {
+	parent.AddCommand(backupCmd)
+}
+
 // The backup category of commands.
 // `corso backup [<subcommand>] [<flag>...]`
 var backupCmd = &cobra.Command{
 	Use:   "backup",
-	Short: "Back up your application data.",
-	Long:  `Back up the data stored in one of your M365 applications.`,
+	Short: "Backup your application data.",
+	Long:  `Backup the data stored in one of your M365 applications.`,
 	Run:   handleBackupCmd,
 	Args:  cobra.NoArgs,
 }
