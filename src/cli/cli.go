127df32284a7a4128e8a8c826ea022a6abcb55bc
Vaibhav Kamra <vkamra@alcion.ai>
2022-06-06 19:24:06 -0700

    Initialize a config file

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/alcionai/corso/cli/backup"
 	"github.com/alcionai/corso/cli/repo"
@@ -22,7 +23,31 @@ var corsoCmd = &cobra.Command{
 // the root-level flags
 var (
 	version bool
+	cfgFile string
 )
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".corso" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".corso")
+	}
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
 
 // Handler for flat calls to `corso`.
 // Produces the same output as `corso --help`.
@@ -37,6 +62,7 @@ func handleCorsoCmd(cmd *cobra.Command, args []string) {
 // Handle builds and executes the cli processor.
 func Handle() {
 	corsoCmd.Flags().BoolP("version", "v", version, "current version info")
+	corsoCmd.PersistentFlags().StringVar(&cfgFile, "config-file", "", "config file (default is $HOME/.corso)")
 
 	repo.AddCommands(corsoCmd)
 	backup.AddCommands(corsoCmd)
