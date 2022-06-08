0da0c786b5ee7644a1c5af6a933c07a9f8b38592
Vaibhav Kamra <vkamra@alcion.ai>
2022-06-08 16:33:59 -0700

    Mock ExchangeDataCollection (#166)
    Implements a mock exchange data collection that can be used to test without connecting to graph.
    
    Currently returns a "fixed message" but we can extend this to return anything that implements io.Reader
    e.g. a file that contains a serialized message.

--- src/go.mod
+++ src/go.mod
@@ -77,6 +77,7 @@ require (
 	github.com/rs/xid v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	github.com/zeebo/blake3 v0.2.3 // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
---
+++ src/internal/connector/mockconnector/mock_data_collection.go
@@ -0,0 +1,63 @@
+package mockconnector
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/google/uuid"
+
+	"github.com/alcionai/corso/internal/connector"
+)
+
+// MockExchangeDataCollection represents a mock exchange mailbox
+type MockExchangeDataCollection struct {
+	fullPath     []string
+	messageCount int
+	messagesRead int
+}
+
+var (
+	_ connector.DataCollection = &MockExchangeDataCollection{}
+	_ connector.DataStream     = &MockExchangeData{}
+)
+
+// NewMockExchangeDataCollection creates an data collection that will return the specified number of
+// mock messages when iterated
+func NewMockExchangeDataCollection(pathRepresentation []string, numMessagesToReturn int) *MockExchangeDataCollection {
+	collection := &MockExchangeDataCollection{
+		fullPath:     pathRepresentation,
+		messageCount: numMessagesToReturn,
+		messagesRead: 0,
+	}
+	return collection
+}
+
+func (medc *MockExchangeDataCollection) FullPath() []string {
+	return append([]string{}, medc.fullPath...)
+}
+
+// NextItem returns either the next item in the collection or an error if one occurred.
+// If not more items are available in the collection, returns (nil, nil).
+func (medc *MockExchangeDataCollection) NextItem() (connector.DataStream, error) {
+	if medc.messagesRead < medc.messageCount {
+		medc.messagesRead++
+		// We can plug in whatever data we want here (can be an io.Reader to a test data file if needed)
+		m := []byte("test message")
+		return &MockExchangeData{uuid.NewString(), bytes.NewReader(m)}, nil
+	}
+	return nil, io.EOF
+}
+
+// ExchangeData represents a single item retrieved from exchange
+type MockExchangeData struct {
+	id     string
+	reader io.Reader
+}
+
+func (med *MockExchangeData) UUID() string {
+	return med.id
+}
+
+func (med *MockExchangeData) ToReader() io.Reader {
+	return med.reader
+}
