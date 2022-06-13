6a78b3abbe9636b9bb39263812e92cc2fd59acbe
Joseph Elwin Fernandes <josephaug26@gmail.com>
2022-06-13 18:05:36 +0530

    mirror changes

--- src/internal/backupManifest/backupManifest.go
+++ src/internal/backupManifest/backupManifest.go
@@ -26,8 +26,8 @@ type AttachmentMetadata struct {
 
 type BackManifestHandler interface {
 	Open(path string) error
-	Insert(bem BackEntityMeta) error
-	Search(calbck func(bem BackEntityMeta, err error) error, filters ...string) error
+	Insert(bem BackEntityMetadata) error
+	Search(callbck func(bem BackEntityMetadata, err error) error, filters ...string) error
 	Close() error
 	Delete() error
 }
