15e7c102a42bc92fca353231d210d97c0f221394
Keepers <104464746+ryanfkeepers@users.noreply.github.com>
2022-06-15 16:10:11 -0600

    add RestoreOperation (#211)
    Adds the RestoreOperation to the /internal/operations pkg.
    restoreOp.Run() is a no-op at this time since data layer
    packages are not prepared to hook up e2e.

---
+++ src/internal/operations/restore_test.go
@@ -0,0 +1,63 @@
+package operations_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/alcionai/corso/internal/kopia"
+	"github.com/alcionai/corso/internal/operations"
+	ctesting "github.com/alcionai/corso/internal/testing"
+	"github.com/alcionai/corso/pkg/credentials"
+)
+
+type RestoreOpIntegrationSuite struct {
+	suite.Suite
+}
+
+func TestRestoreOpIntegrationSuite(t *testing.T) {
+	if err := ctesting.RunOnAny(ctesting.CorsoCITests); err != nil {
+		t.Skip(err)
+	}
+	suite.Run(t, new(RestoreOpIntegrationSuite))
+}
+
+func (suite *RestoreOpIntegrationSuite) SetupSuite() {
+	_, err := ctesting.GetRequiredEnvVars(
+		credentials.TenantID,
+		credentials.ClientID,
+		credentials.ClientSecret,
+	)
+	require.NoError(suite.T(), err)
+}
+
+func (suite *RestoreOpIntegrationSuite) TestNewRestoreOperation() {
+	kw := &kopia.KopiaWrapper{}
+	creds := credentials.GetM365()
+	table := []struct {
+		name     string
+		opts     operations.OperationOpts
+		kw       *kopia.KopiaWrapper
+		creds    credentials.M365
+		targets  []string
+		errCheck assert.ErrorAssertionFunc
+	}{
+		{"good", operations.OperationOpts{}, kw, creds, nil, assert.NoError},
+		{"missing kopia", operations.OperationOpts{}, nil, creds, nil, assert.Error},
+		{"invalid creds", operations.OperationOpts{}, kw, credentials.M365{}, nil, assert.Error},
+	}
+	for _, test := range table {
+		suite.T().Run(test.name, func(t *testing.T) {
+			_, err := operations.NewRestoreOperation(
+				context.Background(),
+				operations.OperationOpts{},
+				test.kw,
+				test.creds,
+				nil)
+			test.errCheck(t, err)
+		})
+	}
+}
