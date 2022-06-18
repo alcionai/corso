15bca25efc767c67b9b54feac929dab532d586f7
Joseph Elwin Fernandes <josephaug26@gmail.com>
2022-06-18 19:17:58 +0530

    Fixing review comments

--- src/internal/backupManifest/backupManifest.go
+++ src/internal/backupManifest/backupManifest.go
@@ -1,4 +1,4 @@
-package backupManifest
+package backupmanifest
 
 import (
 	"database/sql"
@@ -6,8 +6,6 @@ import (
 
 type BackEntityMetadata interface {
 	GetID() string
-	Insert(db *sql.DB) error
-	readCallBack(callback searchCallBack, row sql.Row) error // call the user call back internally
 }
 
 /*******************************************************************
@@ -27,16 +25,6 @@ func (mm *MessageMetadata) GetID() string {
 	return ""
 }
 
-func (mm *MessageMetadata) Insert(db *sql.DB) error {
-	//TODO
-	return nil
-}
-
-func (mm *MessageMetadata) readCallBack(callback searchCallBack, row sql.Row) error {
-	//TODO
-	return nil
-}
-
 /*******************************************************************
 
 							EventsMetadata
@@ -52,16 +40,6 @@ type EventsMetadata struct {
 func (em *EventsMetadata) GetID() string {
 	//TODO
 	return ""
-}
-
-func (em *EventsMetadata) Insert(db *sql.DB) error {
-	//TODO
-	return nil
-}
-
-func (em *EventsMetadata) readCallBack(callback searchCallBack, row sql.Row) error {
-	//TODO
-	return nil
 }
 
 /*******************************************************************
@@ -81,16 +59,6 @@ func (em *ContactMetadata) GetID() string {
 	return ""
 }
 
-func (em *ContactMetadata) Insert(db *sql.DB) error {
-	//TODO
-	return nil
-}
-
-func (em *ContactMetadata) readCallBack(callback searchCallBack, row sql.Row) error {
-	//TODO
-	return nil
-}
-
 /* For Future reference
 //implements BackEntityMetadata
 type FileMetadata struct {
@@ -106,83 +74,21 @@ type ObjectMetadata struct {
 type AttachmentMetadata struct {
 }
 
-type searchCallBack func(bem BackEntityMetadata, err error)
-
 /*******************************************************************
 
 						BackManifestHandler
 
 ********************************************************************/
 
-type BackManifestHandler interface {
+type FilterMap map[string]string
+
+type BackupManifestHandler interface {
 	//Validate Search Query Index And Filters
 	validateSearchQueryIndexAndFilters(searchQueryPrefixIndex []int, XXXfilters []string) error
 	//Get Search Query Prefix using index
 	getSearchQueryPrefixString(searchQueryPrefixIndex int) string
 	//Open the database
 	Open(name string) error
-	//Insert into the database
-	Insert(bem BackEntityMetadata) error
-	//Search in the database with filters
-	/*
-
-		THIS COMMENT IS ONLY FOR DESIGN UNDERSTANDING!
-
-		const XXXSearchQueryAllXXXMetadataType1PrefixIndex int = 1 // XXXMetadataType1 type
-		const exchangeSearchQueryAllXXXMetadataType2PrefixIndex int = 2   // XXXMetadataType2 type
-		const exchangeSearchQueryAllXXXMetadataType3PrefixIndex = 3      //XXXMetadataType3 type
-
-		const exchangeSearchQueryAllXXXMetadataType1PrefixString string = "SELECT * FROM type1 TYPE1"
-		const exchangeSearchQueryAllXXXMetadataType2PrefixString string = "SELECT * FROM type2 TYPE2"
-		const exchangeSearchQueryAllXXXMetadataType3PrefixString string = "SELECT * FROM type3 TYPE3"
-
-
-
-		The user calls Search() with a call back function.
-		//Pseudo code for a XXXBackupManifestHandler which handles metadata of
-		// XXXMetadataType1, XXXMetadataType2, XXXMetadataType3 etc
-		func (XBMH *XXXBackupManifestHandler) Search (callback searchCallBack, searchQueryPrefixIndex []int, XXXfilters []string) error {
-
-			//Validation of searchQueryPrefixIndex and XXXfilters for the appropriate XXXBackupManifestHandler
-			err := XBMH.validateXXXSearchQueryIndexAndFilters (searchQueryPrefixIndex, XXXfilters)
-			if err != nil {
-				return err
-			}
-
-			for sqIndex in searchQueryPrefixIndex {
-				var actualSearchQuery []string
-				actualSearchQuery = XBMH.getSearchQueryPrefixString(sqIndex)
-				//If there are filters
-				if !isEmpty(XXXfilters) {
-					append(actualSearchQuery, " where ")
-					for filter in XXXfilters {
-						//Only append filter of appropriate to the search query/XXXMetadata type
-						if (isFilterOfType(filter, sqIndex)) {
-							append(actualSearchQuery, filter)
-						}
-					}
-				}
-
-
-				rows = XBMH.db.Query (actualSearchQuery)
-				for row in rows {
-					switch (sqIndex) {
-						case type1:
-							XXXMetadataType1 := NewXXXMetadataType1()
-							XXXMetadataType1.readCallBack(callback, row) // This call the users callback function for each resultant row
-						case type2:
-							XXXMetadataType1 := NewXXXMetadataType2()
-							XXXMetadataType1.readCallBack(callback, row) // This call the users callback function for each resultant row
-						case type3:
-							....
-					}
-				}
-			}
-
-		}
-
-	*/
-	Search(callback searchCallBack, searchQueryIndex []int, filters []string) error
 	//Close the database
 	Close() error
 	//Destory the database
@@ -201,18 +107,10 @@ type ExchangeBackupManifestHandler struct {
 	db                     sql.DB
 }
 
-// Exchange Search query constant for exchange
-const exchangeSearchQueryAllMessagesPrefixIndex int = 1 // Messages type
-const exchangeSearchQueryAllEventsPrefixIndex int = 2   // Event type
-const exchangeSearchQueryAllContactPrefixIndex = 3      //Contact type
-
-const exchangeSearchQueryAllMessagesPrefixString string = "SELECT * FROM message MESSAGE"
-const exchangeSearchQueryAllEventsPrefixString string = "SELECT * FROM event EVENTS"
-const exchangeSearchQueryAllContactsPrefixString string = "SELECT * FROM contact CONTACTS"
-
-// Exchange Search query filter constants examples:
-const exchangeSearchQueryFilterMessageSender string = "message.sender"
-const exchangeSearchQueryFilterEventSender string = "event.sender"
+//Insert into the database
+func (ebmh *ExchangeBackupManifestHandler) InsertMessageMetadata(mm MessageMetadata) error {
+	return nil
+}
 
 //Etc..more to be added according to List/Restore requirement
 
