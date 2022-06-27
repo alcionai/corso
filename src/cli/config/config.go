89b53c78ddb38666cc14323f32d6cb7276426962
ryanfkeepers <ryanfkeepers@gmail.com>
2022-06-27 14:47:39 -0600

    refactor cli/config for better local testing

--- src/cli/config/config.go
+++ src/cli/config/config.go
@@ -16,13 +16,14 @@ import (
 
 const (
 	// S3 config
-	ProviderTypeKey = "provider"
-	BucketNameKey   = "bucket"
-	EndpointKey     = "endpoint"
-	PrefixKey       = "prefix"
+	StorageProviderTypeKey = "provider"
+	BucketNameKey          = "bucket"
+	EndpointKey            = "endpoint"
+	PrefixKey              = "prefix"
 
 	// M365 config
-	TenantIDKey = "tenantid"
+	AccountProviderTypeKey = "account_provider"
+	TenantIDKey            = "tenantid"
 )
 
 func InitConfig(configFilePath string) error {
@@ -64,7 +65,7 @@ func InitConfig(configFilePath string) error {
 func WriteRepoConfig(s3Config storage.S3Config, account account.M365Config) error {
 	// Rudimentary support for persisting repo config
 	// TODO: Handle conflicts, support other config types
-	viper.Set(ProviderTypeKey, storage.ProviderS3.String())
+	viper.Set(StorageProviderTypeKey, storage.ProviderS3.String())
 	viper.Set(BucketNameKey, s3Config.Bucket)
 	viper.Set(EndpointKey, s3Config.Endpoint)
 	viper.Set(PrefixKey, s3Config.Prefix)
@@ -79,57 +80,80 @@ func WriteRepoConfig(s3Config storage.S3Config, account account.M365Config) erro
 	return nil
 }
 
-func ReadRepoConfig() (storage.S3Config, account.Account, error) {
-	var (
-		s3Config storage.S3Config
-		acct     account.Account
-		err      error
-	)
+func readRepoConfig() error {
+	var err error
 
 	if err = viper.ReadInConfig(); err != nil {
-		return s3Config, acct, errors.Wrap(err, "reading config file: "+viper.ConfigFileUsed())
+		return errors.Wrap(err, "reading config file: "+viper.ConfigFileUsed())
 	}
 
-	if providerType := viper.GetString(ProviderTypeKey); providerType != storage.ProviderS3.String() {
-		return s3Config, acct, errors.New("Unsupported storage provider: " + providerType)
+	return err
+}
+
+// prerequisite: readRepoConfig must have been run prior to this to populate the global viper values.
+func s3ConfigsFromViper() (storage.S3Config, error) {
+	var s3Config storage.S3Config
+
+	providerType := viper.GetString(StorageProviderTypeKey)
+	if providerType != storage.ProviderS3.String() {
+		return s3Config, errors.New("unsupported storage provider: " + providerType)
 	}
 
 	s3Config.Bucket = viper.GetString(BucketNameKey)
 	s3Config.Endpoint = viper.GetString(EndpointKey)
 	s3Config.Prefix = viper.GetString(PrefixKey)
+	return s3Config, nil
+}
 
-	m365Creds := credentials.GetM365()
-	tenantID := os.Getenv(account.TenantID)
-	cfgTenantID := viper.GetString(TenantIDKey)
-	if len(cfgTenantID) > 0 {
-		tenantID = cfgTenantID
+// prerequisite: readRepoConfig must have been run prior to this to populate the global viper values.
+func m365ConfigsFromViper() (account.M365Config, error) {
+	var m365 account.M365Config
+
+	providerType := viper.GetString(AccountProviderTypeKey)
+	if providerType != account.ProviderM365.String() {
+		return m365, errors.New("unsupported account provider: " + providerType)
 	}
-	acct, err = account.NewAccount(
-		account.ProviderM365,
-		account.M365Config{
-			M365:     m365Creds,
-			TenantID: tenantID,
-		},
-	)
 
-	return s3Config, acct, err
+	m365.TenantID = first(viper.GetString(TenantIDKey), os.Getenv(account.TenantID))
+	return m365, nil
 }
 
 // GetStorageAndAccount creates a storage and account instance by mediating all the possible
 // data sources (config file, env vars, flag overrides) and the config file.
 func GetStorageAndAccount(readFromFile bool, overrides map[string]string) (storage.Storage, account.Account, error) {
 	var (
-		s3Cfg storage.S3Config
-		acct  account.Account
-		err   error
+		s3Cfg   storage.S3Config
+		store   storage.Storage
+		m365Cfg account.M365Config
+		acct    account.Account
+		err     error
 	)
 
 	// possibly read the prior config from a .corso file
 	if readFromFile {
-		s3Cfg, acct, err = ReadRepoConfig()
-		if err != nil {
-			return storage.Storage{}, acct, errors.Wrap(err, "reading corso config file")
+		if err = readRepoConfig(); err != nil {
+			return store, acct, errors.Wrap(err, "reading corso config file")
 		}
+
+		s3Cfg, err = s3ConfigsFromViper()
+		if err != nil {
+			return store, acct, errors.Wrap(err, "reading s3 configs from corso config file")
+		}
+
+		m365Cfg, err = m365ConfigsFromViper()
+		if err != nil {
+			return store, acct, errors.Wrap(err, "reading m365 configs from corso config file")
+		}
+	}
+
+	// compose the m365 account config and credentials
+	m365Cfg = account.M365Config{
+		M365:     credentials.GetM365(),
+		TenantID: first(overrides[account.TenantID], m365Cfg.TenantID, os.Getenv(account.TenantID)),
+	}
+	acct, err = account.NewAccount(account.ProviderM365, m365Cfg)
+	if err != nil {
+		return store, acct, errors.Wrap(err, "retrieving m365 account configuration")
 	}
 
 	// compose the s3 storage config and credentials
