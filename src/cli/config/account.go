fc631d32a7e19db08fa97a969a5cfcc97bf9bf77
ryanfkeepers <ryanfkeepers@gmail.com>
2022-06-29 17:07:38 -0600

    allow connect to create .corso config file
    Current bug: if no .corso config file exists, then repo connect
    will always fail, even if it has the correct details to build
    a new config file.  Solution: allow connect to build a .corso
    config file when missing, so long as the operation succeeds
    otherwise.
    
    In tandem, return an error whenever a user attempts to
    call repo connect with details that do not match the existing
    .corso config file.

---
+++ src/cli/config/account.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+	"os"
+
+	"github.com/alcionai/corso/cli/utils"
+	"github.com/alcionai/corso/pkg/account"
+	"github.com/alcionai/corso/pkg/credentials"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+// prerequisite: readRepoConfig must have been run prior to this to populate the global viper values.
+func m365ConfigsFromViper(vpr *viper.Viper) (account.M365Config, error) {
+	var m365 account.M365Config
+
+	providerType := vpr.GetString(AccountProviderTypeKey)
+	if providerType != account.ProviderM365.String() {
+		return m365, errors.New("unsupported account provider: " + providerType)
+	}
+
+	m365.TenantID = first(vpr.GetString(TenantIDKey), os.Getenv(account.TenantID))
+	return m365, nil
+}
+
+func m365Overrides(in map[string]string) map[string]string {
+	return map[string]string{
+		account.TenantID:       in[account.TenantID],
+		AccountProviderTypeKey: in[AccountProviderTypeKey],
+	}
+}
+
+// configureAccount builds a complete account configuration from a mix of
+// viper properties and manual overrides.
+func configureAccount(vpr *viper.Viper, readConfigFromViper bool, overrides map[string]string) (account.Account, error) {
+	var (
+		m365Cfg account.M365Config
+		acct    account.Account
+		err     error
+	)
+
+	if readConfigFromViper {
+		m365Cfg, err = m365ConfigsFromViper(vpr)
+		if err != nil {
+			return acct, errors.Wrap(err, "reading m365 configs from corso config file")
+		}
+
+		if err := mustMatchConfig(vpr, m365Overrides(overrides)); err != nil {
+			return acct, errors.Wrap(err, "verifying m365 configs in corso config file")
+		}
+	}
+
+	// compose the m365 config and credentials
+	m365 := credentials.GetM365()
+	if err := m365.Validate(); err != nil {
+		return acct, errors.Wrap(err, "validating m365 credentials")
+	}
+
+	m365Cfg = account.M365Config{
+		M365:     m365,
+		TenantID: first(overrides[account.TenantID], m365Cfg.TenantID, os.Getenv(account.TenantID)),
+	}
+
+	// ensure requried properties are present
+	if err := utils.RequireProps(map[string]string{
+		credentials.ClientID:     m365Cfg.ClientID,
+		credentials.ClientSecret: m365Cfg.ClientSecret,
+		account.TenantID:         m365Cfg.TenantID,
+	}); err != nil {
+		return acct, err
+	}
+
+	// build the account
+	acct, err = account.NewAccount(account.ProviderM365, m365Cfg)
+	if err != nil {
+		return acct, errors.Wrap(err, "retrieving m365 account configuration")
+	}
+
+	return acct, nil
+}
---
+++ src/cli/config/storage.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"github.com/alcionai/corso/cli/utils"
+	"github.com/alcionai/corso/pkg/credentials"
+	"github.com/alcionai/corso/pkg/storage"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+// prerequisite: readRepoConfig must have been run prior to this to populate the global viper values.
+func s3ConfigsFromViper(vpr *viper.Viper) (storage.S3Config, error) {
+	var s3Config storage.S3Config
+
+	providerType := vpr.GetString(StorageProviderTypeKey)
+	if providerType != storage.ProviderS3.String() {
+		return s3Config, errors.New("unsupported storage provider: " + providerType)
+	}
+
+	s3Config.Bucket = vpr.GetString(BucketNameKey)
+	s3Config.Endpoint = vpr.GetString(EndpointKey)
+	s3Config.Prefix = vpr.GetString(PrefixKey)
+	return s3Config, nil
+}
+
+func s3Overrides(in map[string]string) map[string]string {
+	return map[string]string{
+		storage.Bucket:         in[storage.Bucket],
+		storage.Endpoint:       in[storage.Endpoint],
+		storage.Prefix:         in[storage.Prefix],
+		StorageProviderTypeKey: in[StorageProviderTypeKey],
+	}
+}
+
+// configureStorage builds a complete storage configuration from a mix of
+// viper properties and manual overrides.
+func configureStorage(vpr *viper.Viper, readConfigFromViper bool, overrides map[string]string) (storage.Storage, error) {
+	var (
+		s3Cfg storage.S3Config
+		store storage.Storage
+		err   error
+	)
+
+	if readConfigFromViper {
+		if s3Cfg, err = s3ConfigsFromViper(vpr); err != nil {
+			return store, errors.Wrap(err, "reading s3 configs from corso config file")
+		}
+
+		if err := mustMatchConfig(vpr, s3Overrides(overrides)); err != nil {
+			return store, errors.Wrap(err, "verifying s3 configs in corso config file")
+		}
+	}
+
+	// compose the s3 storage config and credentials
+	aws := credentials.GetAWS(overrides)
+	if err := aws.Validate(); err != nil {
+		return store, errors.Wrap(err, "validating aws credentials")
+	}
+
+	s3Cfg = storage.S3Config{
+		AWS:      aws,
+		Bucket:   first(overrides[storage.Bucket], s3Cfg.Bucket),
+		Endpoint: first(overrides[storage.Endpoint], s3Cfg.Endpoint),
+		Prefix:   first(overrides[storage.Prefix], s3Cfg.Prefix),
+	}
+
+	// compose the common config and credentials
+	corso := credentials.GetCorso()
+	if err := corso.Validate(); err != nil {
+		return store, errors.Wrap(err, "validating corso credentials")
+	}
+	cCfg := storage.CommonConfig{
+		Corso: corso,
+	}
+
+	// ensure requried properties are present
+	if err := utils.RequireProps(map[string]string{
+		credentials.AWSAccessKeyID:     aws.AccessKey,
+		storage.Bucket:                 s3Cfg.Bucket,
+		credentials.AWSSecretAccessKey: aws.SecretKey,
+		credentials.AWSSessionToken:    aws.SessionToken,
+		credentials.CorsoPassword:      corso.CorsoPassword,
+	}); err != nil {
+		return storage.Storage{}, err
+	}
+
+	// build the storage
+	store, err = storage.NewStorage(storage.ProviderS3, s3Cfg, cCfg)
+	if err != nil {
+		return store, errors.Wrap(err, "configuring repository storage")
+	}
+
+	return store, nil
+}
