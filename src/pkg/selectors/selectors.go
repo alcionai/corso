ae778df5ae47ccbd751bd75790ea26801164bb2b
Keepers <104464746+ryanfkeepers@users.noreply.github.com>
2022-06-30 09:50:24 -0600

    rename scope to selector (#257)
    * rename scope to selector
    
    per design changes, source is being renamed to selector
    for better readability and clarity.

--- src/pkg/selectors/selectors.go
+++ src/pkg/selectors/selectors.go
@@ -1,4 +1,4 @@
-package source
+package selectors
 
 import (
 	"strconv"
@@ -14,7 +14,7 @@ const (
 	ServiceExchange                // Exchange
 )
 
-var ErrorBadSourceCast = errors.New("wrong source service type")
+var ErrorBadSelectorCast = errors.New("wrong selector service type")
 
 const (
 	scopeKeyGranularity = "granularity"
@@ -27,30 +27,30 @@ const (
 	All = "*"
 )
 
-// The core source.  Has no api for setting or retrieving data.
-// Is only used to pass along more specific source instances.
-type Source struct {
+// The core selector.  Has no api for setting or retrieving data.
+// Is only used to pass along more specific selector instances.
+type Selector struct {
 	TenantID string              // The tenant making the request.
 	service  service             // The service scope of the data.  Exchange, Teams, Sharepoint, etc.
 	scopes   []map[string]string // A slice of scopes.  Expected to get cast to fooScope within each service handler.
 }
 
-// helper for specific source instance constructors.
-func newSource(tenantID string, s service) Source {
-	return Source{
+// helper for specific selector instance constructors.
+func newSelector(tenantID string, s service) Selector {
+	return Selector{
 		TenantID: tenantID,
 		service:  s,
 		scopes:   []map[string]string{},
 	}
 }
 
-// Service return the service enum for the source.
-func (s Source) Service() service {
+// Service return the service enum for the selector.
+func (s Selector) Service() service {
 	return s.service
 }
 
 func badCastErr(cast, is service) error {
-	return errors.Wrapf(ErrorBadSourceCast, "%s service is not %s", cast, is)
+	return errors.Wrapf(ErrorBadSelectorCast, "%s service is not %s", cast, is)
 }
 
 type scopeGranularity int
@@ -68,8 +68,8 @@ func (g scopeGranularity) String() string {
 	return strconv.Itoa(int(g))
 }
 
-func granularityOf(source map[string]string) scopeGranularity {
-	return scopeGranularity(getIota(source, scopeKeyGranularity))
+func granularityOf(selector map[string]string) scopeGranularity {
+	return scopeGranularity(getIota(selector, scopeKeyGranularity))
 }
 
 // retrieves the iota, stored as a string, and transforms it to
