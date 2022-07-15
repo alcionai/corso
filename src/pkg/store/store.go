cd8c32e7b671235ced8d12eaa7e7dc035c30bcf1
ryanfkeepers <ryanfkeepers@gmail.com>
2022-07-15 16:58:26 -0600

    combine store interfaces

--- src/pkg/store/store.go
+++ src/pkg/store/store.go
@@ -12,23 +12,14 @@ import (
 var _ Storer = &kopia.ModelStore{}
 
 type (
-	deleter interface {
+	Storer interface {
 		Delete(ctx context.Context, s model.Schema, id model.ID) error
 		DeleteWithModelStoreID(ctx context.Context, id manifest.ID) error
-	}
-	getter interface {
 		Get(ctx context.Context, s model.Schema, id model.ID, data model.Model) error
 		GetIDsForType(ctx context.Context, s model.Schema, tags map[string]string) ([]*model.BaseModel, error)
 		GetWithModelStoreID(ctx context.Context, s model.Schema, id manifest.ID, data model.Model) error
-	}
-	updater interface {
 		Put(ctx context.Context, s model.Schema, m model.Model) error
 		Update(ctx context.Context, s model.Schema, m model.Model) error
-	}
-	Storer interface {
-		getter
-		updater
-		deleter
 	}
 )
 
