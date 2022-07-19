b77f92b110dace51fc55015346939855fb5ed64e
ryanfkeepers <ryanfkeepers@gmail.com>
2022-07-19 17:39:49 -0600

    filter backup details by flags
    `backup details` should have its output filtered by the flags provided by
    the user.  In addition, the selector's FilterDetails should maintain
    information (esp service info) about the entries, rather than slicing them
    down to only the path reference.

--- src/cli/backup/exchange_test.go
+++ src/cli/backup/exchange_test.go
@@ -207,7 +207,7 @@ func (suite *ExchangeSuite) TestExchangeBackupCreateSelectors() {
 	}
 	for _, test := range table {
 		suite.T().Run(test.name, func(t *testing.T) {
-			sel := exchangeBackupCreateSelectors(test.all, test.user, test.data)
+			sel := exchangeExchangeBackupCreateSelectors(test.all, test.user, test.data)
 			assert.Equal(t, test.expectIncludeLen, len(sel.Includes))
 		})
 	}
