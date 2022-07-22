5bd420c8af9df42a8416d25af29835995601358d
Danny <danny@alcion.ai>
2022-07-22 17:02:13 -0400

    Update src/internal/connector/disconnected_test.go
    testing change
    
    Co-authored-by: Keepers <104464746+ryanfkeepers@users.noreply.github.com>

--- src/internal/connector/disconnected_test.go
+++ src/internal/connector/disconnected_test.go
@@ -145,7 +145,7 @@ func (suite *DisconnectedGraphConnectorSuite) TestGraphConnector_ErrorChecking()
 		suite.T().Run(test.name, func(t *testing.T) {
 			recoverable := IsRecoverableError(test.err)
 			nonRecoverable := IsNonRecoverableError(test.err)
-			test.returnRecoverable(suite.T(), recoverable, "Test: %s Recoverable-received %v", test.name, recoverable)
+			test.returnRecoverable(t, recoverable, "Test: %s Recoverable-received %v", test.name, recoverable)
 			test.returnNonRecoverable(suite.T(), nonRecoverable, "Test: %s non-recoverable: %v", test.name, nonRecoverable)
 		})
 	}
