7c5ced6b9e849b6cdde7b45262139af3c0ae28d0
Danny Adams <da469@cornell.edu>
2022-07-26 18:30:17 -0400

    Update to collection object naming convention and comments

--- src/internal/data/data_collection.go
+++ src/internal/data/data_collection.go
@@ -6,15 +6,14 @@ import (
 	"github.com/alcionai/corso/pkg/backup/details"
 )
 
-
-// A DataCollection represents a collection of data of the
-// same type (e.g. mail)
-type DataCollection interface {
+// A Collection represents a compilation of data from the
+// same type application (e.g. mail)
+type Collection interface {
 	// Items returns a channel from which items in the collection can be read.
 	// Each returned struct contains the next item in the collection
 	// The channel is closed when there are no more items in the collection or if
 	// an unrecoverable error caused an early termination in the sender.
-	Items() <-chan DataStream
+	Items() <-chan Stream
 	// FullPath returns a slice of strings that act as metadata tags for this
 	// DataCollection. Returned items should be ordered from most generic to least
 	// generic. For example, a DataCollection for emails from a specific user
@@ -24,7 +23,7 @@ type DataCollection interface {
 
 // DataStream represents a single item within a DataCollection
 // that can be consumed as a stream (it embeds io.Reader)
-type DataStream interface {
+type Stream interface {
 	// ToReader returns an io.Reader for the DataStream
 	ToReader() io.ReadCloser
 	// UUID provides a unique identifier for this data
@@ -33,6 +32,6 @@ type DataStream interface {
 
 // DataStreamInfo is used to provide service specific
 // information about the DataStream
-type DataStreamInfo interface {
+type StreamInfo interface {
 	Info() details.ItemInfo
 }
