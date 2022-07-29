46f4fda7b76bccd4e4f7f02f9b6fe7d1190589cd
Danny Adams <da469@cornell.edu>
2022-07-29 10:01:03 -0400

    First steps for initialization to be created with collection.Items()

--- src/internal/connector/exchange/exchange_data_collection.go
+++ src/internal/connector/exchange/exchange_data_collection.go
@@ -41,6 +41,8 @@ type Collection struct {
 	// is desired to be sent through the data channel for eventual storage
 	jobs []string
 
+	service graph.Service
+	status  chan<- *support.ConnectorOperationStatus
 	// FullPath is the slice representation of the action context passed down through the hierarchy.
 	//The original request can be gleaned from the slice. (e.g. {<tenant ID>, <user ID>, "emails"})
 	fullPath []string
@@ -65,6 +67,18 @@ func (eoc *Collection) AddJob(objID string) {
 // PopulateCollection TODO: remove after async functionilty completed
 func (eoc *Collection) PopulateCollection(newData *Stream) {
 	eoc.data <- newData
+}
+
+// Items utility function to asynchronously execute process to fill data channel with
+// M365 exchange objects and returns the data channel
+func (eoc *Collection) Items() <-chan data.Stream {
+
+	go PopulateFromCollection(context.TODO(), eoc.service, make(map[string]*Collection, 0), eoc.status) // PlaceHolder until PR #441 approved
+	return eoc.data
+}
+
+func (edc *Collection) FullPath() []string {
+	return append([]string{}, edc.fullPath...)
 }
 
 // populateFromTaskList async call to fill DataCollection via channel implementation
@@ -159,14 +173,6 @@ func messageToDataCollection(
 	return nil
 }
 
-func (eoc *Collection) Items() <-chan data.Stream {
-	return eoc.data
-}
-
-func (edc *Collection) FullPath() []string {
-	return append([]string{}, edc.fullPath...)
-}
-
 // Stream represents a single item retrieved from exchange
 type Stream struct {
 	id string
@@ -189,6 +195,8 @@ func (od *Stream) ToReader() io.ReadCloser {
 func (od *Stream) Info() details.ItemInfo {
 	return details.ItemInfo{Exchange: od.info}
 }
+
+// NewStream constructor for exchange.Stream object
 func NewStream(identifier string, bytes []byte, detail details.ExchangeInfo) Stream {
 	return Stream{
 		id:      identifier,
