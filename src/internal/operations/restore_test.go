7c62939c85a26aa8ddc38db09df459a96a04e848
Keepers <104464746+ryanfkeepers@users.noreply.github.com>
2022-07-29 15:09:15 -0600

    update GC's restore to use AwaitStatus (#451)
    GC's restore funcs update their status field differently than
    the backup funcs.  This brings the behavior in line.

--- src/internal/operations/restore_test.go
+++ src/internal/operations/restore_test.go
@@ -59,13 +59,13 @@ func (suite *RestoreOpSuite) TestRestoreOperation_PersistResults() {
 
 	op.persistResults(now, &stats)
 
-	assert.Equal(t, op.Status, Failed)
-	assert.Equal(t, op.Results.ItemsRead, len(stats.cs))
-	assert.Equal(t, op.Results.ReadErrors, stats.readErr)
-	assert.Equal(t, op.Results.ItemsWritten, stats.gc.ObjectCount)
-	assert.Equal(t, op.Results.WriteErrors, stats.writeErr)
-	assert.Equal(t, op.Results.StartedAt, now)
-	assert.Less(t, now, op.Results.CompletedAt)
+	assert.Equal(t, op.Status, Failed, "status")
+	assert.Equal(t, op.Results.ItemsRead, len(stats.cs), "items read")
+	assert.Equal(t, op.Results.ReadErrors, stats.readErr, "read errors")
+	assert.Equal(t, op.Results.ItemsWritten, stats.gc.Successful, "items written")
+	assert.Equal(t, op.Results.WriteErrors, stats.writeErr, "write errors")
+	assert.Equal(t, op.Results.StartedAt, now, "started at")
+	assert.Less(t, now, op.Results.CompletedAt, "completed at")
 }
 
 // ---------------------------------------------------------------------------
