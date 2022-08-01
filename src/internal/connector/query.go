890c8f2b7f28fc33579fd158d4e52875b227c497
Danny <danny@alcion.ai>
2022-08-01 13:27:30 -0400

    Exchange service: Abstractions (#450)
    - Creation of exchange service-based query file. Test package expanded for support

--- src/internal/connector/query.go
+++ src/internal/connector/query.go
@@ -1,9 +1,6 @@
 package connector
 
 import (
-	"errors"
-
-	msfolder "github.com/microsoftgraph/msgraph-sdk-go/users/item/mailfolders"
 	msmessage "github.com/microsoftgraph/msgraph-sdk-go/users/item/messages"
 )
 
@@ -28,24 +25,6 @@ func Contains(elems []string, value string) bool {
 	return false
 }
 
-// optionsForMailFolders creates transforms the 'select' into a more dynamic call for MailFolders.
-// var moreOps is a []string of options(e.g. "displayName", "isHidden")
-// return is first call in MailFolders().GetWithRequestConfigurationAndResponseHandler(options, handler)
-func optionsForMailFolders(moreOps []string) (*msfolder.MailFoldersRequestBuilderGetRequestConfiguration, error) {
-	selecting, err := buildOptions(moreOps, folders)
-	if err != nil {
-		return nil, err
-	}
-
-	requestParameters := &msfolder.MailFoldersRequestBuilderGetQueryParameters{
-		Select: selecting,
-	}
-	options := &msfolder.MailFoldersRequestBuilderGetRequestConfiguration{
-		QueryParameters: requestParameters,
-	}
-	return options, nil
-}
-
 func optionsForMessageSnapshot() *msmessage.MessagesRequestBuilderGetRequestConfiguration {
 	selecting := []string{"id", "parentFolderId"}
 	options := &msmessage.MessagesRequestBuilderGetRequestConfiguration{
@@ -54,50 +33,4 @@ func optionsForMessageSnapshot() *msmessage.MessagesRequestBuilderGetRequestConf
 		},
 	}
 	return options
-}
-
-func optionsForMessages(moreOps []string) (*msmessage.MessagesRequestBuilderGetRequestConfiguration, error) {
-	selecting, err := buildOptions(moreOps, messages)
-	if err != nil {
-		return nil, err
-	}
-	requestParameters := &msmessage.MessagesRequestBuilderGetQueryParameters{
-		Select: selecting,
-	}
-	options := &msmessage.MessagesRequestBuilderGetRequestConfiguration{
-		QueryParameters: requestParameters,
-	}
-	return options, nil
-}
-
-// CheckOptions Utility Method for verifying if select options are valid the m365 object type
-// returns a list of valid options
-func buildOptions(options []string, selection optionIdentifier) ([]string, error) {
-	var allowedOptions []string
-
-	fieldsForFolders := []string{"displayName", "isHidden", "parentFolderId", "totalItemCount"}
-	fieldsForUsers := []string{"birthday", "businessPhones", "city", "companyName", "department", "displayName", "employeeId"}
-	fieldsForMessages := []string{"conservationId", "conversationIndex", "parentFolderId", "subject", "webLink"}
-	returnedOptions := []string{"id"}
-
-	switch selection {
-	case folders:
-		allowedOptions = fieldsForFolders
-	case users:
-		allowedOptions = fieldsForUsers
-	case messages:
-		allowedOptions = fieldsForMessages
-	default:
-		return nil, errors.New("unsupported option")
-	}
-
-	for _, entry := range options {
-		result := Contains(allowedOptions, entry)
-		if result {
-			returnedOptions = append(returnedOptions, entry)
-		} else {
-			return nil, errors.New("unsupported option")
-		}
-	}
-	return returnedOptions, nil
 }
