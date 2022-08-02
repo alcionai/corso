3b7fe55c3b93099613d818b800aae879f3cb0093
Danny Adams <da469@cornell.edu>
2022-08-02 12:33:41 -0400

    Removal of internal  methods

--- src/internal/connector/exchange/exchange_data_collection_test.go
+++ src/internal/connector/exchange/exchange_data_collection_test.go
@@ -12,15 +12,6 @@ type ExchangeDataCollectionSuite struct {
 	suite.Suite
 }
 
-func contains(elems []string, value string) bool {
-	for _, s := range elems {
-		if value == s {
-			return true
-		}
-	}
-	return false
-}
-
 func TestExchangeDataCollectionSuite(t *testing.T) {
 	suite.Run(t, new(ExchangeDataCollectionSuite))
 }
@@ -64,9 +55,9 @@ func (suite *ExchangeDataCollectionSuite) TestExchangeDataCollection_NewExchange
 		fullPath: []string{"Directory", "File", "task"},
 	}
 	suite.Equal(name, edc.user)
-	suite.True(contains(edc.FullPath(), "Directory"))
-	suite.True(contains(edc.FullPath(), "File"))
-	suite.True(contains(edc.FullPath(), "task"))
+	suite.Contains(edc.FullPath(), "Directory")
+	suite.Contains(edc.FullPath(), "File")
+	suite.Contains(edc.FullPath(), "task")
 }
 
 func (suite *ExchangeDataCollectionSuite) TestExchangeCollection_AddJob() {
