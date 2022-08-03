0c8f88e64b4d246d87be20fb01a52bcaf1abc8e2
Danny Adams <da469@cornell.edu>
2022-08-03 14:30:52 -0400

    Exchange Service tests extended.

--- src/internal/connector/exchange/exchange_service_test.go
+++ src/internal/connector/exchange/exchange_service_test.go
@@ -4,15 +4,41 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+
+	"github.com/alcionai/corso/internal/tester"
 )
 
 type ExchangeServiceSuite struct {
 	suite.Suite
+	es *exchangeService
 }
 
 func TestExchangeServiceSuite(t *testing.T) {
+	if err := tester.RunOnAny(
+		tester.CorsoCITests,
+		tester.CorsoGraphConnectorTests,
+	); err != nil {
+		t.Skip(err)
+	}
 	suite.Run(t, new(ExchangeServiceSuite))
+}
+
+func (suite *ExchangeServiceSuite) SetupSuite() {
+	if err := tester.RunOnAny(tester.CorsoCITests); err != nil {
+		suite.T().Skip(err)
+	}
+	_, err := tester.GetRequiredEnvVars(tester.M365AcctCredEnvs...)
+	require.NoError(suite.T(), err)
+
+	a, err := tester.NewM365Account()
+	require.NoError(suite.T(), err)
+	m365, err := a.M365Config()
+	require.NoError(suite.T(), err)
+	service, err := createService(m365, false)
+	require.NoError(suite.T(), err)
+	suite.es = service
 }
 
 // TestExchangeService_optionsForMessages checks to ensure approved query
@@ -51,10 +77,10 @@ func (suite *ExchangeServiceSuite) TestExchangeService_optionsForMessages() {
 	}
 }
 
-// TestExchangeService_optionsForFolders ensures that approved query options
+// TestExchangeService_optionsForFoldersAndContacts ensures that approved query options
 // are added to the RequestBuildConfiguration. Expected will always be +1
 // on than the input as "id" are always included within the select parameters
-func (suite *ExchangeServiceSuite) TestExchangeService_optionsForFolders() {
+func (suite *ExchangeServiceSuite) TestExchangeService_optionsForFoldersAndContacts() {
 	tests := []struct {
 		name       string
 		params     []string
@@ -86,8 +112,43 @@ func (suite *ExchangeServiceSuite) TestExchangeService_optionsForFolders() {
 			if err == nil {
 				suite.Equal(test.expected, len(config.QueryParameters.Select))
 			}
+			options, err := optionsForMailContacts(test.params)
+			test.checkError(t, err)
+			if err == nil {
+				suite.Equal(test.expected, len(options.QueryParameters.Select))
+			}
 		})
 
 	}
 
+}
+
+// TestExchangeService_GraphQueryFunctions verifies if Query functions APIs
+// through Microsoft Graph are functional
+func (suite *ExchangeServiceSuite) TestExchangeService_GraphQueryFunctions() {
+	userID, err := tester.M365UserID()
+	require.NoError(suite.T(), err)
+	tests := []struct {
+		name     string
+		params   []string
+		function GraphQuery
+	}{
+		{
+			name:     "GraphQuery: Get All Messages For User",
+			params:   []string{userID},
+			function: GetAllMessagesForUser,
+		},
+		{
+			name:     "GraphQuery: Get All Contacts For User",
+			params:   []string{userID},
+			function: GetAllContactsForUser,
+		},
+	}
+	for _, test := range tests {
+		suite.T().Run(test.name, func(t *testing.T) {
+			response, err := test.function(suite.es, test.params)
+			assert.NoError(t, err)
+			assert.NotNil(t, response)
+		})
+	}
 }
