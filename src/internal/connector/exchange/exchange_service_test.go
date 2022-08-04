74c8cc6396b644cdecbfdc1c2369c4eb8dab5751
Danny Adams <da469@cornell.edu>
2022-08-04 02:20:49 -0400

    Setup test added to package.

--- src/internal/connector/exchange/exchange_service_test.go
+++ src/internal/connector/exchange/exchange_service_test.go
@@ -3,7 +3,10 @@ package exchange
 import (
 	"testing"
 
+	"github.com/alcionai/corso/internal/tester"
+	"github.com/alcionai/corso/pkg/selectors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -90,4 +93,31 @@ func (suite *ExchangeServiceSuite) TestExchangeService_optionsForFolders() {
 
 	}
 
+}
+
+func (suite *ExchangeServiceSuite) TestExchangeService_SetupExchangeCollection() {
+
+	userID, err := tester.M365UserID()
+	require.NoError(suite.T(), err)
+	sel := selectors.NewExchangeBackup()
+	sel.Include(sel.Users([]string{userID}))
+	eb, err := sel.ToExchangeBackup()
+	require.NoError(suite.T(), err)
+	scopes := eb.Scopes()
+
+	for _, test := range scopes {
+		suite.T().Run(test.Category().String(), func(t *testing.T) {
+			discriminateFunc, graphQuery, iterFunc, err := SetupExchangeCollectionVars(test)
+			if test.Category() == selectors.ExchangeMailFolder ||
+				test.Category() == selectors.ExchangeContactFolder {
+				assert.NoError(t, err)
+				assert.NotNil(t, discriminateFunc)
+				assert.NotNil(t, graphQuery)
+				assert.NotNil(t, iterFunc)
+
+			}
+
+		})
+
+	}
 }
