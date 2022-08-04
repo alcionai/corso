342dd2e9f9997ddf55c8f686f7f849c1c11b1943
Keepers <ryanfkeepers@gmail.com>
2022-08-04 11:15:13 -0600

    set up CLI for integration testing (#478)
    Makes the necessary changes, including adding helper
    funcs, to bring the CLI up to an integration-testable
    state.  The changes made in this commit should be
    sufficient for most other CLI tests.  Includes a single
    test as verification.

---
+++ src/internal/tester/cli.go
@@ -0,0 +1,36 @@
+package tester
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+
+	"github.com/alcionai/corso/internal/common"
+)
+
+// StubRootCmd builds a stub cobra command to be used as
+// the root command for integration testing on the CLI
+func StubRootCmd(args ...string) *cobra.Command {
+	id := uuid.NewString()
+	now := common.FormatTime(time.Now())
+	cmdArg := "testing-corso"
+	c := &cobra.Command{
+		Use:   cmdArg,
+		Short: id,
+		Long:  id + " - " + now,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintf(cmd.OutOrStdout(), "test command args: %+v", args)
+			return nil
+		},
+	}
+	c.SetArgs(args)
+	return c
+}
+
+func NewContext() context.Context {
+	type stub struct{}
+	return context.WithValue(context.Background(), stub{}, stub{})
+}
