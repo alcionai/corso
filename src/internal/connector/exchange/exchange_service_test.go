91d2202cf72d6b6d4f706a92ce7c446f4b09e252
Danny Adams <da469@cornell.edu>
2022-08-04 02:39:57 -0400

    Import library order changed.

--- src/internal/connector/exchange/exchange_service_test.go
+++ src/internal/connector/exchange/exchange_service_test.go
@@ -3,11 +3,12 @@ package exchange
 import (
 	"testing"
 
-	"github.com/alcionai/corso/internal/tester"
-	"github.com/alcionai/corso/pkg/selectors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+
+	"github.com/alcionai/corso/internal/tester"
+	"github.com/alcionai/corso/pkg/selectors"
 )
 
 type ExchangeServiceSuite struct {
