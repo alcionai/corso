a2b76c118cc8a849f8d0843dca5ee2b5e815f30e
ryanfkeepers <ryanfkeepers@gmail.com>
2022-08-15 17:10:36 -0600

    refactor scope filtering
    Scope filtering is currently hardcoded to the exchange
    use case.  In order for future work to rely on boilerplate
    rather than re-writing the full filtering logic on each new
    type, as much of that code as is possible has been moved
    into a generic toolset.

--- src/pkg/selectors/scopes.go
+++ src/pkg/selectors/scopes.go
@@ -110,7 +110,7 @@ type (
 // funcs
 // ---------------------------------------------------------------------------
 
-// contains returns true if the category is included in the scope's
+// Contains returns true if the category is included in the scope's
 // data type, and the target string is included in the scope.
 func contains[T scopeT](s T, cat categorizer, target string) bool {
 	if !s.categorizer().includesType(cat) {
@@ -160,7 +160,6 @@ func isAnyTarget[T scopeT](s T, cat categorizer) bool {
 
 // reduce filters the entries in the details to only those that match the
 // inclusions, filters, and exclusions in the selector.
-//
 func reduce[T scopeT, C categoryT](
 	deets *details.Details,
 	s Selector,
