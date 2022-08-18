3e6088995a251dbc21f2dbe570ba3e34aa097d30
Danny Adams <da469@cornell.edu>
2022-08-18 15:23:35 -0400

    Addition of Mockdata test.

--- src/internal/connector/exchange/event_test.go
+++ src/internal/connector/exchange/event_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/alcionai/corso/internal/common"
+	"github.com/alcionai/corso/internal/connector/mockconnector"
+	"github.com/alcionai/corso/internal/connector/support"
 	"github.com/alcionai/corso/pkg/backup/details"
 )
 
@@ -24,7 +26,9 @@ func TestEventSuite(t *testing.T) {
 // can be properly retrieved from a models.Eventable object
 func (suite *EventSuite) TestEventInfo() {
 	initial := time.Now()
+
 	now := initial.Format(common.StandardTimeFormat)
+	suite.T().Logf("Initial: %v\nFormatted: %v\n", initial, now)
 	tests := []struct {
 		name     string
 		evtAndRP func() (models.Eventable, *details.ExchangeInfo)
@@ -56,11 +60,40 @@ func (suite *EventSuite) TestEventInfo() {
 				return event, &details.ExchangeInfo{Subject: subject}
 			},
 		},
+		{
+			name: "Using mockable",
+			evtAndRP: func() (models.Eventable, *details.ExchangeInfo) {
+				bytes := mockconnector.GetMockEventBytes("Test Mock")
+				event, err := support.CreateEventFromBytes(bytes)
+				require.NoError(suite.T(), err)
+				subject := " Test MockReview + Lunch"
+				sender := "foobar3@8qzvrj.onmicrosoft.com"
+				eventTime := time.Date(2022, time.April, 28, 3, 41, 58, 0, time.UTC)
+				return event, &details.ExchangeInfo{
+					Subject:    subject,
+					Sender:     sender,
+					EventStart: eventTime,
+				}
+
+			},
+		},
 	}
 	for _, test := range tests {
 		suite.T().Run(test.name, func(t *testing.T) {
 			event, expected := test.evtAndRP()
-			suite.Equal(expected, EventInfo(event))
+			result := EventInfo(event)
+			suite.Equal(expected.Subject, result.Subject)
+			suite.Equal(expected.Sender, result.Sender)
+			e_yr, e_mon, e_day := expected.EventStart.Date()
+			e_hr, e_min, e_sec := expected.EventStart.Clock()
+			r_yr, r_mon, r_day := result.EventStart.Date()
+			r_hr, r_min, r_sec := result.EventStart.Clock()
+			suite.Equal(e_yr, r_yr)
+			suite.Equal(e_mon, r_mon)
+			suite.Equal(e_day, r_day)
+			suite.Equal(e_hr, r_hr)
+			suite.Equal(e_min, r_min)
+			suite.Equal(e_sec, r_sec)
 		})
 	}
 }
