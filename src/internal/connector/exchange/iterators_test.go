5c14225bb6200bb542a2716631d42ad87bcb2db5
Danny Adams <da469@cornell.edu>
2022-08-26 15:48:47 -0400

    Added test suite to test iterator functions.

---
+++ src/internal/connector/exchange/iterators_test.go
@@ -0,0 +1,41 @@
+package exchange
+
+import (
+	"testing"
+
+	"github.com/alcionai/corso/internal/connector/mockconnector"
+	"github.com/alcionai/corso/internal/connector/support"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type ExchangeIteratorSuite struct {
+	suite.Suite
+}
+
+func TestExchangeIteratorSuite(t *testing.T) {
+	suite.Run(t, new(ExchangeIteratorSuite))
+}
+
+func (suite *ExchangeIteratorSuite) TestDisplayable() {
+	t := suite.T()
+	bytes := mockconnector.GetMockContactBytes("Displayable")
+	contact, err := support.CreateContactFromBytes(bytes)
+	require.NoError(t, err)
+	aDisplayable, ok := contact.(displayable)
+	assert.True(t, ok)
+	assert.NotNil(t, aDisplayable.GetId())
+	assert.NotNil(t, aDisplayable.GetDisplayName())
+}
+
+func (suite *ExchangeIteratorSuite) TestDescendable() {
+	t := suite.T()
+	bytes := mockconnector.GetMockMessageBytes("Descendable")
+	message, err := support.CreateMessageFromBytes(bytes)
+	require.NoError(t, err)
+	aDescendable, ok := message.(descendable)
+	assert.True(t, ok)
+	assert.NotNil(t, aDescendable.GetId())
+	assert.NotNil(t, aDescendable.GetParentFolderId())
+}
