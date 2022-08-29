ae187f4281aeab07795c7f64bc9a15706130616a
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-08-29 15:36:57 -0700

    Refine comments and receiver names

--- src/internal/path/resource_path.go
+++ src/internal/path/resource_path.go
@@ -17,19 +17,17 @@ const (
 )
 
 // dataLayerResourcePath allows callers to extract information from a
-// resource-specific path. This struct is package-private so that callers are
+// resource-specific path. This struct is unexported so that callers are
 // forced to use the pre-defined constructors, making it impossible to create a
 // dataLayerResourcePath with invalid service/category combinations.
 //
 // All dataLayerResourcePaths start with the same prefix:
-// <tenant ID>/<service>/<owner ID> which allows extracting high-level
-// information from the path. The prefix may additionally contain a <category>
-// after the owner ID if the service has multiple disjoint data sets (e.x.
-// Exchange has email and contacts). The path elements after this prefix
-// represent zero or more folders the path refers to and possibly an item ID if
-// the path refers to an individual item. A valid dataLayerResourcePath must
-// have at least one folder or an item so that the resulting path has at least
-// on element after the prefix.
+// <tenant ID>/<service>/<resource owner ID>/<category>
+// which allows extracting high-level information from the path. The path
+// elements after this prefix represent zero or more folders and, if the path
+// refers to a file or item, an item ID. A valid dataLayerResourcePath must have
+// at least one folder or an item so that the resulting path has at least one
+// element after the prefix.
 type dataLayerResourcePath struct {
 	Builder
 	category CategoryType
@@ -38,48 +36,42 @@ type dataLayerResourcePath struct {
 }
 
 // Tenant returns the tenant ID embedded in the dataLayerResourcePath.
-func (dlrp dataLayerResourcePath) Tenant() string {
-	return dlrp.Builder.elements[0]
+func (rp dataLayerResourcePath) Tenant() string {
+	return rp.Builder.elements[0]
 }
 
 // Service returns the ServiceType embedded in the dataLayerResourcePath.
-func (dlrp dataLayerResourcePath) Service() ServiceType {
-	return dlrp.service
+func (rp dataLayerResourcePath) Service() ServiceType {
+	return rp.service
 }
 
 // Category returns the CategoryType embedded in the dataLayerResourcePath.
-func (dlrp dataLayerResourcePath) Category() CategoryType {
-	return dlrp.category
+func (rp dataLayerResourcePath) Category() CategoryType {
+	return rp.category
 }
 
 // ResourceOwner returns the user ID or group ID embedded in the
 // dataLayerResourcePath.
-func (dlrp dataLayerResourcePath) ResourceOwner() string {
-	return dlrp.Builder.elements[2]
+func (rp dataLayerResourcePath) ResourceOwner() string {
+	return rp.Builder.elements[2]
 }
 
 // Folder returns the folder segment embedded in the dataLayerResourcePath.
-func (dlrp dataLayerResourcePath) Folder() string {
-	startIdx := 3
+func (rp dataLayerResourcePath) Folder() string {
+	endIdx := len(rp.Builder.elements)
 
-	if dlrp.category != UnknownCategory {
-		startIdx++
-	}
-
-	endIdx := len(dlrp.Builder.elements)
-
-	if dlrp.hasItem {
+	if rp.hasItem {
 		endIdx--
 	}
 
-	return dlrp.Builder.join(startIdx, endIdx)
+	return rp.Builder.join(4, endIdx)
 }
 
 // Item returns the item embedded in the dataLayerResourcePath if the path
 // refers to an item.
-func (dlrp dataLayerResourcePath) Item() string {
-	if dlrp.hasItem {
-		return dlrp.Builder.elements[len(dlrp.Builder.elements)-1]
+func (rp dataLayerResourcePath) Item() string {
+	if rp.hasItem {
+		return rp.Builder.elements[len(rp.Builder.elements)-1]
 	}
 
 	return ""
