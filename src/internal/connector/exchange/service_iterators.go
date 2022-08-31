fbc89220d42a53a919424ccdfcd03034f65ee585
Danny Adams <da469@cornell.edu>
2022-08-31 11:52:47 -0400

    GOFMT: WSL fix

--- src/internal/connector/exchange/service_iterators.go
+++ src/internal/connector/exchange/service_iterators.go
@@ -313,8 +313,9 @@ func iterateFindFolderID(
 	return func(entry any) bool {
 		if isCalendar {
 			entry = CreateCalendarDisplayable(entry)
+
 			if entry == nil {
-			  return true
+				return true
 			}
 		}
 
