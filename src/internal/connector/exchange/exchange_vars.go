b27041315bb5dc4a3de99546dc7e747145c66443
Danny Adams <da469@cornell.edu>
2022-09-12 22:22:02 -0400

    Package level variable file created for constants and interfaces.

---
+++ src/internal/connector/exchange/exchange_vars.go
@@ -0,0 +1,11 @@
+package exchange
+
+// exchange_vars.go is package level collection of interfaces and
+// constants that are used within the exchange.
+
+const(
+    	// RestorePropertyTag defined:
+	// https://docs.microsoft.com/en-us/office/client-developer/outlook/mapi/pidtagmessageflags-canonical-property
+	RestorePropertyTag          = "Integer 0x0E07"
+	RestoreCanonicalEnableValue = "4"
+    )
