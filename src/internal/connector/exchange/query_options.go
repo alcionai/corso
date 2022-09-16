4fe6d14fa44c7f534f7ea402873c0551bbebc434
Danny Adams <da469@cornell.edu>
2022-09-16 14:01:13 -0400

    Adds functionality to transform Exchange.Scope to optionIdentifier.

--- src/internal/connector/exchange/query_options.go
+++ src/internal/connector/exchange/query_options.go
@@ -16,6 +16,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/alcionai/corso/src/internal/path"
+	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
 //-----------------------------------------------------------------------
@@ -109,6 +110,19 @@ func categoryToOptionIdentifier(category path.CategoryType) optionIdentifier {
 	case path.ContactsCategory:
 		return contacts
 	case path.EventsCategory:
+		return events
+	default:
+		return unknown
+	}
+}
+
+func selectorToOptionIdentifier(selector selectors.ExchangeScope) optionIdentifier {
+	switch selector.Category() {
+	case selectors.ExchangeMailFolder, selectors.ExchangeMail:
+		return messages
+	case selectors.ExchangeContactFolder, selectors.ExchangeContact:
+		return contacts
+	case selectors.ExchangeEventCalendar, selectors.ExchangeEvent:
 		return events
 	default:
 		return unknown
