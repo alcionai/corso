23a279c2149f3e4a116d7817d2876102ab7eae63
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-09-19 09:00:40 -0700

    Bulk up tests slightly
    * Check for empty and nil strings
    * Check that encoded values don't contain '/' (this should really be
      fuzzed though

--- src/internal/kopia/path_encoder_test.go
+++ src/internal/kopia/path_encoder_test.go
@@ -26,6 +26,8 @@ func (suite *PathEncoderSuite) TestEncodeDecode() {
 	decoded := make([]string, 0, len(elements))
 
 	for _, e := range encoded {
+		// Encoded values should not contain '/'.
+		assert.False(t, strings.ContainsRune(e, '/'))
 		dec, err := decodeElement(e)
 		require.NoError(t, err)
 
@@ -39,14 +41,29 @@ func (suite *PathEncoderSuite) TestEncodeAsPathDecode() {
 	table := []struct {
 		name     string
 		elements []string
+		expected []string
 	}{
 		{
 			name:     "MultipleElements",
 			elements: []string{"these", "are", "some", "path", "elements"},
+			expected: []string{"these", "are", "some", "path", "elements"},
 		},
 		{
 			name:     "SingleElement",
 			elements: []string{"elements"},
+			expected: []string{"elements"},
+		},
+		{
+			name:     "EmptyPath",
+			elements: []string{""},
+			expected: []string{""},
+		},
+		{
+			name:     "NilPath",
+			elements: nil,
+			// Gets "" back because individual elements are decoded and "" is the 0
+			// value for the decoder.
+			expected: []string{""},
 		},
 	}
 
@@ -66,7 +83,7 @@ func (suite *PathEncoderSuite) TestEncodeAsPathDecode() {
 				decoded = append(decoded, dec)
 			}
 
-			assert.Equal(t, test.elements, decoded)
+			assert.Equal(t, test.expected, decoded)
 		})
 	}
 }
