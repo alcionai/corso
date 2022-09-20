766466329b803da7a19db3104eebe28c224023bb
ryanfkeepers <ryanfkeepers@gmail.com>
2022-09-20 15:24:40 -0600

    Count number of resources in a slice of colls
    Backup and Restore metrics need to know the
    count of resource owners in the operation.  This
    processor extracts that data from the collections
    that get passed from producer to consumer.

--- src/internal/data/data_collection_test.go
+++ src/internal/data/data_collection_test.go
@@ -3,13 +3,83 @@ package data
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+
+	"github.com/alcionai/corso/src/internal/path"
 )
+
+type mockColl struct {
+	p path.Path
+}
+
+func (mc mockColl) Items() <-chan Stream {
+	return nil
+}
+
+func (mc mockColl) FullPath() path.Path {
+	return mc.p
+}
 
 type CollectionSuite struct {
 	suite.Suite
 }
 
-func TestDataCollectionSuite(t *testing.T) {
+// ------------------------------------------------------------------------------------------------
+// tests
+// ------------------------------------------------------------------------------------------------
+
+func TestCollectionSuite(t *testing.T) {
 	suite.Run(t, new(CollectionSuite))
+}
+
+func (suite *CollectionSuite) TestResourceOwnerSet() {
+	t := suite.T()
+	toColl := func(t *testing.T, resource string) Collection {
+		p, err := path.Builder{}.
+			Append("foo").
+			ToDataLayerExchangePathForCategory("tid", resource, path.EventsCategory, false)
+		require.NoError(t, err)
+
+		return mockColl{p}
+	}
+
+	table := []struct {
+		name   string
+		input  []Collection
+		expect []string
+	}{
+		{
+			name:   "empty",
+			input:  []Collection{},
+			expect: []string{},
+		},
+		{
+			name:   "nil",
+			input:  nil,
+			expect: []string{},
+		},
+		{
+			name:   "single resource",
+			input:  []Collection{toColl(t, "fnords")},
+			expect: []string{"fnords"},
+		},
+		{
+			name:   "multiple resource",
+			input:  []Collection{toColl(t, "fnords"), toColl(t, "smarfs")},
+			expect: []string{"fnords", "smarfs"},
+		},
+		{
+			name:   "duplciate resources",
+			input:  []Collection{toColl(t, "fnords"), toColl(t, "smarfs"), toColl(t, "fnords")},
+			expect: []string{"fnords", "smarfs"},
+		},
+	}
+	for _, test := range table {
+		suite.T().Run(test.name, func(t *testing.T) {
+			rs := ResourceOwnerSet(test.input)
+			assert.Equal(t, test.expect, rs)
+		})
+	}
 }
