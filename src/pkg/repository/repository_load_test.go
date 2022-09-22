d633be9eef51676baacd70d40874ef80e25f1872
Keepers <ryanfkeepers@gmail.com>
2022-09-22 09:16:25 -0600

    nightly load test footprint and workflow (#928)
    ## Description
    
    Adds the footprint file for running nightly load
    tests out of the repository package.  The tests
    don't do anything at this stage, but that's okay.
    Also introduces a workflow to kick off the tests
    every day at 2am UTC.
    
    ## Type of change
    
    - [X] :robot: Test
    - [X] :computer: CI/Deployment
    
    ## Issue(s)
    
    * #902
    
    ## Test Plan
    
    - [X] :green_heart: E2E

---
+++ src/pkg/repository/repository_load_test.go
@@ -0,0 +1,147 @@
+package repository_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/alcionai/corso/src/internal/tester"
+	"github.com/alcionai/corso/src/pkg/account"
+	"github.com/alcionai/corso/src/pkg/control"
+	"github.com/alcionai/corso/src/pkg/logger"
+	"github.com/alcionai/corso/src/pkg/repository"
+	"github.com/alcionai/corso/src/pkg/storage"
+)
+
+func initM365Repo(t *testing.T) (
+	context.Context, *repository.Repository, account.Account, storage.Storage,
+) {
+	_, err := tester.GetRequiredEnvSls(
+		tester.AWSStorageCredEnvs,
+		tester.M365AcctCredEnvs,
+	)
+	require.NoError(t, err)
+
+	ctx := tester.NewContext()
+	st := tester.NewPrefixedS3Storage(t)
+	ac := tester.NewM365Account(t)
+	opts := control.Options{
+		DisableMetrics: true,
+		FailFast:       true,
+	}
+
+	repo, err := repository.Initialize(ctx, ac, st, opts)
+	require.NoError(t, err)
+
+	return ctx, repo, ac, st
+}
+
+// ------------------------------------------------------------------------------------------------
+// Exchange
+// ------------------------------------------------------------------------------------------------
+
+type RepositoryLoadTestExchangeSuite struct {
+	suite.Suite
+	ctx  context.Context
+	repo *repository.Repository
+	acct account.Account
+	st   storage.Storage
+}
+
+func TestRepositoryLoadTestExchangeSuite(t *testing.T) {
+	if err := tester.RunOnAny(tester.CorsoLoadTests); err != nil {
+		t.Skip(err)
+	}
+
+	suite.Run(t, new(RepositoryLoadTestExchangeSuite))
+}
+
+func (suite *RepositoryLoadTestExchangeSuite) SetupSuite() {
+	t := suite.T()
+	suite.ctx, suite.repo, suite.acct, suite.st = initM365Repo(t)
+}
+
+func (suite *RepositoryLoadTestExchangeSuite) TeardownSuite() {
+	suite.repo.Close(suite.ctx)
+}
+
+func (suite *RepositoryLoadTestExchangeSuite) SetupTest() {
+	suite.ctx, _ = logger.SeedLevel(context.Background(), logger.Development)
+}
+
+func (suite *RepositoryLoadTestExchangeSuite) TeardownTest() {
+	logger.Flush(suite.ctx)
+}
+
+func (suite *RepositoryLoadTestExchangeSuite) TestExchange() {
+	// var (
+	// 	t   = suite.T()
+	// 	ctx = context.Background()
+	// )
+
+	// t.parallel()
+
+	// backup
+
+	// list
+
+	// details
+
+	// restore
+}
+
+// ------------------------------------------------------------------------------------------------
+// OneDrive
+// ------------------------------------------------------------------------------------------------
+
+type RepositoryLoadTestOneDriveSuite struct {
+	suite.Suite
+	ctx  context.Context
+	repo *repository.Repository
+	acct account.Account
+	st   storage.Storage
+}
+
+func TestRepositoryLoadTestOneDriveSuite(t *testing.T) {
+	if err := tester.RunOnAny(tester.CorsoLoadTests); err != nil {
+		t.Skip(err)
+	}
+
+	suite.Run(t, new(RepositoryLoadTestOneDriveSuite))
+}
+
+func (suite *RepositoryLoadTestOneDriveSuite) SetupSuite() {
+	t := suite.T()
+	suite.ctx, suite.repo, suite.acct, suite.st = initM365Repo(t)
+}
+
+func (suite *RepositoryLoadTestOneDriveSuite) TeardownSuite() {
+	suite.repo.Close(suite.ctx)
+}
+
+func (suite *RepositoryLoadTestOneDriveSuite) SetupTest() {
+	suite.ctx, _ = logger.SeedLevel(context.Background(), logger.Development)
+}
+
+func (suite *RepositoryLoadTestOneDriveSuite) TeardownTest() {
+	logger.Flush(suite.ctx)
+}
+
+func (suite *RepositoryLoadTestOneDriveSuite) TestExchange() {
+	// var (
+	// 	t   = suite.T()
+	// 	ctx = context.Background()
+	// )
+
+	// t.parallel()
+
+	// backup
+
+	// list
+
+	// details
+
+	// restore
+}
