60e3be041f9cb9e1e82dc4b92f21fe15e5e89153
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-09-23 12:30:31 -0700

    Some name changes and comments

--- src/pkg/selectors/destination.go
+++ src/pkg/selectors/destination.go
@@ -1,8 +1,14 @@
 package selectors
 
-// RestoreDestination is a POD that contains the resource owner to restore data
-// under and the name of the root of the restored directory hierarchy.
+// RestoreDestination is a POD that contains an override of the resource owner
+// to restore data under and the name of the root of the restored container
+// hierarchy.
 type RestoreDestination struct {
-	ResourceOwner string
+	// ResourceOwnerOverride overrides the default resource owner to restore to.
+	// If it is not populated items should be restored under the previous resource
+	// owner of the item.
+	ResourceOwnerOverride string
+	// ContainerName is the name of the root of the restored container hierarchy.
+	// This field must be populated for a restore.
 	ContainerName string
 }
