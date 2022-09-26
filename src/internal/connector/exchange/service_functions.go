3be818f185be9b1d97c97cae6e994bf12e8ba0ff
ryanfkeepers <ryanfkeepers@gmail.com>
2022-09-26 14:59:59 -0600

    remove debug print

--- src/internal/connector/exchange/service_functions.go
+++ src/internal/connector/exchange/service_functions.go
@@ -239,7 +239,6 @@ func GetAllContactFolders(
 			return false
 		}
 
-		fmt.Printf("\n-----\nchecking inclusion %v %v\n-----\n", *folder.GetDisplayName(), nameContains)
 		include := len(nameContains) == 0 ||
 			(len(nameContains) > 0 && strings.Contains(*folder.GetDisplayName(), nameContains))
 		if include {
