56358ff1049e92f47047da72ce102239e0e43301
ryanfkeepers <ryanfkeepers@gmail.com>
2022-10-04 14:58:51 -0600

    add no-op to restore, clean up tests

--- src/internal/operations/restore_test.go
+++ src/internal/operations/restore_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 	"time"
 
-	multierror "github.com/hashicorp/go-multierror"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -38,56 +37,82 @@ func TestRestoreOpSuite(t *testing.T) {
 	suite.Run(t, new(RestoreOpSuite))
 }
 
-// TODO: after modelStore integration is added, mock the store and/or
-// move this to an integration test.
 func (suite *RestoreOpSuite) TestRestoreOperation_PersistResults() {
 	var (
-		t   = suite.T()
-		ctx = context.Background()
-
+		ctx  = context.Background()
 		kw   = &kopia.Wrapper{}
 		sw   = &store.Wrapper{}
 		acct = account.Account{}
 		now  = time.Now()
-		rs   = restoreStats{
-			started:       true,
-			readErr:       multierror.Append(nil, assert.AnError),
-			writeErr:      assert.AnError,
-			resourceCount: 1,
-			bytesRead: &stats.ByteCounter{
-				NumBytes: 42,
-			},
-			cs: []data.Collection{&exchange.Collection{}},
-			gc: &support.ConnectorOperationStatus{
-				ObjectCount: 1,
-			},
-		}
 		dest = control.DefaultRestoreDestination(common.SimpleDateTimeFormat)
 	)
 
-	op, err := NewRestoreOperation(
-		ctx,
-		control.Options{},
-		kw,
-		sw,
-		acct,
-		"foo",
-		selectors.Selector{},
-		dest,
-		evmock.NewBus())
-	require.NoError(t, err)
+	table := []struct {
+		expectStatus opStatus
+		expectErr    assert.ErrorAssertionFunc
+		stats        restoreStats
+	}{
+		{
+			expectStatus: Completed,
+			expectErr:    assert.NoError,
+			stats: restoreStats{
+				started:       true,
+				resourceCount: 1,
+				bytesRead: &stats.ByteCounter{
+					NumBytes: 42,
+				},
+				cs: []data.Collection{&exchange.Collection{}},
+				gc: &support.ConnectorOperationStatus{
+					ObjectCount: 1,
+				},
+			},
+		},
+		{
+			expectStatus: Failed,
+			expectErr:    assert.Error,
+			stats: restoreStats{
+				started:   false,
+				bytesRead: &stats.ByteCounter{},
+				gc:        &support.ConnectorOperationStatus{},
+			},
+		},
+		{
+			expectStatus: NoData,
+			expectErr:    assert.NoError,
+			stats: restoreStats{
+				started:   true,
+				bytesRead: &stats.ByteCounter{},
+				cs:        []data.Collection{},
+				gc:        &support.ConnectorOperationStatus{},
+			},
+		},
+	}
+	for _, test := range table {
+		suite.T().Run(test.expectStatus.String(), func(t *testing.T) {
+			op, err := NewRestoreOperation(
+				ctx,
+				control.Options{},
+				kw,
+				sw,
+				acct,
+				"foo",
+				selectors.Selector{},
+				dest,
+				evmock.NewBus())
+			require.NoError(t, err)
+			test.expectErr(t, op.persistResults(ctx, now, &test.stats))
 
-	require.NoError(t, op.persistResults(ctx, now, &rs))
-
-	assert.Equal(t, op.Status.String(), Completed.String(), "status")
-	assert.Equal(t, op.Results.ItemsRead, len(rs.cs), "items read")
-	assert.Equal(t, op.Results.ReadErrors, rs.readErr, "read errors")
-	assert.Equal(t, op.Results.ItemsWritten, rs.gc.Successful, "items written")
-	assert.Equal(t, rs.bytesRead.NumBytes, op.Results.BytesRead, "resource owners")
-	assert.Equal(t, rs.resourceCount, op.Results.ResourceOwners, "resource owners")
-	assert.Equal(t, op.Results.WriteErrors, rs.writeErr, "write errors")
-	assert.Equal(t, op.Results.StartedAt, now, "started at")
-	assert.Less(t, now, op.Results.CompletedAt, "completed at")
+			assert.Equal(t, test.expectStatus.String(), op.Status.String(), "status")
+			assert.Equal(t, len(test.stats.cs), op.Results.ItemsRead, "items read")
+			assert.Equal(t, test.stats.readErr, op.Results.ReadErrors, "read errors")
+			assert.Equal(t, test.stats.gc.Successful, op.Results.ItemsWritten, "items written")
+			assert.Equal(t, test.stats.bytesRead.NumBytes, op.Results.BytesRead, "resource owners")
+			assert.Equal(t, test.stats.resourceCount, op.Results.ResourceOwners, "resource owners")
+			assert.Equal(t, test.stats.writeErr, op.Results.WriteErrors, "write errors")
+			assert.Equal(t, now, op.Results.StartedAt, "started at")
+			assert.Less(t, now, op.Results.CompletedAt, "completed at")
+		})
+	}
 }
 
 // ---------------------------------------------------------------------------
