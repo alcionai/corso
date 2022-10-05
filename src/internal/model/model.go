030a0f558fff73d4b2e50c2f56485148502d56bb
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-10-05 15:25:12 -0700

    Don't store version in JSON
    Version is pulled from the tags in the ModelStore so there's no need to
    give it a JSON field. This save a few bytes per model stored.

--- src/internal/model/model.go
+++ src/internal/model/model.go
@@ -45,7 +45,7 @@ type BaseModel struct {
 	// field should be treated as read-only by users.
 	ModelStoreID manifest.ID `json:"-"`
 	// Version is a version number that can help track changes across models.
-	Version int
+	Version int `json:"-"`
 	// Tags associated with this model in the store to facilitate lookup. Tags in
 	// the struct are not serialized directly into the stored model, but are part
 	// of the metadata for the model.
