92a9cf4c2753adff4050035a513a7bfc30884449
Danny Adams <da469@cornell.edu>
2022-10-08 15:21:20 -0400

    Event Calendar Cache: Logic Additions for Populate and populateEventRoot added.

--- src/internal/connector/exchange/event_calendar_cache.go
+++ src/internal/connector/exchange/event_calendar_cache.go
@@ -3,9 +3,13 @@ package exchange
 import (
 	"context"
 
-	"github.com/alcionai/corso/src/internal/connector/graph"
-	"github.com/alcionai/corso/src/pkg/path"
+	msgraphgocore "github.com/microsoftgraph/msgraph-sdk-go-core"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/pkg/errors"
+
+	"github.com/alcionai/corso/src/internal/connector/graph"
+	"github.com/alcionai/corso/src/internal/connector/support"
+	"github.com/alcionai/corso/src/pkg/path"
 )
 
 var _ graph.ContainerResolver = &eventCalendarCache{}
@@ -21,8 +25,100 @@ func (ecc *eventCalendarCache) populateEventRoot(
 	directoryID string,
 	baseContainerPath []string,
 ) error {
-	//wantedOpts := []string{}
+	wantedOpts := []string{"name"}
+
+	opts, err := optionsForIndividualCalendar(wantedOpts)
+	if err != nil {
+		return errors.Wrapf(err, "getting options for event cache %v", wantedOpts)
+	}
+
+	cal, err := ecc.gs.
+		Client().
+		UsersById(ecc.userID).
+		CalendarsById(directoryID).
+		Get(ctx, opts)
+	if err != nil {
+		return errors.Wrap(err, "fetching default calendar "+support.ConnectorStackErrorTrace(err))
+	}
+
+	idPtr := cal.GetId()
+
+	if idPtr == nil || len(*idPtr) == 0 {
+		return errors.New("root calendar has no ID")
+	}
+
+	identifier := *idPtr
+	transform := CreateCalendarDisplayable(cal, identifier)
+	temp := eventCalendar{
+		Container: transform,
+		p:         path.Builder{}.Append(baseContainerPath...),
+	}
+
+	ecc.cache[identifier] = &temp
+	ecc.rootID = identifier
+
 	return nil
+}
+
+func (ecc *eventCalendarCache) Populate(
+	ctx context.Context,
+	baseID string,
+	baseContainerPath ...string,
+) error {
+	if err := ecc.init(ctx, baseID, baseContainerPath); err != nil {
+		return err
+	}
+
+	options, err := optionsForCalendars([]string{"name"})
+	if err != nil {
+		return err
+	}
+
+	directories := make(map[string]graph.Container)
+	errUpdater := func(s string, e error) {
+		err = support.WrapAndAppend(s, e, err)
+	}
+
+	query, err := ecc.gs.Client().UsersById(ecc.userID).Calendars().Get(ctx, options)
+	if err != nil {
+		return errors.Wrap(err, support.ConnectorStackErrorTrace(err))
+	}
+
+	iter, err := msgraphgocore.NewPageIterator(
+		query,
+		ecc.gs.Adapter(),
+		models.CreateCalendarCollectionResponseFromDiscriminatorValue,
+	)
+	if err != nil {
+		return err
+	}
+
+	cb := IterativeCollectCalendarContainers(
+		directories,
+		"",
+		ecc.rootID,
+		errUpdater,
+	)
+
+	iterateErr := iter.Iterate(ctx, cb)
+	if iterateErr != nil {
+		return iterateErr
+	}
+
+	if err != nil {
+		return err
+	}
+
+	for _, containerr := range directories {
+		if err := ecc.AddToCache(containerr); err != nil {
+			iterateErr = support.WrapAndAppend(
+				"failure adding "+*containerr.GetDisplayName(),
+				err,
+				iterateErr)
+		}
+	}
+
+	return iterateErr
 }
 
 func (ecc *eventCalendarCache) init(
@@ -69,7 +165,6 @@ func (ecc *eventCalendarCache) IDToPath(
 // AddToCache places container into internal cache field. For EventCalendars
 // this means that the object has to be transformed prior to calling
 // this function.
-// @see
 func (ecc *eventCalendarCache) AddToCache(f graph.Container) error {
 	if err := checkRequiredValues(f); err != nil {
 		return err
@@ -84,4 +179,22 @@ func (ecc *eventCalendarCache) AddToCache(f graph.Container) error {
 	}
 
 	return nil
+}
+
+func (ecc *eventCalendarCache) PathInCache(pathString string) (string, bool) {
+	if len(pathString) == 0 || ecc.cache == nil {
+		return "", false
+	}
+
+	for _, containerr := range ecc.cache {
+		if containerr.Path() == nil {
+			continue
+		}
+
+		if containerr.Path().String() == pathString {
+			return *containerr.GetId(), true
+		}
+	}
+
+	return "", false
 }
