eb2fa35b7f98a735a8314367066a52678dab4a08
ryanfkeepers <ryanfkeepers@gmail.com>
2022-10-10 15:34:09 -0600

    add progress display handler and api
    Adds a new package- Observe- for owning user-
    oriented displays like progress bars.  This PR adds
    an initial progress bar to onedrive backups as a
    proof-of-concept.  The API is more important than
    the specific progress bar package at this time.
    Future changes may opt for a different pkg.

---
+++ src/internal/observe/observe_test.go
@@ -0,0 +1,53 @@
+package observe_test
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/alcionai/corso/src/internal/observe"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type ObserveProgressUnitSuite struct {
+	suite.Suite
+}
+
+func TestObserveProgressUnitSuite(t *testing.T) {
+	suite.Run(t, new(ObserveProgressUnitSuite))
+}
+
+func (suite *ObserveProgressUnitSuite) TestDoesThings() {
+	t := suite.T()
+
+	recorder := strings.Builder{}
+	observe.SeedWriter(&recorder)
+
+	from := make([]byte, 100)
+	prog := observe.ItemProgress(
+		io.NopCloser(bytes.NewReader(from)),
+		"test",
+		100)
+	require.NotNil(t, prog)
+
+	for {
+		to := make([]byte, 25)
+		n, err := prog.Read(to)
+
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		assert.NoError(t, err)
+		assert.Less(t, 0, n)
+	}
+
+	recorded := recorder.String()
+	assert.Contains(t, recorded, "25%")
+	assert.Contains(t, recorded, "50%")
+	assert.Contains(t, recorded, "75%")
+}
