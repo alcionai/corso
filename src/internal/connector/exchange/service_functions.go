3cbeea8ffecb193142286925d7f5d1758a94d25b
Danny Adams <da469@cornell.edu>
2022-10-11 12:49:47 -0400

    Beginning data removal.

--- src/internal/connector/exchange/service_functions.go
+++ src/internal/connector/exchange/service_functions.go
@@ -233,6 +233,7 @@ func GetAllCalendars(ctx context.Context, gs graph.Service, user, nameContains s
 // https://github.com/alcionai/corso/issues/1122
 func GetAllContactFolders(
 	ctx context.Context,
+	qp graph.QueryParams,
 	gs graph.Service,
 	user, nameContains string,
 ) ([]graph.Container, error) {
@@ -244,7 +245,10 @@ func GetAllContactFolders(
 			errs = support.WrapAndAppend(s, e, errs)
 		}
 	)
-
+	
+	resolver := Populate
+	
+}
 	resp, err := GetAllContactFolderNamesForUser(ctx, gs, user)
 	if err != nil {
 		return nil, err
@@ -271,79 +275,6 @@ func GetAllContactFolders(
 	return containers, err
 }
 
-// GetContainerID query function to retrieve a container's M365 ID.
-// @param containerName is the target's name, user-readable and case sensitive
-// @param category switches query and iteration to support  multiple exchange applications
-// @returns a *string if the folder exists. If the folder does not exist returns nil, error-> folder not found
-func GetContainerID(
-	ctx context.Context,
-	service graph.Service,
-	containerName,
-	user string,
-	category optionIdentifier,
-) (*string, error) {
-	var (
-		errs       error
-		targetID   *string
-		query      GraphQuery
-		transform  absser.ParsableFactory
-		isCalendar bool
-		errUpdater = func(id string, err error) {
-			errs = support.WrapAndAppend(id, err, errs)
-		}
-	)
-
-	switch category {
-	case messages:
-		query = GetAllFolderNamesForUser
-		transform = models.CreateMailFolderCollectionResponseFromDiscriminatorValue
-	case contacts:
-		query = GetAllContactFolderNamesForUser
-		transform = models.CreateContactFolderCollectionResponseFromDiscriminatorValue
-	case events:
-		query = GetAllCalendarNamesForUser
-		transform = models.CreateCalendarCollectionResponseFromDiscriminatorValue
-		isCalendar = true
-	default:
-		return nil, fmt.Errorf("unsupported category %s for GetContainerID()", category)
-	}
-
-	response, err := query(ctx, service, user)
-	if err != nil {
-		return nil, errors.Wrapf(
-			err,
-			"user %s M365 query: %s",
-			user, support.ConnectorStackErrorTrace(err),
-		)
-	}
-
-	pageIterator, err := msgraphgocore.NewPageIterator(
-		response,
-		service.Adapter(),
-		transform,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	callbackFunc := iterateFindContainerID(
-		&targetID,
-		containerName,
-		service.Adapter().GetBaseUrl(),
-		isCalendar,
-		errUpdater,
-	)
-
-	if err := pageIterator.Iterate(ctx, callbackFunc); err != nil {
-		return nil, support.WrapAndAppend(service.Adapter().GetBaseUrl(), err, errs)
-	}
-
-	if targetID == nil {
-		return nil, ErrFolderNotFound
-	}
-
-	return targetID, errs
-}
 
 // SetupExchangeCollectionVars is a helper function returns a sets
 // Exchange.Type specific functions based on scope
@@ -388,7 +319,7 @@ func SetupExchangeCollectionVars(scope selectors.ExchangeScope) (
 // this category of data. If one is not available, returns nil so that other
 // logic in the caller can complete as long as they check if the resolver is not
 // nil. If an error occurs populating the resolver, returns an error.
-func maybeGetAndPopulateFolderResolver(
+func PopulateExchangeContainerResolver(
 	ctx context.Context,
 	qp graph.QueryParams,
 	category path.CategoryType,
@@ -399,9 +330,10 @@ func maybeGetAndPopulateFolderResolver(
 		service, err = createService(qp.Credentials, qp.FailFast)
 	)
 
-	if err != nil {
-		return nil, err
+	if err != nil{
+		return err
 	}
+
 
 	switch category {
 	case path.EmailCategory:
@@ -426,7 +358,7 @@ func maybeGetAndPopulateFolderResolver(
 		cacheRoot = DefaultCalendar
 
 	default:
-		return nil, nil
+		return nil, fmt.Errorf("ContainerResolver not present for %s type", category)
 	}
 
 	if err := res.Populate(ctx, cacheRoot); err != nil {
