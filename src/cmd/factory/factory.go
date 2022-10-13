be23e83f27eced86fe721125511f460508e16f32
ryanfkeepers <ryanfkeepers@gmail.com>
2022-10-13 12:31:05 -0600

    add script boilerplate for mock data generation
    Introduces a `factory` script to help devs generate
    fake m365 data in bulk for testing purposes.  Only
    boilerplate at this time.  Any TODO's or not-yet-
    implemented statuses are present as placeholders
    alone, without official deadlines.

---
+++ src/cmd/factory/factory.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	. "github.com/alcionai/corso/src/cli/print"
+)
+
+var factoryCmd = &cobra.Command{
+	Use:   "factory",
+	Short: "Generate all types of m365 folders",
+	RunE:  handleFactoryRoot,
+}
+
+var exchangeCmd = &cobra.Command{
+	Use:   "exchange",
+	Short: "Generate exchange data",
+	RunE:  handleExchangeFactory,
+}
+
+var oneDriveCmd = &cobra.Command{
+	Use:   "onedrive",
+	Short: "Generate onedrive data",
+	RunE:  handleOneDriveFactory,
+}
+
+var (
+	count     int
+	container string
+	tenant    string
+	user      string
+)
+
+var (
+	ErrGenerating       = errors.New("not all items were successfully generated")
+	ErrNotYetImplemeted = errors.New("not yet implemented")
+)
+
+// ------------------------------------------------------------------------------------------
+// CLI command handlers
+// ------------------------------------------------------------------------------------------
+
+func main() {
+	ctx := SetRootCmd(context.Background(), factoryCmd)
+
+	// persistent flags that are common to all use cases
+	fs := factoryCmd.PersistentFlags()
+	fs.StringVar(&tenant, "tenant", "", "m365 tenant containing the user")
+	fs.StringVar(&user, "user", "", "m365 user owning the new data")
+	cobra.CheckErr(factoryCmd.MarkPersistentFlagRequired("user"))
+	fs.IntVar(&count, "count", 0, "count of items to produce")
+	cobra.CheckErr(factoryCmd.MarkPersistentFlagRequired("count"))
+	fs.StringVar(&container, "container", "", "container location of the new data (will create as needed)")
+	cobra.CheckErr(factoryCmd.MarkPersistentFlagRequired("container"))
+
+	factoryCmd.AddCommand(exchangeCmd)
+	addExchangeCommands(exchangeCmd)
+	factoryCmd.AddCommand(oneDriveCmd)
+	addOneDriveCommands(oneDriveCmd)
+
+	if err := factoryCmd.ExecuteContext(ctx); err != nil {
+		os.Exit(1)
+	}
+}
+
+func handleFactoryRoot(cmd *cobra.Command, args []string) error {
+	Err(cmd.Context(), ErrNotYetImplemeted)
+	return cmd.Help()
+}
+
+func handleExchangeFactory(cmd *cobra.Command, args []string) error {
+	Err(cmd.Context(), ErrNotYetImplemeted)
+	return cmd.Help()
+}
+
+func handleOneDriveFactory(cmd *cobra.Command, args []string) error {
+	Err(cmd.Context(), ErrNotYetImplemeted)
+	return cmd.Help()
+}
