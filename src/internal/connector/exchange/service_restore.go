e399030bae4a57bc50a544da60232e8641133a32
Danny Adams <da469@cornell.edu>
2022-10-17 20:47:52 -0400

    Merge Fix: Use of graph package.

--- src/internal/connector/exchange/service_restore.go
+++ src/internal/connector/exchange/service_restore.go
@@ -576,7 +576,7 @@ func establishEventsRestoreLocation(
 			return "", errors.Wrap(err, "populating event cache")
 		}
 
-		transform := CreateCalendarDisplayable(temp)
+		transform := graph.CreateCalendarDisplayable(temp)
 		if err = ecc.AddToCache(ctx, transform); err != nil {
 			return "", errors.Wrap(err, "adding new calendar to cache")
 		}
