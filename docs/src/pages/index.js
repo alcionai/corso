155d973a97da3243487aca30cf064e8dd8175cda
Abin <mail@meain.io>
2022-10-20 14:00:58 +0530

    Rename tutorial to quickstart

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="tutorial">
-            Corso Tutorial - 5min ⏱️
+            to="quickstart">
+            Corso Quickstart - 5min ⏱️
           </Link>
         </div>
       </div>
