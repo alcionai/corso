39de3778ba3718316e85cbc66171b8148d3d40cb
ryanfkeepers <ryanfkeepers@gmail.com>
2022-11-02 11:57:01 -0600

    add path prefix/contains filters
    Adds two specialized filters: path prefix and path
    contains.  These filters differ from prior additions
    in two ways: 1/ they accept a slice of inputs and
    perform an implicit any-match when compared,
    2/ they have particular rules about matching
    completely within delimited elements.
    
    In the next PR, these filters will replace the
    standard prefix and contains comparisons that
    are currently used in folder selection.

--- src/pkg/filters/filters.go
+++ src/pkg/filters/filters.go
@@ -1,7 +1,10 @@
 package filters
 
 import (
+	"regexp"
 	"strings"
+
+	"github.com/alcionai/corso/src/pkg/path"
 )
 
 type comparator int
@@ -24,12 +27,38 @@ const (
 	Fails
 	// passthrough for the target
 	IdentityValue
-	// "foo" is a prefix of "foo/bar/baz"
+	// "foo" is a prefix of "foobarbaz"
 	TargetPrefixes
+	// "foo" equals any complete element prefix of "foo/bar/baz"
+	TargetPathPrefix
+	// "foo" equals any complete element in "foo/bar/baz"
+	TargetPathContains
 )
 
 func norm(s string) string {
 	return strings.ToLower(s)
+}
+
+// normPathElem ensures the string is:
+// 1. prefixed with a single os path delimiter (ex: `/`)
+// 2. suffixed with a single os path delimiter (ex: `/`)
+// This is done to facilitate future regex comparisons
+// without re-running the prefix-suffix addition multiple
+// times per target.
+func normPathElem(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	if s[0] != path.PathSeparator {
+		s = string(path.PathSeparator) + s
+	}
+
+	if s[len(s)-1] != path.PathSeparator {
+		s += string(path.PathSeparator)
+	}
+
+	return s
 }
 
 // Filter contains a comparator func and the target to
@@ -37,8 +66,9 @@ func norm(s string) string {
 // true if Filter.Comparer(filter.target, v) is true.
 type Filter struct {
 	Comparator comparator `json:"comparator"`
-	Target     string     `json:"target"` // the value to compare against
-	Negate     bool       `json:"negate"` // when true, negate the comparator result
+	Target     string     `json:"target"`  // the value to compare against
+	Targets    []string   `json:"targets"` // the set of values to compare against, always with "any-match" behavior
+	Negate     bool       `json:"negate"`  // when true, negate the comparator result
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -133,9 +163,76 @@ func NotPrefix(target string) Filter {
 	return newFilter(TargetPrefixes, target, true)
 }
 
+// PathPrefix creates a filter where Compare(v) is true if
+// target.Prefix(v) &&
+// split(target)[i].Equals(split(v)[i]) for _all_ i in 0..len(target)-1
+// ex: target "/foo/bar" returns true for input "/foo/bar/baz", but false for "/foo/barbaz"
+func PathPrefix(targets []string) Filter {
+	tgts := make([]string, len(targets))
+	for i := range targets {
+		tgts[i] = normPathElem(targets[i])
+	}
+
+	return newSliceFilter(TargetPathPrefix, tgts, false)
+}
+
+// NotPathPrefix creates a filter where Compare(v) is true if
+// !target.Prefix(v) ||
+// !split(target)[i].Equals(split(v)[i]) for _any_ i in 0..len(target)-1
+// ex: target "/foo/bar" returns false for input "/foo/bar/baz", but true for "/foo/barbaz"
+func NotPathPrefix(targets []string) Filter {
+	tgts := make([]string, len(targets))
+	for i := range targets {
+		tgts[i] = normPathElem(targets[i])
+	}
+
+	return newSliceFilter(TargetPathPrefix, tgts, true)
+}
+
+// PathContains creates a filter where Compare(v) is true if
+// for _any_ elem e in split(v), target.Equals(e) ||
+// for _any_ sequence of elems in split(v), target.Equals(path.Join(e[n:m]))
+// ex: target "foo" returns true for input "/baz/foo/bar", but false for "/baz/foobar"
+// ex: target "baz/foo" returns true for input "/baz/foo/bar", but false for "/baz/foobar"
+func PathContains(targets []string) Filter {
+	tgts := make([]string, len(targets))
+	for i := range targets {
+		tgts[i] = normPathElem(targets[i])
+	}
+
+	return newSliceFilter(TargetPathContains, tgts, false)
+}
+
+// PathContains creates a filter where Compare(v) is true if
+// for _every_ elem e in split(v), !target.Equals(e) ||
+// for _every_ sequence of elems in split(v), !target.Equals(path.Join(e[n:m]))
+// ex: target "foo" returns false for input "/baz/foo/bar", but true for "/baz/foobar"
+// ex: target "baz/foo" returns false for input "/baz/foo/bar", but true for "/baz/foobar"
+func NotPathContains(targets []string) Filter {
+	tgts := make([]string, len(targets))
+	for i := range targets {
+		tgts[i] = normPathElem(targets[i])
+	}
+
+	return newSliceFilter(TargetPathContains, tgts, true)
+}
+
 // newFilter is the standard filter constructor.
 func newFilter(c comparator, target string, negate bool) Filter {
-	return Filter{c, target, negate}
+	return Filter{
+		Comparator: c,
+		Target:     target,
+		Negate:     negate,
+	}
+}
+
+// newSliceFilter constructs filters that contain multiple targets
+func newSliceFilter(c comparator, targets []string, negate bool) Filter {
+	return Filter{
+		Comparator: c,
+		Targets:    targets,
+		Negate:     negate,
+	}
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -144,6 +241,12 @@ func newFilter(c comparator, target string, negate bool) Filter {
 
 // CompareAny checks whether any one of all the provided
 // inputs passes the filter.
+//
+// Note that, as a gotcha, CompareAny can resolve truthily
+// for both the standard and negated versions of a filter.
+// Ex: consider the input CompareAny(true, false), which
+// will return true for both Equals(true) and NotEquals(true),
+// because at least one element matches for both filters.
 func (f Filter) CompareAny(inputs ...string) bool {
 	for _, in := range inputs {
 		if f.Compare(in) {
@@ -156,7 +259,10 @@ func (f Filter) CompareAny(inputs ...string) bool {
 
 // Compare checks whether the input passes the filter.
 func (f Filter) Compare(input string) bool {
-	var cmp func(string, string) bool
+	var (
+		cmp      func(string, string) bool
+		hasSlice bool
+	)
 
 	switch f.Comparator {
 	case EqualTo, IdentityValue:
@@ -171,18 +277,36 @@ func (f Filter) Compare(input string) bool {
 		cmp = in
 	case TargetPrefixes:
 		cmp = prefixed
+	case TargetPathPrefix:
+		cmp = pathPrefix
+		hasSlice = true
+	case TargetPathContains:
+		cmp = pathContains
+		hasSlice = true
 	case Passes:
 		return true
 	case Fails:
 		return false
 	}
 
-	result := cmp(norm(f.Target), norm(input))
-	if f.Negate {
-		result = !result
+	targets := []string{f.Target}
+	if hasSlice {
+		targets = f.Targets
 	}
 
-	return result
+	for _, tgt := range targets {
+		success := cmp(norm(tgt), norm(input))
+		if f.Negate {
+			success = !success
+		}
+
+		// any-match
+		if success {
+			return true
+		}
+	}
+
+	return false
 }
 
 // true if t == i
@@ -215,18 +339,67 @@ func prefixed(target, input string) bool {
 	return strings.HasPrefix(input, target)
 }
 
+// true if target is an _element complete_ prefix match
+// on the input.  Element complete means we do not
+// succeed on partial elementt matches (ex: "/foo" does
+// not match "/foobar").
+//
+// As a precondition, assumes the target value has been
+// passed through normPathElem().
+//
+// The input is assumed to be the complete path that may
+// have the target as a prefix.
+//
+// Basic principle: For any normalized, well formed target
+// and input (ex: values like "/a/b/c/"), the target is a
+// prefix if the regex `^target` matches the input.
+func pathPrefix(target, input string) bool {
+	re, err := regexp.Compile("^" + target)
+	if err != nil {
+		return false
+	}
+
+	return re.MatchString(normPathElem(input))
+}
+
+// true if target has an _element complete_ equality
+// with any element, or any sequence of elements, from
+// the input.  Element complete means we do not succeed
+// on partial element matches (ex: foo does not match
+// /foobar, and foo/bar does not match foo/barbaz).
+//
+// As a precondition, assumes the target value has been
+// passed through normPathElem().
+//
+// Input is assumed to be the complete path that may
+// contain the target as an element or sequence of elems.
+//
+// Basic principle: For any normalized, well formed target
+// and input (ex: values like "/a/b/c/"), the input contains
+// the target if the regex `target` matches the input.
+func pathContains(target, input string) bool {
+	re, err := regexp.Compile(target)
+	if err != nil {
+		return false
+	}
+
+	return re.MatchString(normPathElem(input))
+}
+
 // ----------------------------------------------------------------------------------------------------
 // Helpers
 // ----------------------------------------------------------------------------------------------------
 
 // prefixString maps the comparators to string prefixes for printing.
 var prefixString = map[comparator]string{
-	EqualTo:        "eq:",
-	GreaterThan:    "gt:",
-	LessThan:       "lt:",
-	TargetContains: "cont:",
-	TargetIn:       "in:",
-	TargetPrefixes: "pr:",
+	EqualTo:            "eq:",
+	GreaterThan:        "gt:",
+	LessThan:           "lt:",
+	TargetContains:     "cont:",
+	TargetIn:           "in:",
+	TargetPrefixes:     "pfx:",
+	TargetPathPrefix:   "pathPfx:",
+	TargetPathContains: "pathCont:",
 }
 
 func (f Filter) String() string {
