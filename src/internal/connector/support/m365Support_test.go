ecde9cc242bb16537edf555ecc33501daa540025
Danny <danny@alcion.ai>
2022-11-04 16:57:28 -0400

    GC: Backup: Sharepoint: Mock Support (#1456)
    Creates Sharepoint.List mock object. Tests support the creation and hydration of objects.

--- src/internal/connector/support/m365Support_test.go
+++ src/internal/connector/support/m365Support_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/alcionai/corso/src/internal/connector/mockconnector"
@@ -114,6 +115,45 @@ func (suite *DataSupportSuite) TestCreateEventFromBytes() {
 	for _, test := range tests {
 		suite.T().Run(test.name, func(t *testing.T) {
 			result, err := CreateEventFromBytes(test.byteArray)
+			test.checkError(t, err)
+			test.isNil(t, result)
+		})
+	}
+}
+
+func (suite *DataSupportSuite) TestCreateListFromBytes() {
+	listBytes, err := mockconnector.GetMockListBytes("DataSupportSuite")
+	require.NoError(suite.T(), err)
+
+	tests := []struct {
+		name       string
+		byteArray  []byte
+		checkError assert.ErrorAssertionFunc
+		isNil      assert.ValueAssertionFunc
+	}{
+		{
+			name:       "Empty Byes",
+			byteArray:  make([]byte, 0),
+			checkError: assert.Error,
+			isNil:      assert.Nil,
+		},
+		{
+			name:       "Invalid Bytes",
+			byteArray:  []byte("Invalid byte stream \"subject:\" Not going to work"),
+			checkError: assert.Error,
+			isNil:      assert.Nil,
+		},
+		{
+			name:       "Valid List",
+			byteArray:  listBytes,
+			checkError: assert.NoError,
+			isNil:      assert.NotNil,
+		},
+	}
+
+	for _, test := range tests {
+		suite.T().Run(test.name, func(t *testing.T) {
+			result, err := CreateListFromBytes(test.byteArray)
 			test.checkError(t, err)
 			test.isNil(t, result)
 		})
