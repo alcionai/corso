670ec2573a31898d766f281d4743e65700988aef
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2022-11-19 20:50:17 +0100

    fixing the " in the sidebars.js

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -15,54 +15,42 @@
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   docsSidebar: [
-    "intro",
-    "quickstart",
+    'intro',
+    'quickstart',
     {
-      type: "category",
-      label: "Corso setup",
-      items: [
-        "setup/concepts",
-        "setup/download",
-        "setup/m365_access",
-        "setup/configuration",
-        "setup/repos",
-      ],
+      type: 'category',
+      label: 'Corso setup',
+      items: ['setup/concepts', 'setup/download', 'setup/m365_access', 'setup/configuration', 'setup/repos'],
     },
     {
-      type: "category",
-      label: "Command line reference",
+      type: 'category',
+      label: 'Command line reference',
       link: {
-        slug: "cli/corso",
-        description: "Explore the commonly used Corso CLI commands",
-        type: "generated-index",
+        slug: 'cli/corso',
+        description: 'Explore the commonly used Corso CLI commands',
+        type: 'generated-index',
       },
       items: [
-        "cli/corso_repo_init_s3",
-        "cli/corso_repo_connect_s3",
-        "cli/corso_backup_create_exchange",
-        "cli/corso_backup_list_exchange",
-        "cli/corso_backup_details_exchange",
-        "cli/corso_backup_create_onedrive",
-        "cli/corso_backup_list_onedrive",
-        "cli/corso_backup_details_onedrive",
-        "cli/corso_restore_exchange",
-        "cli/corso_restore_onedrive",
-        "cli/corso_env",
-      ],
+        'cli/corso_repo_init_s3', 'cli/corso_repo_connect_s3',
+        'cli/corso_backup_create_exchange', 'cli/corso_backup_list_exchange', 'cli/corso_backup_details_exchange',
+        'cli/corso_backup_create_onedrive', 'cli/corso_backup_list_onedrive', 'cli/corso_backup_details_onedrive',
+        'cli/corso_restore_exchange', 'cli/corso_restore_onedrive',
+        'cli/corso_env'
+      ]
     },
     {
-      type: "category",
-      label: "Support",
+      type: 'category',
+      label: 'Support',
       items: [
-        "support/bugs_and_features",
-        "support/known_issues",
-        "support/faq",
+        'support/bugs_and_features', 'support/known_issues', 'support/faq'
       ],
     },
     {
-      type: "category",
-      label: "Developer guide",
-      items: ["developers/build", "developers/testing", "developers/linters"],
+      type: 'category',
+      label: 'Developer guide',
+      items: [
+        'developers/build', 'developers/testing', 'developers/linters'
+      ],
     },
   ],
 };
