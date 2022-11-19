718502d8a2220f38606775f7d9396a3fc541b2c6
Niraj Tolia <ntolia@users.noreply.github.com>
2022-11-19 14:54:32 -0800

    Force dark mode temporarily while we fix light mode

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -140,8 +140,8 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
-        respectPrefersColorScheme: true,
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
       },
 
       zoom: {
