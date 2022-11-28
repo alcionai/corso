7d53006925c3aba2f35ba01122d129a82ac3a3f7
Ashlie Martinez <ashmrtnz@alcion.ai>
2022-11-28 14:47:55 -0800

    Fetch contacts through delta endpoint
    Does not take options right now as the graph SDK dependency needs to be
    updated for that to work.

--- src/internal/connector/exchange/service_iterators.go
+++ src/internal/connector/exchange/service_iterators.go
@@ -8,6 +8,7 @@ import (
 	multierror "github.com/hashicorp/go-multierror"
 	msgraphgocore "github.com/microsoftgraph/msgraph-sdk-go-core"
 	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	cdelta "github.com/microsoftgraph/msgraph-sdk-go/users/item/contactfolders/item/contacts/delta"
 	mdelta "github.com/microsoftgraph/msgraph-sdk-go/users/item/mailfolders/item/messages/delta"
 	"github.com/pkg/errors"
 
@@ -217,61 +218,54 @@ func FetchEventIDsFromCalendar(
 // FetchContactIDsFromDirectory function that returns a list of  all the m365IDs of the contacts
 // of the targeted directory
 func FetchContactIDsFromDirectory(ctx context.Context, gs graph.Service, user, directoryID string) ([]string, error) {
-	options, err := optionsForContactFoldersItem([]string{"parentFolderId"})
-	if err != nil {
-		return nil, err
-	}
+	var (
+		errs *multierror.Error
+		ids  []string
+	)
 
-	ids := []string{}
+	//options, err := optionsForContactFoldersItem([]string{"parentFolderId"})
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "getting query options")
+	//}
 
-	response, err := gs.Client().
+	query := gs.Client().
 		UsersById(user).
 		ContactFoldersById(directoryID).
 		Contacts().
-		Get(ctx, options)
-	if err != nil {
-		return nil, errors.Wrap(err, support.ConnectorStackErrorTrace(err))
-	}
+		Delta()
 
-	pageIterator, err := msgraphgocore.NewPageIterator(
-		response,
-		gs.Adapter(),
-		models.CreateContactCollectionResponseFromDiscriminatorValue,
-	)
-	if err != nil {
-		return nil, errors.Wrap(err, "iterator creation during FetchContactIDs")
-	}
-
-	var errs *multierror.Error
-
-	err = pageIterator.Iterate(ctx, func(pageItem any) bool {
-		entry, ok := pageItem.(graph.Idable)
-		if !ok {
-			errs = multierror.Append(
-				errs,
-				errors.New("casting pageItem to models.Contactable"),
-			)
-
-			return true
+	for {
+		// TODO(ashmrtn): Update to pass options once graph SDK dependency is updated.
+		resp, err := query.Get(ctx, nil)
+		if err != nil {
+			return nil, errors.Wrap(err, support.ConnectorStackErrorTrace(err))
 		}
 
-		if entry.GetId() == nil {
-			errs = multierror.Append(errs, errors.New("item with nil ID"))
-			return true
+		for _, item := range resp.GetValue() {
+			if item.GetId() == nil {
+				errs = multierror.Append(
+					errs,
+					errors.Errorf("item with nil ID in folder %s", directoryID),
+				)
+
+				// TODO(ashmrtn): Handle fail-fast.
+				continue
+			}
+
+			ids = append(ids, *item.GetId())
 		}
 
-		ids = append(ids, *entry.GetId())
+		nextLinkIface := resp.GetAdditionalData()[nextLinkKey]
+		if nextLinkIface == nil {
+			break
+		}
 
-		return true
-	})
+		nextLink := nextLinkIface.(*string)
+		if len(*nextLink) == 0 {
+			break
+		}
 
-	if err != nil {
-		return nil,
-			errors.Wrap(
-				err,
-				support.ConnectorStackErrorTrace(err)+
-					" :fetching contactIDs from directory "+directoryID,
-			)
+		query = cdelta.NewDeltaRequestBuilder(*nextLink, gs.Adapter())
 	}
 
 	return ids, errs.ErrorOrNil()
