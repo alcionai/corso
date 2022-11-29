c412cb09f441f073f07c624c4c5982f5bda745b5
ryanfkeepers <ryanfkeepers@gmail.com>
2022-11-29 13:31:31 -0700

    add sharepoint library restore

--- src/internal/connector/onedrive/restore.go
+++ src/internal/connector/onedrive/restore.go
@@ -66,7 +66,7 @@ func RestoreCollections(
 
 	// Iterate through the data collections and restore the contents of each
 	for _, dc := range dcs {
-		temp, canceled := restoreCollection(ctx, service, dc, dest.ContainerName, deets, errUpdater)
+		temp, canceled := RestoreCollection(ctx, service, dc, dest.ContainerName, deets, errUpdater)
 
 		restoreMetrics.Combine(temp)
 
@@ -85,10 +85,10 @@ func RestoreCollections(
 		nil
 }
 
-// restoreCollection handles restoration of an individual collection.
-// @returns Integer representing totalItems, restoredItems, and the
-// amount of bytes restored. The bool represents whether the context was cancelled
-func restoreCollection(
+// RestoreCollection handles restoration of an individual collection.
+// returns the metrics gathered during restoration.
+// returns true if the context was cancelled.
+func RestoreCollection(
 	ctx context.Context,
 	service graph.Service,
 	dc data.Collection,
