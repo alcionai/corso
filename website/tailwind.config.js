9c01a5f11dec3f9ddcb6cd9b6a77b0949430929d
Abin <mail@meain.io>
2022-11-30 13:55:25 +0530

    Few small tweaks

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     screens: {
       xs: "540px",
