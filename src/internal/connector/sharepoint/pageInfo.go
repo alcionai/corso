8cbab8620e12e190d39026b69c6ffc4f65186ba5
Danny Adams <da469@cornell.edu>
2023-01-10 08:40:48 -0500

    Issue #2069: Backup Details expansion SharePointInfo method for SitePageable objects. Test suite included.

---
+++ src/internal/connector/sharepoint/pageInfo.go
@@ -0,0 +1,44 @@
+package sharepoint
+
+import (
+	"time"
+
+	bmodel "github.com/microsoftgraph/msgraph-beta-sdk-go/models"
+
+	"github.com/alcionai/corso/src/pkg/backup/details"
+)
+
+// sharePointPageInfo propagates metadata from  the SharePoint Page data type
+// into searchable content.
+// Page Details: https://learn.microsoft.com/en-us/graph/api/resources/sitepage?view=graph-rest-beta
+func sharePointPageInfo(page bmodel.SitePageable, size int64) *details.SharePointInfo {
+	var (
+		name, webURL      string
+		created, modified time.Time
+	)
+
+	if page.GetTitle() != nil {
+		name = *page.GetTitle()
+	}
+
+	if page.GetWebUrl() != nil {
+		webURL = *page.GetWebUrl()
+	}
+
+	if page.GetCreatedDateTime() != nil {
+		created = *page.GetCreatedDateTime()
+	}
+
+	if page.GetLastModifiedDateTime() != nil {
+		modified = *page.GetLastModifiedDateTime()
+	}
+
+	return &details.SharePointInfo{
+		ItemType: details.SharePointItem,
+		ItemName: name,
+		Created:  created,
+		Modified: modified,
+		WebURL:   webURL,
+		Size:     size,
+	}
+}
