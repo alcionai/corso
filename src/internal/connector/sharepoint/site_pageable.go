04b457f7f4ac9d4310b96ca880ae9ca51ef9e31a
Danny Adams <da469@cornell.edu>
2023-01-10 12:14:12 -0500

    Reduced version of site_page.go and its interface from msgraph-beta-sdk-go

---
+++ src/internal/connector/sharepoint/site_pageable.go
@@ -0,0 +1,24 @@
+package sharepoint
+
+import (
+	kioser "github.com/microsoft/kiota-abstractions-go/serialization"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+)
+
+// SitePageable adjusted from msgraph-beta-sdk-go for temporary testing
+type SitePageable interface {
+	models.BaseItemable
+	kioser.Parsable
+	GetContentType() models.ContentTypeInfoable
+	GetPublishingState() models.PublicationFacetable
+	GetShowComments() *bool
+	GetShowRecommendedPages() *bool
+	GetThumbnailWebUrl() *string
+	GetTitle() *string
+	SetContentType(value models.ContentTypeInfoable)
+	SetPublishingState(value models.PublicationFacetable)
+	SetShowComments(value *bool)
+	SetShowRecommendedPages(value *bool)
+	SetThumbnailWebUrl(value *string)
+	SetTitle(value *string)
+}
