675862c3ef73c6d48519eb0099f60e4b5ea5b375
Danny Adams <da469@cornell.edu>
2023-01-18 15:50:38 -0500

    Changes made to collection test to use drive name.:

--- src/internal/connector/onedrive/collections_test.go
+++ src/internal/connector/onedrive/collections_test.go
@@ -265,7 +265,7 @@ func (suite *OneDriveCollectionsSuite) TestUpdateCollections() {
 				nil,
 				control.Options{})
 
-			err := c.UpdateCollections(ctx, "driveID", tt.items)
+			err := c.UpdateCollections(ctx, "driveID", "General", tt.items)
 			tt.expect(t, err)
 			assert.Equal(t, len(tt.expectedCollectionPaths), len(c.CollectionMap), "collection paths")
 			assert.Equal(t, tt.expectedItemCount, c.NumItems, "item count")
