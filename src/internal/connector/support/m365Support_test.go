f425d2436470418380044660c77c0c24645a7b86
Danny Adams <da469@cornell.edu>
2023-01-20 11:11:11 -0500

    Update support library to beta.

--- src/internal/connector/support/m365Support_test.go
+++ src/internal/connector/support/m365Support_test.go
@@ -3,7 +3,7 @@ package support
 import (
 	"testing"
 
-	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	"github.com/microsoftgraph/msgraph-beta-sdk-go/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
