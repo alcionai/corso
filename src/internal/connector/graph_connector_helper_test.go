2a93dc9caf67b199fe13b34cf615329d9161228c
Danny Adams <da469@cornell.edu>
2023-01-25 07:59:09 -0500

    Updates to graph_connector component and helper_test files. Reverts back to msgraph-sdk-go.

--- src/internal/connector/graph_connector_helper_test.go
+++ src/internal/connector/graph_connector_helper_test.go
@@ -6,7 +6,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/microsoftgraph/msgraph-beta-sdk-go/models"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
