c4fe37e642881cbc6596023f49627defe770c002
Abin <mail@meain.io>
2023-01-27 18:51:59 +0530

    dummy

--- src/pkg/backup/backup.go
+++ src/pkg/backup/backup.go
@@ -13,7 +13,7 @@ import (
 	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
-const currentBackupVersion = 1
+const CurrentBackupFormatVersion = 1
 
 // Backup represents the result of a backup operation
 type Backup struct {
@@ -66,7 +66,7 @@ func New(
 		Selector:        selector,
 		ReadWrites:      rw,
 		StartAndEndTime: se,
-		Version:         currentBackupVersion,
+		Version:         CurrentBackupFormatVersion,
 	}
 }
 
