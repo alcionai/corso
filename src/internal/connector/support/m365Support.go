0340ce67fa7169e4d9ab2c9c9de07041f0899d3d
Danny Adams <da469@cornell.edu>
2023-01-28 14:13:01 -0500

    Updates to /support/m365Support.go PR Suggestion: Error messages updated for clarity.

--- src/internal/connector/support/m365Support.go
+++ src/internal/connector/support/m365Support.go
@@ -15,7 +15,7 @@ import (
 func CreateFromBytes(bytes []byte, createFunc absser.ParsableFactory) (absser.Parsable, error) {
 	parseNode, err := js.NewJsonParseNodeFactory().GetRootParseNode("application/json", bytes)
 	if err != nil {
-		return nil, errors.Wrap(err, "parsing byte array into m365 object")
+		return nil, errors.Wrap(err, "deserializing bytes into base m365 object")
 	}
 
 	anObject, err := parseNode.GetObjectValue(createFunc)
@@ -30,7 +30,7 @@ func CreateFromBytes(bytes []byte, createFunc absser.ParsableFactory) (absser.Pa
 func CreateMessageFromBytes(bytes []byte) (models.Messageable, error) {
 	aMessage, err := CreateFromBytes(bytes, models.CreateMessageFromDiscriminatorValue)
 	if err != nil {
-		return nil, errors.Wrap(err, "creating m365 exchange.Mail object from provided bytes")
+		return nil, errors.Wrap(err, "deserializing bytes to exchange message")
 	}
 
 	message := aMessage.(models.Messageable)
@@ -43,7 +43,7 @@ func CreateMessageFromBytes(bytes []byte) (models.Messageable, error) {
 func CreateContactFromBytes(bytes []byte) (models.Contactable, error) {
 	parsable, err := CreateFromBytes(bytes, models.CreateContactFromDiscriminatorValue)
 	if err != nil {
-		return nil, errors.Wrap(err, "creating m365 exchange.Contact object from provided bytes")
+		return nil, errors.Wrap(err, "deserializing bytes to exchange contact")
 	}
 
 	contact := parsable.(models.Contactable)
@@ -55,7 +55,7 @@ func CreateContactFromBytes(bytes []byte) (models.Contactable, error) {
 func CreateEventFromBytes(bytes []byte) (models.Eventable, error) {
 	parsable, err := CreateFromBytes(bytes, models.CreateEventFromDiscriminatorValue)
 	if err != nil {
-		return nil, errors.Wrap(err, "creating m365 exchange.Event object from provided bytes")
+		return nil, errors.Wrap(err, "deserializing bytes to exchange event")
 	}
 
 	event := parsable.(models.Eventable)
@@ -67,7 +67,7 @@ func CreateEventFromBytes(bytes []byte) (models.Eventable, error) {
 func CreateListFromBytes(bytes []byte) (models.Listable, error) {
 	parsable, err := CreateFromBytes(bytes, models.CreateListFromDiscriminatorValue)
 	if err != nil {
-		return nil, errors.Wrap(err, "creating m365 sharepoint.List object from provided bytes")
+		return nil, errors.Wrap(err, "deserializing bytes to sharepoint list")
 	}
 
 	list := parsable.(models.Listable)
@@ -79,7 +79,7 @@ func CreateListFromBytes(bytes []byte) (models.Listable, error) {
 func CreatePageFromBytes(bytes []byte) (bmodels.SitePageable, error) {
 	parsable, err := CreateFromBytes(bytes, bmodels.CreateSitePageFromDiscriminatorValue)
 	if err != nil {
-		return nil, errors.Wrap(err, "createing m365 sharepoint.Page object from provided bytes")
+		return nil, errors.Wrap(err, "deserializing bytes to sharepoint page")
 	}
 
 	page := parsable.(bmodels.SitePageable)
