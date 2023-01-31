282963b22e24783e80162d5b16296cd76d35205d
Danny Adams <da469@cornell.edu>
2023-01-31 16:15:41 -0500

    Updates made to /connector/discovery/api/beta_service.go Refactor Service -> BetaService

--- src/internal/connector/discovery/api/beta_service.go
+++ src/internal/connector/discovery/api/beta_service.go
@@ -10,23 +10,23 @@ import (
 // Service wraps BetaClient's functionality.
 // Abstraction created to comply loosely with graph.Servicer
 // methods for ease of switching between v1.0 and beta connnectors
-type Service struct {
+type BetaService struct {
 	client *betasdk.BetaClient
 }
 
-func (s Service) Client() *betasdk.BetaClient {
+func (s BetaService) Client() *betasdk.BetaClient {
 	return s.client
 }
 
-func NewBetaService(adpt *msgraphsdk.GraphRequestAdapter) *Service {
-	return &Service{
+func NewBetaService(adpt *msgraphsdk.GraphRequestAdapter) *BetaService {
+	return &BetaService{
 		client: betasdk.NewBetaClient(adpt),
 	}
 }
 
 // Seraialize writes an M365 parsable object into a byte array using the built-in
 // application/json writer within the adapter.
-func (s Service) Serialize(object absser.Parsable) ([]byte, error) {
+func (s BetaService) Serialize(object absser.Parsable) ([]byte, error) {
 	writer, err := s.client.Adapter().
 		GetSerializationWriterFactory().
 		GetSerializationWriter("application/json")
