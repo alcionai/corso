b5b457639338369e5e165788b2781b7e3493bd07
ashmrtn <ashmrtnz@alcion.ai>
2023-01-31 17:25:27 -0800

    Run gofmt on beta package (#2340)
    ## Description
    
    This package is mostly generated code, but since it's not formatted and check in it causes subsequent runs of gofmt to format it and add chaff to git diff. Checking in the formatted version removes the chaff.
    
    ## Does this PR need a docs update or release note?
    
    - [ ] :white_check_mark: Yes, it's included
    - [ ] :clock1: Yes, but in a later PR
    - [x] :no_entry: No 
    
    ## Type of change
    
    - [ ] :sunflower: Feature
    - [ ] :bug: Bugfix
    - [ ] :world_map: Documentation
    - [ ] :robot: Test
    - [ ] :computer: CI/Deployment
    - [x] :broom: Tech Debt/Cleanup

--- src/internal/connector/graph/betasdk/models/standard_web_partable.go
+++ src/internal/connector/graph/betasdk/models/standard_web_partable.go
@@ -1,15 +1,15 @@
 package models
 
 import (
-    i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91 "github.com/microsoft/kiota-abstractions-go/serialization"
+	i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91 "github.com/microsoft/kiota-abstractions-go/serialization"
 )
 
-// StandardWebPartable 
+// StandardWebPartable
 type StandardWebPartable interface {
-    i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91.Parsable
-    WebPartable
-    GetData()(WebPartDataable)
-    GetWebPartType()(*string)
-    SetData(value WebPartDataable)()
-    SetWebPartType(value *string)()
+	i878a80d2330e89d26896388a3f487eef27b0a0e6c010c493bf80be1452208f91.Parsable
+	WebPartable
+	GetData() WebPartDataable
+	GetWebPartType() *string
+	SetData(value WebPartDataable)
+	SetWebPartType(value *string)
 }
