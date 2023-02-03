a088da7517172ed203f02645d1cff04ce455c7cf
Danny Adams <da469@cornell.edu>
2023-02-03 13:08:47 -0500

    Updates to /connector/sharepoint/data_collections.go Updates struct fields for usage, libraries, and updates for usage of api directory.

--- src/internal/connector/sharepoint/data_collections.go
+++ src/internal/connector/sharepoint/data_collections.go
@@ -9,6 +9,7 @@ import (
 	"github.com/alcionai/corso/src/internal/connector/discovery/api"
 	"github.com/alcionai/corso/src/internal/connector/graph"
 	"github.com/alcionai/corso/src/internal/connector/onedrive"
+	sapi "github.com/alcionai/corso/src/internal/connector/sharepoint/api"
 	"github.com/alcionai/corso/src/internal/connector/support"
 	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/internal/observe"
@@ -185,13 +186,13 @@ func collectPages(
 
 	betaService := api.NewBetaService(adpt)
 
-	tuples, err := fetchPages(ctx, betaService, siteID)
+	tuples, err := sapi.FetchPages(ctx, betaService, siteID)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, tuple := range tuples {
-		dir, err := path.Builder{}.Append(tuple.name).
+		dir, err := path.Builder{}.Append(tuple.Name).
 			ToDataLayerSharePointPath(
 				tenantID,
 				siteID,
@@ -203,7 +204,7 @@ func collectPages(
 
 		collection := NewCollection(dir, serv, updater.UpdateStatus)
 		collection.betaService = betaService
-		collection.AddJob(tuple.id)
+		collection.AddJob(tuple.ID)
 
 		spcs = append(spcs, collection)
 	}
