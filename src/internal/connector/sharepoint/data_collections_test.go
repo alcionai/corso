a3596468e73637ad54df04d8792689d340756863
Abin <mail@meain.io>
2023-02-09 18:37:54 +0530

    Update sharepoint tests

--- src/internal/connector/sharepoint/data_collections_test.go
+++ src/internal/connector/sharepoint/data_collections_test.go
@@ -20,7 +20,7 @@ import (
 // ---------------------------------------------------------------------------
 
 const (
-	testBaseDrivePath = "drive/driveID1/root:"
+	testBaseDrivePath = "drives/driveID1/root:"
 )
 
 type testFolderMatcher struct {
@@ -60,6 +60,7 @@ func (suite *SharePointLibrariesSuite) TestUpdateCollections() {
 		items                   []models.DriveItemable
 		scope                   selectors.SharePointScope
 		expect                  assert.ErrorAssertionFunc
+		expectedCollectionIDs   []string
 		expectedCollectionPaths []string
 		expectedItemCount       int
 		expectedContainerCount  int
@@ -68,10 +69,12 @@ func (suite *SharePointLibrariesSuite) TestUpdateCollections() {
 		{
 			testCase: "Single File",
 			items: []models.DriveItemable{
+				driveRootItem("root"),
 				driveItem("file", testBaseDrivePath, true),
 			},
-			scope:  anyFolder,
-			expect: assert.NoError,
+			scope:                 anyFolder,
+			expect:                assert.NoError,
+			expectedCollectionIDs: []string{"root"},
 			expectedCollectionPaths: expectedPathAsSlice(
 				suite.T(),
 				tenant,
@@ -101,14 +104,17 @@ func (suite *SharePointLibrariesSuite) TestUpdateCollections() {
 				&MockGraphService{},
 				nil,
 				control.Options{})
-			err := c.UpdateCollections(ctx, "driveID", "General", test.items, paths, newPaths, excluded, true)
+			err := c.UpdateCollections(ctx, "driveID1", "General", test.items, paths, newPaths, excluded, true)
 			test.expect(t, err)
-			assert.Equal(t, len(test.expectedCollectionPaths), len(c.CollectionMap), "collection paths")
+			assert.Equal(t, len(test.expectedCollectionIDs), len(c.CollectionMap), "collection paths")
 			assert.Equal(t, test.expectedItemCount, c.NumItems, "item count")
 			assert.Equal(t, test.expectedFileCount, c.NumFiles, "file count")
 			assert.Equal(t, test.expectedContainerCount, c.NumContainers, "container count")
-			for _, collPath := range test.expectedCollectionPaths {
+			for _, collPath := range test.expectedCollectionIDs {
 				assert.Contains(t, c.CollectionMap, collPath)
+			}
+			for _, col := range c.CollectionMap {
+				assert.Contains(t, test.expectedCollectionPaths, col.FullPath().String())
 			}
 		})
 	}
@@ -126,6 +132,16 @@ func driveItem(name string, path string, isFile bool) models.DriveItemable {
 	if isFile {
 		item.SetFile(models.NewFile())
 	}
+
+	return item
+}
+
+func driveRootItem(id string) models.DriveItemable {
+	name := "root"
+	item := models.NewDriveItem()
+	item.SetName(&name)
+	item.SetId(&id)
+	item.SetRoot(models.NewRoot())
 
 	return item
 }
