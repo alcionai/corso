478ee6a3c361f6b0e11d22c59c303e3dc9d93292
Danny Adams <da469@cornell.edu>
2023-02-10 14:57:47 -0500

    Updates to /common/ptr/pointer.go Expansion of test suite to verify correcto operation on several generic pointers.

--- src/internal/common/ptr/pointer_test.go
+++ src/internal/common/ptr/pointer_test.go
@@ -2,6 +2,7 @@ package ptr_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -17,29 +18,82 @@ func TestPointerSuite(t *testing.T) {
 	suite.Run(t, new(PointerSuite))
 }
 
-func (suite *PointerSuite) TestVal() {
-	word := "Hello World"
-	tables := []struct {
-		name    string
-		pointer *string
-		isEmpty assert.ValueAssertionFunc
-	}{
-		{
-			name:    "Nil",
-			pointer: nil,
-			isEmpty: assert.Empty,
-		},
-		{
-			name:    "Generic",
-			pointer: &word,
-			isEmpty: assert.NotEmpty,
-		},
-	}
+// TestValue  checks to ptr derefencing for the
+// following types:
+// - *string
+// - *bool
+// - *time.Time
+func (suite *PointerSuite) TestValue() {
+	var (
+		t          = suite.T()
+		created    *time.Time
+		testString *string
+		testBool   *bool
+		testInt    *int
+		testInt32  *int32
+		testInt64  *int64
+	)
 
-	for _, test := range tables {
-		suite.T().Run(test.name, func(t *testing.T) {
-			value := ptr.Val(test.pointer)
-			test.isEmpty(t, value)
-		})
-	}
+	// String Checks
+	subject := ptr.Value(testString)
+	assert.Empty(t, subject)
+
+	hello := "Hello World"
+	testString = &hello
+	subject = ptr.Value(testString)
+
+	t.Logf("Received: %s", subject)
+	assert.NotEmpty(t, subject)
+
+	// Time Checks
+
+	myTime := ptr.Value(created)
+	assert.Empty(t, myTime)
+	assert.NotNil(t, myTime)
+
+	now := time.Now()
+	created = &now
+	myTime = ptr.Value(created)
+	assert.NotEmpty(t, myTime)
+
+	// Bool Checks
+	truth := true
+	myBool := ptr.Value(testBool)
+	assert.NotNil(t, myBool)
+	assert.False(t, myBool)
+
+	testBool = &truth
+	myBool = ptr.Value(testBool)
+	assert.NotNil(t, myBool)
+	assert.True(t, myBool)
+
+	// Int checks
+	myInt := ptr.Value(testInt)
+	myInt32 := ptr.Value(testInt32)
+	myInt64 := ptr.Value(testInt64)
+
+	assert.NotNil(t, myInt)
+	assert.NotNil(t, myInt32)
+	assert.NotNil(t, myInt64)
+	assert.Empty(t, myInt)
+	assert.Empty(t, myInt32)
+	assert.Empty(t, myInt64)
+
+	num := 4071
+	num32 := int32(num * 32)
+	num64 := int64(num * 2048)
+	testInt = &num
+	testInt32 = &num32
+	testInt64 = &num64
+
+	myInt = ptr.Value(testInt)
+	myInt32 = ptr.Value(testInt32)
+	myInt64 = ptr.Value(testInt64)
+
+	assert.NotNil(t, myInt)
+	assert.NotNil(t, myInt32)
+	assert.NotNil(t, myInt64)
+	assert.NotEmpty(t, myInt)
+	assert.NotEmpty(t, myInt32)
+	assert.NotEmpty(t, myInt64)
 }
