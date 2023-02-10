173f2d033e754ac56e410306df1e009ac6d47320
Danny Adams <da469@cornell.edu>
2023-02-10 14:56:28 -0500

    Updates to /common/ptr/pointer.go Updates comments for Value function Removes Val() function in favor of Value() function that operates on generic types.

--- src/internal/common/ptr/pointer.go
+++ src/internal/common/ptr/pointer.go
@@ -3,22 +3,18 @@ package ptr
 // ptr package is a common package used for pointer
 // access and deserialization.
 
-// func Value[T any](generic T, ptr *T) (T, bool) {
-// 	if ptr == nil {
-// 		return generic, false
-// 	}
-
-// 	return *ptr, true
-// }
-
-// Val helper method for unwrapping strings
+// Value Generic option for dereferencing pointers.
 // Microsoft Graph saves many variables as string pointers.
 // Function will safely check if the point is nil prior to
 // dereferencing the pointer. If the pointer is nil,
-// an empty string is returned.
-func Val(ptr *string) string {
+// an empty version of the object is returned.
+// Operation does not work on Nested objects.
+// For example:
+// *evt.GetEnd().GetDateTime() will still cause a panic
+// if evt is nil or GetEnd() is nil
+func Value[T any](ptr *T) T {
 	if ptr == nil {
-		return ""
+		return *new(T)
 	}
 
 	return *ptr
