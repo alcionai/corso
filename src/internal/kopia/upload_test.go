e64c73afecba96df5b606205a1a39972d709e669
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-02-10 12:41:36 -0800

    Extra test for some recursive delete behavior
    Folders replaced after a delete should not have their subfolders
    reparented to the new folder.

--- src/internal/kopia/upload_test.go
+++ src/internal/kopia/upload_test.go
@@ -1688,6 +1688,40 @@ func (suite *HierarchyBuilderUnitSuite) TestBuildDirectoryTreeMultipleSubdirecto
 			),
 		},
 		{
+			name: "ReplaceDeletedSubtreeWithNew",
+			inputCollections: func(t *testing.T) []data.BackupCollection {
+				oldInbox := mockconnector.NewMockExchangeCollection(inboxPath, 0)
+				oldInbox.PrevPath = inboxPath
+				oldInbox.ColState = data.DeletedState
+
+				newCol := mockconnector.NewMockExchangeCollection(inboxPath, 1)
+				newCol.ColState = data.NewState
+				newCol.Names[0] = workFileName2
+				newCol.Data[0] = workFileData2
+
+				return []data.BackupCollection{oldInbox, newCol}
+			},
+			expected: expectedTreeWithChildren(
+				[]string{
+					testTenant,
+					service,
+					testUser,
+					category,
+				},
+				[]*expectedNode{
+					{
+						name: testInboxDir,
+						children: []*expectedNode{
+							{
+								name: workFileName2,
+								data: workFileData2,
+							},
+						},
+					},
+				},
+			),
+		},
+		{
 			name: "ReplaceMovedDirectory",
 			inputCollections: func(t *testing.T) []data.BackupCollection {
 				newPersonalPath := makePath(
