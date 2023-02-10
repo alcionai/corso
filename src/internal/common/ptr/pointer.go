cd1d04e316cb484e6fed5ecf4240e316cc0bbf74
Danny Adams <da469@cornell.edu>
2023-02-10 11:05:05 -0500

    Update to /common/ptr Added framework for abstraction. Incomplete.

--- src/internal/common/ptr/pointer.go
+++ src/internal/common/ptr/pointer.go
@@ -1,5 +1,16 @@
 package ptr
 
+// ptr package is a common package used for pointer
+// access and deserialization.
+
+// func Value[T any](source T, ptr *T) (T, bool) {
+// 	if ptr == nil {
+// 		return source, false
+// 	}
+
+// 	return *ptr, true
+// }
+
 // Val helper method for unwrapping strings
 // Microsoft Graph saves many variables as string pointers.
 // Function will safely check if the point is nil prior to
