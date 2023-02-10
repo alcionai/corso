8f45f599554950adde81332563ed0ecaabacc654
Danny Adams <da469@cornell.edu>
2023-02-10 15:48:43 -0500

    Update to /common/ptr/pointer.go Refactor function name from Value() -> Val()

--- src/internal/common/ptr/pointer.go
+++ src/internal/common/ptr/pointer.go
@@ -3,7 +3,7 @@ package ptr
 // ptr package is a common package used for pointer
 // access and deserialization.
 
-// Value Generic option for dereferencing pointers.
+// Val Generic function for dereferencing pointers.
 // Microsoft Graph saves many variables as string pointers.
 // Function will safely check if the point is nil prior to
 // dereferencing the pointer. If the pointer is nil,
@@ -12,7 +12,7 @@ package ptr
 // For example:
 // *evt.GetEnd().GetDateTime() will still cause a panic
 // if evt is nil or GetEnd() is nil
-func Value[T any](ptr *T) T {
+func Val[T any](ptr *T) T {
 	if ptr == nil {
 		return *new(T)
 	}
