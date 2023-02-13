5fa531e95949808b75bb3328dda4039ad0a85243
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-02-13 14:03:12 -0800

    Begin saving OneDrive file names in metadata files
    Start saving the file name of the backed up file in the metadata file.
    This doesn't change restore behavior at all and files are still saved in
    kopia with their OneDrive file name.

--- src/internal/connector/onedrive/item.go
+++ src/internal/connector/onedrive/item.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/alcionai/clues"
 	msdrives "github.com/microsoftgraph/msgraph-sdk-go/drives"
 	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/pkg/errors"
@@ -61,18 +62,55 @@ func oneDriveItemMetaReader(
 	service graph.Servicer,
 	driveID string,
 	item models.DriveItemable,
-) (io.ReadCloser, int, error) {
-	meta, err := oneDriveItemMetaInfo(ctx, service, driveID, item)
-	if err != nil {
-		return nil, 0, err
+	fetchPermissions bool,
+) (r io.ReadCloser, size int, err error) {
+	meta := Metadata{
+		FileName: *item.GetName(),
 	}
 
-	metaJSON, err := json.Marshal(meta)
-	if err != nil {
-		return nil, 0, err
+	// Always try to serialize the metadata struct so we get the file name.
+	defer func() {
+		metaJSON, serializeErr := json.Marshal(meta)
+		if serializeErr != nil {
+			serializeErr = clues.Wrap(serializeErr, "serializing item metadata")
+
+			// Need to check if err was already non-nil since it doesn't filter nil
+			// values out in calls to Stack().
+			if err != nil {
+				err = clues.Stack(err, serializeErr)
+			} else {
+				err = serializeErr
+			}
+
+			return
+		}
+
+		r = io.NopCloser(bytes.NewReader(metaJSON))
+		size = len(metaJSON)
+	}()
+
+	if !fetchPermissions {
+		return
 	}
 
-	return io.NopCloser(bytes.NewReader(metaJSON)), len(metaJSON), nil
+	var perms []UserPermission
+
+	err = graph.RunWithRetry(func() error {
+		var fetchErr error
+
+		perms, fetchErr = oneDriveItemMetaInfo(ctx, service, driveID, item)
+		return fetchErr
+	})
+	if err != nil {
+		// Keep this in an if-block because if it's not then we have a weird issue
+		// of having no value in error but golang thinking it's non nil because of
+		// the way interfaces work.
+		err = clues.Wrap(err, "fetching item permissions")
+	} else {
+		meta.Permissions = perms
+	}
+
+	return
 }
 
 // oneDriveItemReader will return a io.ReadCloser for the specified item
@@ -186,17 +224,17 @@ func oneDriveItemInfo(di models.DriveItemable, itemSize int64) *details.OneDrive
 func oneDriveItemMetaInfo(
 	ctx context.Context, service graph.Servicer,
 	driveID string, di models.DriveItemable,
-) (Metadata, error) {
+) ([]UserPermission, error) {
 	itemID := di.GetId()
 
 	perm, err := service.Client().DrivesById(driveID).ItemsById(*itemID).Permissions().Get(ctx, nil)
 	if err != nil {
-		return Metadata{}, err
+		return nil, err
 	}
 
 	uperms := filterUserPermissions(perm.GetValue())
 
-	return Metadata{Permissions: uperms}, nil
+	return uperms, nil
 }
 
 func filterUserPermissions(perms []models.Permissionable) []UserPermission {
