734e90c9608095b8fed1356dfa8170fe49c9842a
ryanfkeepers <ryanfkeepers@gmail.com>
2023-02-14 14:59:33 -0700

    adding clues & fault to onedrive restore

--- src/internal/connector/data_collections.go
+++ src/internal/connector/data_collections.go
@@ -268,7 +268,7 @@ func (gc *GraphConnector) RestoreDataCollections(
 	case selectors.ServiceExchange:
 		status, err = exchange.RestoreExchangeDataCollections(ctx, creds, gc.Service, dest, dcs, deets, errs)
 	case selectors.ServiceOneDrive:
-		status, err = onedrive.RestoreCollections(ctx, backupVersion, gc.Service, dest, opts, dcs, deets)
+		status, err = onedrive.RestoreCollections(ctx, backupVersion, gc.Service, dest, opts, dcs, deets, errs)
 	case selectors.ServiceSharePoint:
 		status, err = sharepoint.RestoreCollections(ctx, backupVersion, creds, gc.Service, dest, dcs, deets, errs)
 	default:
