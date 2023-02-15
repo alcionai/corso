81f57f24b068e66e9cdb2177a62b6eb7eaac670d
Danny Adams <da469@cornell.edu>
2023-02-15 10:02:49 -0500

    Creation of /connector/onedrive/data_collections.go. Backup specific operations for onedrive enclosed.

---
+++ src/internal/connector/onedrive/data_collections.go
@@ -0,0 +1,81 @@
+package onedrive
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/alcionai/corso/src/internal/connector/graph"
+	"github.com/alcionai/corso/src/internal/connector/support"
+	"github.com/alcionai/corso/src/internal/data"
+	"github.com/alcionai/corso/src/pkg/control"
+	"github.com/alcionai/corso/src/pkg/logger"
+	"github.com/alcionai/corso/src/pkg/selectors"
+	"github.com/pkg/errors"
+	"golang.org/x/exp/maps"
+)
+
+// ---------------------------------------------------------------------------
+// OneDrive
+// ---------------------------------------------------------------------------
+
+type odFolderMatcher struct {
+	scope selectors.OneDriveScope
+}
+
+func (fm odFolderMatcher) IsAny() bool {
+	return fm.scope.IsAny(selectors.OneDriveFolder)
+}
+
+func (fm odFolderMatcher) Matches(dir string) bool {
+	return fm.scope.Matches(selectors.OneDriveFolder, dir)
+}
+
+// OneDriveDataCollections returns a set of DataCollection which represents the OneDrive data
+// for the specified user
+func DataCollections(
+	ctx context.Context,
+	selector selectors.Selector,
+	metadata []data.RestoreCollection,
+	tenant string,
+	itemClient *http.Client,
+	service graph.Servicer,
+	su support.StatusUpdater,
+	ctrlOpts control.Options,
+) ([]data.BackupCollection, map[string]struct{}, error) {
+	odb, err := selector.ToOneDriveBackup()
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "oneDriveDataCollection: parsing selector")
+	}
+
+	var (
+		user        = selector.DiscreteOwner
+		collections = []data.BackupCollection{}
+		allExcludes = map[string]struct{}{}
+		errs        error
+	)
+
+	// for each scope that includes oneDrive items, get all
+	for _, scope := range odb.Scopes() {
+		logger.Ctx(ctx).With("user", user).Debug("Creating OneDrive collections")
+
+		odcs, excludes, err := NewCollections(
+			itemClient,
+			tenant,
+			user,
+			OneDriveSource,
+			odFolderMatcher{scope},
+			service,
+			su,
+			ctrlOpts,
+		).Get(ctx, metadata)
+		if err != nil {
+			return nil, nil, support.WrapAndAppend(user, err, errs)
+		}
+
+		collections = append(collections, odcs...)
+
+		maps.Copy(allExcludes, excludes)
+	}
+
+	return collections, allExcludes, errs
+}
