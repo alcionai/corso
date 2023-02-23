bdb7ce7fc9c05f325e80e808e07b3630d5573c17
Niraj Tolia <ntolia@users.noreply.github.com>
2023-02-23 14:26:29 -0800

    Tighten user section

--- website/src/components/parts/Users.js
+++ website/src/components/parts/Users.js
@@ -12,21 +12,9 @@ export default function Users() {
   return (
     <section className="relative !tracking-wide flex flex-col home-wrapper items-center overflow-hidden">
       <div className="container md:mt-24 mt-16">
-        <div
-          className="grid grid-cols-1 pb-8 text-center wow animate__animated animate__fadeInUp"
-          data-wow-delay=".1s"
-        >
-          <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
-            What Our Users Say
-          </h3>
-
-          <p className="text-slate-400 max-w-xl mx-auto">
-            Start working with Tailwind CSS that can provide everything you need
-            to generate awareness, drive traffic, connect.
-          </p>
-        </div>
-
         <div className="grid grid-cols-1 mt-8">
+        <h3 className="text-center mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
+            What Corso Users Say</h3>
           <div
             className="tiny-three-item wow animate__animated animate__fadeInUp"
             data-wow-delay=".3s"
@@ -36,36 +24,10 @@ export default function Users() {
                 <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
                   <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
                   <p className="text-slate-400">
-                    " It seems that only fragments of the original text remain
+                    "It seems that only fragments of the original text remain
                     in the Lorem Ipsum texts used today. "
                   </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/01.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Calvin Carlo</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
+                  <h6 className="mt-2 font-semibold">Calvin Carlo, Manager</h6>
                 </div>
               </div>
             </div>
@@ -75,197 +37,30 @@ export default function Users() {
                 <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
                   <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
                   <p className="text-slate-400">
-                    " The most well-known dummy text is the 'Lorem Ipsum', which
-                    is said to have originated in the 16th century. "
+                    "The most well-known dummy text is the 'Lorem Ipsum', which
+                    is said to have originated in the 16th century."
                   </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/02.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Christa Smith</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
+                  <h6 className="mt-2 font-semibold">Christa Smith, Manager</h6>
                 </div>
               </div>
             </div>
+
 
             <div className="tiny-slide text-center">
               <div className="customer-testi">
                 <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
                   <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
                   <p className="text-slate-400">
-                    " One disadvantage of Lorum Ipsum is that in Latin certain
-                    letters appear more frequently than others. "
+                    "According to most sources, Lorum Ipsum can be traced back
+                    to a text composed by Cicero."
                   </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/03.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Jemina CLone</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiny-slide text-center">
-              <div className="customer-testi">
-                <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
-                  <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
-                  <p className="text-slate-400">
-                    " Thus, Lorem Ipsum has only limited suitability as a visual
-                    filler for German texts. "
-                  </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/04.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Smith Vodka</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiny-slide text-center">
-              <div className="customer-testi">
-                <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
-                  <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
-                  <p className="text-slate-400">
-                    " There is now an abundance of readable dummy texts. These
-                    are usually used when a text is required. "
-                  </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/05.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Cristino Murfi</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiny-slide text-center">
-              <div className="customer-testi">
-                <div className="content relative rounded shadow dark:shadow-gray-800 m-2 p-6 bg-white dark:bg-slate-900">
-                  <i className="mdi mdi-format-quote-open mdi-48px text-indigo-600"></i>
-                  <p className="text-slate-400">
-                    " According to most sources, Lorum Ipsum can be traced back
-                    to a text composed by Cicero. "
-                  </p>
-                  <ul className="list-none mb-0 text-amber-400 mt-3">
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                    <li className="inline">
-                      <i className="mdi mdi-star"></i>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="text-center mt-5">
-                  <img
-                    src="assets/images/client/06.jpg"
-                    className="h-14 w-14 rounded-full shadow-md mx-auto"
-                    alt=""
-                  />
-                  <h6 className="mt-2 font-semibold">Cristino Murfi</h6>
-                  <span className="text-slate-400 text-sm">Manager</span>
+                  <h6 className="mt-2 font-semibold">Cristino Murfi, Manager</h6>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div></div>
       </div>
     </section>
   );
