3584a756d6d9d6c854dd2b7951b76c9e6bd0a9ef
Abin Simon <abinsimon10@gmail.com>
2023-02-27 19:47:45 +0530

    Update src/internal/connector/onedrive/collection.go
    Co-authored-by: Keepers <ryanfkeepers@gmail.com>

--- src/internal/connector/onedrive/collection.go
+++ src/internal/connector/onedrive/collection.go
@@ -372,7 +372,7 @@ func (oc *Collection) populateItems(ctx context.Context, errs *fault.Bus) {
 
 					// check for errors following retries
 					if err != nil {
-						logger.Ctx(ctx).With("error", err.Error()).Infow("unable to download item")
+						logger.Ctx(ctx).With("error", err.Error()).Error("downloading item")
 						el.AddRecoverable(clues.Stack(err).WithClues(ctx).Label(fault.LabelForceNoBackupCreation))
 						return nil, err
 					}
