6cb1cec0415e840272dced61fc5e78e5735e6509
Danny Adams <da469@cornell.edu>
2023-03-01 13:24:45 -0500

    Create file /internal/connector/graph/shared.go Adds common shared file for unwrapping email.Recipients.

---
+++ src/internal/connector/graph/shared.go
@@ -0,0 +1,19 @@
+package graph
+
+// shared.go
+// Contains helper methods that are common across multiple
+// Microsoft Graph Applications.
+
+import (
+	"github.com/alcionai/corso/src/internal/common/ptr"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+)
+
+func UnwrapAddress(contact models.Recipientable) string {
+	var empty string
+	if contact == nil || contact.GetEmailAddress() == nil {
+		return empty
+	}
+
+	return ptr.Val(contact.GetEmailAddress().GetAddress())
+}
