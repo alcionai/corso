f4d8309562a55345ab0a2c6ba92465efd56051b4
Keepers <ryanfkeepers@gmail.com>
2023-03-14 15:50:20 -0600

    add printable interface to items and skips (#2747)
    Extends the fault items and fault skipped structs with print.Printable
    interface compliance. This will allow us to print those values to the
    terminal from a fault.Errors struct.

--- src/pkg/fault/item_test.go
+++ src/pkg/fault/item_test.go
@@ -81,6 +81,76 @@ func (suite *ItemUnitSuite) TestOwnerErr() {
 	assert.Equal(t, expect, *i)
 }
 
+func (suite *ItemUnitSuite) TestItemType_Printable() {
+	table := []struct {
+		t      itemType
+		expect string
+	}{
+		{
+			t:      FileType,
+			expect: "File",
+		},
+		{
+			t:      ContainerType,
+			expect: "Container",
+		},
+		{
+			t:      ResourceOwnerType,
+			expect: "Resource Owner",
+		},
+		{
+			t:      itemType("foo"),
+			expect: "Unknown",
+		},
+	}
+	for _, test := range table {
+		suite.Run(string(test.t), func() {
+			assert.Equal(suite.T(), test.expect, test.t.Printable())
+		})
+	}
+}
+
+func (suite *ItemUnitSuite) TestItem_HeadersValues() {
+	var (
+		err   = assert.AnError
+		cause = err.Error()
+		addtl = map[string]any{
+			AddtlContainerID:   "cid",
+			AddtlContainerName: "cname",
+		}
+	)
+
+	table := []struct {
+		name   string
+		item   *Item
+		expect []string
+	}{
+		{
+			name:   "file",
+			item:   FileErr(assert.AnError, "id", "name", addtl),
+			expect: []string{"Error", FileType.Printable(), "name", "cname", cause},
+		},
+		{
+			name:   "container",
+			item:   ContainerErr(assert.AnError, "id", "name", addtl),
+			expect: []string{"Error", ContainerType.Printable(), "name", "cname", cause},
+		},
+		{
+			name:   "owner",
+			item:   OwnerErr(assert.AnError, "id", "name", nil),
+			expect: []string{"Error", ResourceOwnerType.Printable(), "name", "", cause},
+		},
+	}
+	for _, test := range table {
+		suite.Run(test.name, func() {
+			t := suite.T()
+
+			assert.Equal(t, []string{"Action", "Type", "Name", "Container", "Cause"}, test.item.Headers())
+			assert.Equal(t, test.expect, test.item.Values())
+		})
+	}
+}
+
 func (suite *ItemUnitSuite) TestSkipped_String() {
 	var (
 		t = suite.T()
@@ -142,4 +212,41 @@ func (suite *ItemUnitSuite) TestOwnerSkip() {
 	}
 
 	assert.Equal(t, Skipped{expect}, *i)
+}
+
+func (suite *ItemUnitSuite) TestSkipped_HeadersValues() {
+	addtl := map[string]any{
+		AddtlContainerID:   "cid",
+		AddtlContainerName: "cname",
+	}
+
+	table := []struct {
+		name   string
+		skip   *Skipped
+		expect []string
+	}{
+		{
+			name:   "file",
+			skip:   FileSkip(SkipMalware, "id", "name", addtl),
+			expect: []string{"Skip", FileType.Printable(), "name", "cname", string(SkipMalware)},
+		},
+		{
+			name:   "container",
+			skip:   ContainerSkip(SkipMalware, "id", "name", addtl),
+			expect: []string{"Skip", ContainerType.Printable(), "name", "cname", string(SkipMalware)},
+		},
+		{
+			name:   "owner",
+			skip:   OwnerSkip(SkipMalware, "id", "name", nil),
+			expect: []string{"Skip", ResourceOwnerType.Printable(), "name", "", string(SkipMalware)},
+		},
+	}
+	for _, test := range table {
+		suite.Run(test.name, func() {
+			t := suite.T()
+
+			assert.Equal(t, []string{"Action", "Type", "Name", "Container", "Cause"}, test.skip.Headers())
+			assert.Equal(t, test.expect, test.skip.Values())
+		})
+	}
 }
