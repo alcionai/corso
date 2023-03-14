alcionai/corso

commit 214db6fe97858ecaf84bdb8487f9321c52d9f864

add corso start event at start of cli command (#2534)
## Description

Trigger an event at start of every CLI command. 
## Does this PR need a docs update or release note?

- [ ] :white_check_mark: Yes, it's included

## Type of change

<!--- Please check the type of change your PR introduces: --->
- [x] :sunflower: Feature

## Issue(s)
* https://github.com/alcionai/corso/issues/1761

## Test Plan

<!-- How will this be tested prior to merging.-->
- [ ] :muscle: Manual

---------

Co-authored-by: Keepers <ryanfkeepers@gmail.com>
Co-authored-by: aviator-app[bot] <48659329+aviator-app[bot]@users.noreply.github.com>

src/cli/cli.go
@@ -42,7 +42,8 @@ func preRun(cc *cobra.Command, args []string) error {
 		return err
 	}
 
-	log := logger.Ctx(cc.Context())
+	ctx := cc.Context()
+	log := logger.Ctx(ctx)
 
 	flags := utils.GetPopulatedFlags(cc)
 	flagSl := make([]string, 0, len(flags))
@@ -55,8 +56,31 @@ func preRun(cc *cobra.Command, args []string) error {
 	avoidTheseCommands := []string{
 		"corso", "env", "help", "backup", "details", "list", "restore", "delete", "repo", "init", "connect",
 	}
+
 	if len(logger.LogFile) > 0 && !slices.Contains(avoidTheseCommands, cc.Use) {
-		print.Info(cc.Context(), "Logging to file: "+logger.LogFile)
+		print.Info(ctx, "Logging to file: "+logger.LogFile)
+	}
+
+	avoidTheseCommands = []string{
+		"help for corso",
+		"Initialize a repository.",
+		"Initialize a S3 repository",
+	}
+
+	if !slices.Contains(avoidTheseCommands, cc.Short) {
+		cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
+		if err != nil {
+			log.Error("Error while getting config info to run command: ", cc.Use)
+			return err
+		}
+
+		utils.SendStartCorsoEvent(
+			ctx,
+			cfg.Storage,
+			cfg.Account.ID(),
+			map[string]any{"command": cc.CommandPath()},
+			cfg.RepoID,
+			options.Control())
 	}
 
 	log.Infow("cli command", "command", cc.CommandPath(), "flags", flagSl, "version", version.CurrentVersion())

src/cli/repo/s3.go
@@ -109,22 +109,31 @@ func initS3Cmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, a, err := config.GetStorageAndAccount(ctx, false, s3Overrides())
+	cfg, err := config.GetConfigRepoDetails(ctx, false, s3Overrides())
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	s3Cfg, err := s.S3Config()
+	// SendStartCorsoEvent uses distict ID as tenant ID because repoID is still not generated
+	utils.SendStartCorsoEvent(
+		ctx,
+		cfg.Storage,
+		cfg.Account.ID(),
+		map[string]any{"command": "init repo"},
+		cfg.Account.ID(),
+		options.Control())
+
+	s3Cfg, err := cfg.Storage.S3Config()
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Retrieving s3 configuration"))
 	}
 
-	m365, err := a.M365Config()
+	m365, err := cfg.Account.M365Config()
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to parse m365 account config"))
 	}
 
-	r, err := repository.Initialize(ctx, a, s, options.Control())
+	r, err := repository.Initialize(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
 		if succeedIfExists && errors.Is(err, repository.ErrorRepoAlreadyExists) {
 			return nil
@@ -137,7 +146,7 @@ func initS3Cmd(cmd *cobra.Command, args []string) error {
 
 	Infof(ctx, "Initialized a S3 repository within bucket %s.", s3Cfg.Bucket)
 
-	if err = config.WriteRepoConfig(ctx, s3Cfg, m365); err != nil {
+	if err = config.WriteRepoConfig(ctx, s3Cfg, m365, r.GetID()); err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to write repository configuration"))
 	}
 
@@ -168,22 +177,22 @@ func connectS3Cmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, a, err := config.GetStorageAndAccount(ctx, true, s3Overrides())
+	cfg, err := config.GetConfigRepoDetails(ctx, true, s3Overrides())
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	s3Cfg, err := s.S3Config()
+	s3Cfg, err := cfg.Storage.S3Config()
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Retrieving s3 configuration"))
 	}
 
-	m365, err := a.M365Config()
+	m365, err := cfg.Account.M365Config()
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to parse m365 account config"))
 	}
 
-	r, err := repository.ConnectAndSendConnectEvent(ctx, a, s, options.Control())
+	r, err := repository.ConnectAndSendConnectEvent(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to connect to the S3 repository"))
 	}
@@ -192,7 +201,7 @@ func connectS3Cmd(cmd *cobra.Command, args []string) error {
 
 	Infof(ctx, "Connected to S3 bucket %s.", s3Cfg.Bucket)
 
-	if err = config.WriteRepoConfig(ctx, s3Cfg, m365); err != nil {
+	if err = config.WriteRepoConfig(ctx, s3Cfg, m365, r.GetID()); err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to write repository configuration"))
 	}
 

src/cli/backup/exchange.go
@@ -260,14 +260,14 @@ func createExchangeCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -277,7 +277,7 @@ func createExchangeCmd(cmd *cobra.Command, args []string) error {
 	// TODO: log/print recoverable errors
 	errs := fault.New(false)
 
-	users, err := m365.UserPNs(ctx, acct, errs)
+	users, err := m365.UserPNs(ctx, cfg.Account, errs)
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to retrieve M365 user(s)"))
 	}
@@ -384,14 +384,14 @@ func exchangeListCmd() *cobra.Command {
 func listExchangeCmd(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Context()
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -465,16 +465,16 @@ func detailsExchangeCmd(cmd *cobra.Command, args []string) error {
 		Populated: utils.GetPopulatedFlags(cmd),
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
 	ctrlOpts := options.Control()
 
-	r, err := repository.Connect(ctx, acct, s, ctrlOpts)
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, ctrlOpts)
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -549,14 +549,14 @@ func deleteExchangeCmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)

src/cli/backup/onedrive.go
@@ -184,14 +184,14 @@ func createOneDriveCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -201,7 +201,7 @@ func createOneDriveCmd(cmd *cobra.Command, args []string) error {
 	// TODO: log/print recoverable errors
 	errs := fault.New(false)
 
-	users, err := m365.UserPNs(ctx, acct, errs)
+	users, err := m365.UserPNs(ctx, cfg.Account, errs)
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to retrieve M365 users"))
 	}
@@ -285,14 +285,14 @@ func oneDriveListCmd() *cobra.Command {
 func listOneDriveCmd(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Context()
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -345,16 +345,16 @@ func detailsOneDriveCmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
 	ctrlOpts := options.Control()
 
-	r, err := repository.Connect(ctx, acct, s, ctrlOpts)
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, ctrlOpts)
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -438,14 +438,14 @@ func deleteOneDriveCmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)

src/cli/backup/sharepoint.go
@@ -201,14 +201,14 @@ func createSharePointCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -216,7 +216,7 @@ func createSharePointCmd(cmd *cobra.Command, args []string) error {
 	// TODO: log/print recoverable errors
 	errs := fault.New(false)
 
-	gc, err := connector.NewGraphConnector(ctx, graph.HTTPClient(graph.NoTimeout()), acct, connector.Sites, errs)
+	gc, err := connector.NewGraphConnector(ctx, graph.HTTPClient(graph.NoTimeout()), cfg.Account, connector.Sites, errs)
 	if err != nil {
 		return Only(ctx, errors.Wrap(err, "Failed to connect to Microsoft APIs"))
 	}
@@ -372,14 +372,14 @@ func sharePointListCmd() *cobra.Command {
 func listSharePointCmd(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Context()
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -432,14 +432,14 @@ func deleteSharePointCmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, acct, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)
@@ -476,16 +476,16 @@ func detailsSharePointCmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	s, acct, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
 	ctrlOpts := options.Control()
 
-	r, err := repository.Connect(ctx, acct, s, ctrlOpts)
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, ctrlOpts)
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)

src/cli/config/config.go
@@ -24,6 +24,7 @@ const (
 	PrefixKey                 = "prefix"
 	DisableTLSKey             = "disable_tls"
 	DisableTLSVerificationKey = "disable_tls_verification"
+	RepoID                    = "repo_id"
 
 	// M365 config
 	AccountProviderTypeKey = "account_provider"
@@ -36,6 +37,14 @@ var (
 	configDir          string
 	displayDefaultFP   = filepath.Join("$HOME", ".corso.toml")
 )
+
+// RepoDetails holds the repository configuration retrieved from
+// the .corso.toml configuration file.
+type RepoDetails struct {
+	Storage storage.Storage
+	Account account.Account
+	RepoID  string
+}
 
 // Attempts to set the default dir and config file path.
 // Default is always $HOME.
@@ -178,13 +187,23 @@ func Read(ctx context.Context) error {
 
 // WriteRepoConfig currently just persists corso config to the config file
 // It does not check for conflicts or existing data.
-func WriteRepoConfig(ctx context.Context, s3Config storage.S3Config, m365Config account.M365Config) error {
-	return writeRepoConfigWithViper(GetViper(ctx), s3Config, m365Config)
+func WriteRepoConfig(
+	ctx context.Context,
+	s3Config storage.S3Config,
+	m365Config account.M365Config,
+	repoID string,
+) error {
+	return writeRepoConfigWithViper(GetViper(ctx), s3Config, m365Config, repoID)
 }
 
 // writeRepoConfigWithViper implements WriteRepoConfig, but takes in a viper
 // struct for testing.
-func writeRepoConfigWithViper(vpr *viper.Viper, s3Config storage.S3Config, m365Config account.M365Config) error {
+func writeRepoConfigWithViper(
+	vpr *viper.Viper,
+	s3Config storage.S3Config,
+	m365Config account.M365Config,
+	repoID string,
+) error {
 	s3Config = s3Config.Normalize()
 	// Rudimentary support for persisting repo config
 	// TODO: Handle conflicts, support other config types
@@ -194,6 +213,7 @@ func writeRepoConfigWithViper(vpr *viper.Viper, s3Config storage.S3Config, m365C
 	vpr.Set(PrefixKey, s3Config.Prefix)
 	vpr.Set(DisableTLSKey, s3Config.DoNotUseTLS)
 	vpr.Set(DisableTLSVerificationKey, s3Config.DoNotVerifyTLS)
+	vpr.Set(RepoID, repoID)
 
 	vpr.Set(AccountProviderTypeKey, account.ProviderM365.String())
 	vpr.Set(AzureTenantIDKey, m365Config.AzureTenantID)
@@ -211,12 +231,16 @@ func writeRepoConfigWithViper(vpr *viper.Viper, s3Config storage.S3Config, m365C
 
 // GetStorageAndAccount creates a storage and account instance by mediating all the possible
 // data sources (config file, env vars, flag overrides) and the config file.
-func GetStorageAndAccount(
+func GetConfigRepoDetails(
 	ctx context.Context,
 	readFromFile bool,
 	overrides map[string]string,
-) (storage.Storage, account.Account, error) {
-	return getStorageAndAccountWithViper(GetViper(ctx), readFromFile, overrides)
+) (
+	RepoDetails,
+	error,
+) {
+	config, err := getStorageAndAccountWithViper(GetViper(ctx), readFromFile, overrides)
+	return config, err
 }
 
 // getSorageAndAccountWithViper implements GetSorageAndAccount, but takes in a viper
@@ -225,11 +249,13 @@ func getStorageAndAccountWithViper(
 	vpr *viper.Viper,
 	readFromFile bool,
 	overrides map[string]string,
-) (storage.Storage, account.Account, error) {
+) (
+	RepoDetails,
+	error,
+) {
 	var (
-		store storage.Storage
-		acct  account.Account
-		err   error
+		config RepoDetails
+		err    error
 	)
 
 	readConfigFromViper := readFromFile
@@ -239,24 +265,27 @@ func getStorageAndAccountWithViper(
 		err = vpr.ReadInConfig()
 		if err != nil {
 			if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-				return store, acct, errors.Wrap(err, "reading corso config file: "+vpr.ConfigFileUsed())
+				return config, errors.Wrap(err, "reading corso config file: "+vpr.ConfigFileUsed())
 			}
 
 			readConfigFromViper = false
 		}
+
+		// in case of existing config, fetch repoid from config file
+		config.RepoID = vpr.GetString(RepoID)
 	}
 
-	acct, err = configureAccount(vpr, readConfigFromViper, overrides)
+	config.Account, err = configureAccount(vpr, readConfigFromViper, overrides)
 	if err != nil {
-		return store, acct, errors.Wrap(err, "retrieving account configuration details")
+		return config, errors.Wrap(err, "retrieving account configuration details")
 	}
 
-	store, err = configureStorage(vpr, readConfigFromViper, overrides)
+	config.Storage, err = configureStorage(vpr, readConfigFromViper, overrides)
 	if err != nil {
-		return store, acct, errors.Wrap(err, "retrieving storage provider details")
+		return config, errors.Wrap(err, "retrieving storage provider details")
 	}
 
-	return store, acct, nil
+	return config, nil
 }
 
 // ---------------------------------------------------------------------------

src/cli/config/config_test.go
@@ -87,8 +87,7 @@ func (suite *ConfigSuite) TestWriteReadConfig() {
 
 	s3Cfg := storage.S3Config{Bucket: bkt, DoNotUseTLS: true, DoNotVerifyTLS: true}
 	m365 := account.M365Config{AzureTenantID: tid}
-
-	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365), "writing repo config")
+	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365, "repoid"), "writing repo config")
 	require.NoError(t, vpr.ReadInConfig(), "reading repo config")
 
 	readS3Cfg, err := s3ConfigsFromViper(vpr)
@@ -120,7 +119,7 @@ func (suite *ConfigSuite) TestMustMatchConfig() {
 	s3Cfg := storage.S3Config{Bucket: bkt}
 	m365 := account.M365Config{AzureTenantID: tid}
 
-	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365), "writing repo config")
+	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365, "repoid"), "writing repo config")
 	require.NoError(t, vpr.ReadInConfig(), "reading repo config")
 
 	table := []struct {
@@ -217,25 +216,26 @@ func (suite *ConfigIntegrationSuite) TestGetStorageAndAccount() {
 	}
 	m365 := account.M365Config{AzureTenantID: tid}
 
-	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365), "writing repo config")
+	require.NoError(t, writeRepoConfigWithViper(vpr, s3Cfg, m365, "repoid"), "writing repo config")
 	require.NoError(t, vpr.ReadInConfig(), "reading repo config")
 
-	st, ac, err := getStorageAndAccountWithViper(vpr, true, nil)
+	config, err := getStorageAndAccountWithViper(vpr, true, nil)
 	require.NoError(t, err, "getting storage and account from config")
 
-	readS3Cfg, err := st.S3Config()
+	readS3Cfg, err := config.Storage.S3Config()
 	require.NoError(t, err, "reading s3 config from storage")
 	assert.Equal(t, readS3Cfg.Bucket, s3Cfg.Bucket)
 	assert.Equal(t, readS3Cfg.Endpoint, s3Cfg.Endpoint)
 	assert.Equal(t, readS3Cfg.Prefix, s3Cfg.Prefix)
 	assert.Equal(t, readS3Cfg.DoNotUseTLS, s3Cfg.DoNotUseTLS)
 	assert.Equal(t, readS3Cfg.DoNotVerifyTLS, s3Cfg.DoNotVerifyTLS)
+	assert.Equal(t, config.RepoID, "repoid")
 
-	common, err := st.CommonConfig()
+	common, err := config.Storage.CommonConfig()
 	require.NoError(t, err, "reading common config from storage")
 	assert.Equal(t, common.CorsoPassphrase, os.Getenv(credentials.CorsoPassphrase))
 
-	readM365, err := ac.M365Config()
+	readM365, err := config.Account.M365Config()
 	require.NoError(t, err, "reading m365 config from account")
 	assert.Equal(t, readM365.AzureTenantID, m365.AzureTenantID)
 	assert.Equal(t, readM365.AzureClientID, os.Getenv(credentials.AzureClientID))
@@ -266,22 +266,23 @@ func (suite *ConfigIntegrationSuite) TestGetStorageAndAccount_noFileOnlyOverride
 		StorageProviderTypeKey: storage.ProviderS3.String(),
 	}
 
-	st, ac, err := getStorageAndAccountWithViper(vpr, false, overrides)
+	config, err := getStorageAndAccountWithViper(vpr, false, overrides)
 	require.NoError(t, err, "getting storage and account from config")
 
-	readS3Cfg, err := st.S3Config()
+	readS3Cfg, err := config.Storage.S3Config()
 	require.NoError(t, err, "reading s3 config from storage")
 	assert.Equal(t, readS3Cfg.Bucket, bkt)
+	assert.Equal(t, config.RepoID, "")
 	assert.Equal(t, readS3Cfg.Endpoint, end)
 	assert.Equal(t, readS3Cfg.Prefix, pfx)
 	assert.True(t, readS3Cfg.DoNotUseTLS)
 	assert.True(t, readS3Cfg.DoNotVerifyTLS)
 
-	common, err := st.CommonConfig()
+	common, err := config.Storage.CommonConfig()
 	require.NoError(t, err, "reading common config from storage")
 	assert.Equal(t, common.CorsoPassphrase, os.Getenv(credentials.CorsoPassphrase))
 
-	readM365, err := ac.M365Config()
+	readM365, err := config.Account.M365Config()
 	require.NoError(t, err, "reading m365 config from account")
 	assert.Equal(t, readM365.AzureTenantID, m365.AzureTenantID)
 	assert.Equal(t, readM365.AzureClientID, os.Getenv(credentials.AzureClientID))

src/cli/restore/exchange.go
@@ -204,14 +204,14 @@ func restoreExchangeCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	s, a, err := config.GetStorageAndAccount(ctx, true, nil)
+	cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
 	if err != nil {
 		return Only(ctx, err)
 	}
 
-	r, err := repository.Connect(ctx, a, s, options.Control())
+	r, err := repository.Connect(ctx, cfg.Account, cfg.Storage, options.Control())
 	if err != nil {
-		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", s.Provider))
+		return Only(ctx, errors.Wrapf(err, "Failed to connect to the %s repository", cfg.Storage.Provider))
 	}
 
 	defer utils.CloseRepo(ctx, r)

commit 921542c0f542135f4a897bab5527e4e2a15e53a8

Fix repo connect error when we don't have a config (#2771)
Previously we were failing to connect to the repo if we did not have an existing config file present but could not run init to generate it (had already initialized the repo).

Error: retrieving storage provider details: bucket is required to perform this command

<!-- Insert PR description-->

---

#### Does this PR need a docs update or release note?

- [x] :white_check_mark: Yes, it's included
- [ ] :clock1: Yes, but in a later PR
- [ ] :no_entry: No

#### Type of change

<!--- Please check the type of change your PR introduces: --->
- [ ] :sunflower: Feature
- [x] :bug: Bugfix
- [ ] :world_map: Documentation
- [ ] :robot: Test
- [ ] :computer: CI/Deployment
- [ ] :broom: Tech Debt/Cleanup

#### Issue(s)

<!-- Can reference multiple issues. Use one of the following "magic words" - "closes, fixes" to auto-close the Github issue. -->
* Regression from https://github.com/alcionai/corso/pull/2534

#### Test Plan

<!-- How will this be tested prior to merging.-->
- [ ] :muscle: Manual
- [ ] :zap: Unit test
- [x] :green_heart: E2E

src/cli/cli.go
@@ -68,7 +68,14 @@ func preRun(cc *cobra.Command, args []string) error {
 	}
 
 	if !slices.Contains(avoidTheseDescription, cc.Short) {
-		cfg, err := config.GetConfigRepoDetails(ctx, true, nil)
+		overrides := map[string]string{}
+		if cc.Short == "Connect to a S3 repository" {
+			// Get s3 overrides for connect. Ideally we also need this
+			// for init, but we don't reach this block for init.
+			overrides = repo.S3Overrides()
+		}
+
+		cfg, err := config.GetConfigRepoDetails(ctx, true, overrides)
 		if err != nil {
 			log.Error("Error while getting config info to run command: ", cc.Use)
 			return err

src/cli/repo/s3.go
@@ -109,7 +109,7 @@ func initS3Cmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	cfg, err := config.GetConfigRepoDetails(ctx, false, s3Overrides())
+	cfg, err := config.GetConfigRepoDetails(ctx, false, S3Overrides())
 	if err != nil {
 		return Only(ctx, err)
 	}
@@ -177,7 +177,7 @@ func connectS3Cmd(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	cfg, err := config.GetConfigRepoDetails(ctx, true, s3Overrides())
+	cfg, err := config.GetConfigRepoDetails(ctx, true, S3Overrides())
 	if err != nil {
 		return Only(ctx, err)
 	}
@@ -208,7 +208,7 @@ func connectS3Cmd(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func s3Overrides() map[string]string {
+func S3Overrides() map[string]string {
 	return map[string]string{
 		config.AccountProviderTypeKey: account.ProviderM365.String(),
 		config.StorageProviderTypeKey: storage.ProviderS3.String(),

src/cli/cli_test.go
@@ -11,15 +11,15 @@ import (
 	"github.com/alcionai/corso/src/internal/tester"
 )
 
-type CLISuite struct {
+type CLIUnitSuite struct {
 	tester.Suite
 }
 
-func TestCLISuite(t *testing.T) {
-	suite.Run(t, &CLISuite{Suite: tester.NewUnitSuite(t)})
+func TestCLIUnitSuite(t *testing.T) {
+	suite.Run(t, &CLIUnitSuite{Suite: tester.NewUnitSuite(t)})
 }
 
-func (suite *CLISuite) TestAddCommands_noPanics() {
+func (suite *CLIUnitSuite) TestAddCommands_noPanics() {
 	t := suite.T()
 
 	test := &cobra.Command{

src/cli/repo/s3_e2e_test.go
@@ -1,6 +1,7 @@
 package repo_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,16 +31,24 @@ func TestS3E2ESuite(t *testing.T) {
 
 func (suite *S3E2ESuite) TestInitS3Cmd() {
 	table := []struct {
-		name         string
-		bucketPrefix string
+		name          string
+		bucketPrefix  string
+		hasConfigFile bool
 	}{
 		{
-			name:         "NoPrefix",
-			bucketPrefix: "",
+			name:          "NoPrefix",
+			bucketPrefix:  "",
+			hasConfigFile: true,
 		},
 		{
-			name:         "S3Prefix",
-			bucketPrefix: "s3://",
+			name:          "S3Prefix",
+			bucketPrefix:  "s3://",
+			hasConfigFile: true,
+		},
+		{
+			name:          "NoConfigFile",
+			bucketPrefix:  "",
+			hasConfigFile: false,
 		},
 	}
 
@@ -55,6 +64,11 @@ func (suite *S3E2ESuite) TestInitS3Cmd() {
 			require.NoError(t, err)
 
 			vpr, configFP := tester.MakeTempTestConfigClone(t, nil)
+			if !test.hasConfigFile {
+				// Ideally we could use `/dev/null`, but you need a
+				// toml file plus this works cross platform
+				os.Remove(configFP)
+			}
 
 			ctx = config.SetViper(ctx, vpr)
 
@@ -131,16 +145,24 @@ func (suite *S3E2ESuite) TestInitS3Cmd_missingBucket() {
 
 func (suite *S3E2ESuite) TestConnectS3Cmd() {
 	table := []struct {
-		name         string
-		bucketPrefix string
+		name          string
+		bucketPrefix  string
+		hasConfigFile bool
 	}{
 		{
-			name:         "NoPrefix",
-			bucketPrefix: "",
+			name:          "NoPrefix",
+			bucketPrefix:  "",
+			hasConfigFile: true,
 		},
 		{
-			name:         "S3Prefix",
-			bucketPrefix: "s3://",
+			name:          "S3Prefix",
+			bucketPrefix:  "s3://",
+			hasConfigFile: true,
+		},
+		{
+			name:          "NoConfigFile",
+			bucketPrefix:  "",
+			hasConfigFile: false,
 		},
 	}
 
@@ -161,6 +183,11 @@ func (suite *S3E2ESuite) TestConnectS3Cmd() {
 				tester.TestCfgPrefix:          cfg.Prefix,
 			}
 			vpr, configFP := tester.MakeTempTestConfigClone(t, force)
+			if !test.hasConfigFile {
+				// Ideally we could use `/dev/null`, but you need a
+				// toml file plus this works cross platform
+				os.Remove(configFP)
+			}
 
 			ctx = config.SetViper(ctx, vpr)
 