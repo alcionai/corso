2a3eea45699d55afdb34798e2b8c66d066d465cf
Georgi Matev <gmatev@alcion.ai>
2023-03-14 20:46:34 -0700

    Script to set retention for all mailboxes in a tenant

---
+++ src/cmd/purge/scripts/setRetention.ps1
@@ -0,0 +1,23 @@
+# This is tested on Mac as well as Docker (with m365pnp/powershell image)
+# To run in Docker with the script in the current working diredctory 
+# docker run --rm -it -v "$(pwd):/usr/reset-retnention" -e M365TENANT_ADMIN_USER -e M365TENANT_ADMIN_PASSWORD \
+#                     -w /usr/reset-retnention m365pnp/powershell pwsh -c "./setRetention.ps1"
+Param (
+    [Parameter(Mandatory = $False, HelpMessage = "Exchange Admin email")]
+    [String]$AdminUser = $ENV:M365TENANT_ADMIN_USER,
+
+    [Parameter(Mandatory = $False, HelpMessage = "Exchange Admin password")]
+    [String]$AdminPwd = $ENV:M365TENANT_ADMIN_PASSWORD
+)
+
+# Setup ExchangeOnline
+Install-Module -Name PSWSMan -Force
+Install-Module -Name ExchangeOnlineManagement -Force
+
+$password = convertto-securestring -String "$AdminPwd" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AdminUser, $password
+Connect-ExchangeOnline -Credential $cred
+
+# Set retention values for all mailboxes 
+Get-Mailbox | ForEach-Object { Set-Mailbox -Identity $_.Alias -RetentionHoldEnabled $false  -LitigationHoldEnabled $false -SingleItemRecoveryEnabled $false -RetainDeletedItemsFor 0 -AuditLogAgeLimit 0 -Force }
+Get-Mailbox | ForEach-Object { Start-ManagedFolderAssistant -Identity $_.Alias }
