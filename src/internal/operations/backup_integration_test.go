83536c50eaed5845068861efb1c1b0ab116384cc
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-03-14 18:53:06 -0700

    Fixup integration test item count
    Metadata files aren't currently counted by GraphConnector.

--- src/internal/operations/backup_integration_test.go
+++ src/internal/operations/backup_integration_test.go
@@ -1410,7 +1410,7 @@ func (suite *BackupOpIntegrationSuite) TestBackup_Run_oneDriveIncrementals() {
 
 				containerIDs[container3] = ptr.Val(resp.GetId())
 			},
-			itemsRead:    4, // 2*2 (.data and .meta for 2 files)
+			itemsRead:    2, // 2 .data for 2 files
 			itemsWritten: 6, // read items + 2 directory meta
 		},
 	}
