862a8143c77254caebfd5c49e806ea3eea859130
ryanfkeepers <ryanfkeepers@gmail.com>
2023-03-17 16:28:25 -0600

    linter fix

--- src/internal/connector/graph/service.go
+++ src/internal/connector/graph/service.go
@@ -36,7 +36,7 @@ const (
 	rateLimitHeader           = "RateLimit-Limit"
 	rateRemainingHeader       = "RateLimit-Remaining"
 	rateResetHeader           = "RateLimit-Reset"
-	defaultHttpClientTimeout  = 1 * time.Hour
+	defaultHTTPClientTimeout  = 1 * time.Hour
 )
 
 // AllMetadataFileNames produces the standard set of filenames used to store graph
@@ -200,7 +200,7 @@ func HTTPClient(opts ...option) *http.Client {
 	noOfRetries, minRetryDelay := clientconfig.applyMiddlewareConfig()
 	middlewares := GetKiotaMiddlewares(&clientOptions, noOfRetries, minRetryDelay)
 	httpClient := msgraphgocore.GetDefaultClient(&clientOptions, middlewares...)
-	httpClient.Timeout = defaultHttpClientTimeout
+	httpClient.Timeout = defaultHTTPClientTimeout
 
 	clientconfig.apply(httpClient)
 
