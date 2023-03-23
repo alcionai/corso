82b3d0a62a4ba0738589f71173a60a07a6923b12
ryanfkeepers <ryanfkeepers@gmail.com>
2023-03-23 16:25:19 -0600

    introduce idnamer
    Adds a common interface: idNamer, which is used
    to pass around tuples of an id and a name for some
    resource.  Also adds compliance to this iface in
    selectors, where a selector's ID and Name are the
    DiscreteOwner values.

--- src/pkg/selectors/selectors_test.go
+++ src/pkg/selectors/selectors_test.go
@@ -248,6 +248,47 @@ func (suite *SelectorSuite) TestSplitByResourceOnwer() {
 	}
 }
 
+func (suite *SelectorSuite) TestIDName() {
+	table := []struct {
+		title                string
+		id, name             string
+		expectID, expectName string
+	}{
+		{"empty", "", "", "", ""},
+		{"only id", "id", "", "id", "id"},
+		{"only name", "", "name", "", "name"},
+		{"both", "id", "name", "id", "name"},
+	}
+	for _, test := range table {
+		suite.Run(test.title, func() {
+			sel := Selector{DiscreteOwner: test.id, DiscreteOwnerName: test.name}
+			assert.Equal(suite.T(), test.expectID, sel.ID())
+			assert.Equal(suite.T(), test.expectName, sel.Name())
+		})
+	}
+}
+
+func (suite *SelectorSuite) TestSetDiscreteOwnerIDName() {
+	table := []struct {
+		title                string
+		id, name             string
+		expectID, expectName string
+	}{
+		{"empty", "", "", "", ""},
+		{"only id", "id", "", "id", "id"},
+		{"only name", "", "", "", ""},
+		{"both", "id", "name", "id", "name"},
+	}
+	for _, test := range table {
+		suite.Run(test.title, func() {
+			sel := Selector{}
+			sel = sel.SetDiscreteOwnerIDName(test.id, test.name)
+			assert.Equal(suite.T(), test.expectID, sel.ID())
+			assert.Equal(suite.T(), test.expectName, sel.Name())
+		})
+	}
+}
+
 // TestPathCategories verifies that no scope produces a `path.UnknownCategory`
 func (suite *SelectorSuite) TestPathCategories_includes() {
 	users := []string{"someuser@onmicrosoft.com"}
