0e0e2ee24312bee7b5b3ce6ff3ce196d16b64fa5
ryanfkeepers <ryanfkeepers@gmail.com>
2023-04-13 17:38:01 -0600

    migrate onedrive using prefix collection

--- src/internal/kopia/upload.go
+++ src/internal/kopia/upload.go
@@ -920,8 +920,7 @@ func traverseBaseDir(
 			oldDirPath,
 			currentPath,
 			dEntry,
-			roots,
-		)
+			roots)
 	})
 	if err != nil {
 		return clues.Wrap(err, "traversing base directory")
@@ -1021,6 +1020,11 @@ func inflateBaseTree(
 			return clues.Wrap(err, "subtree root is not directory").WithClues(ictx)
 		}
 
+		newSubtreePath := subtreePath
+		if p, ok := updatedPaths[subtreePath.String()]; ok {
+			newSubtreePath = p.ToBuilder()
+		}
+
 		// We're assuming here that the prefix for the path has not changed (i.e.
 		// all of tenant, service, resource owner, and category are the same in the
 		// old snapshot (snap) and the snapshot we're currently trying to make.
@@ -1029,7 +1033,7 @@ func inflateBaseTree(
 			0,
 			updatedPaths,
 			subtreePath.Dir(),
-			subtreePath.Dir(),
+			newSubtreePath.Dir(),
 			subtreeDir,
 			roots,
 		); err != nil {
