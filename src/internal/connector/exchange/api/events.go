2707c672ef427107e9d11391bc61528c1c0b8d9a
Abin <mail@meain.io>
2023-04-26 11:13:10 +0530

    Address review comments

--- src/internal/connector/exchange/api/events.go
+++ src/internal/connector/exchange/api/events.go
@@ -254,15 +254,15 @@ func (c Events) EnumerateContainers(
 	return el.Failure()
 }
 
+const (
+	eventBetaDeltaURLTemplate = "https://graph.microsoft.com/beta/users/%s/calendars/%s/events/delta"
+)
+
 // ---------------------------------------------------------------------------
 // item pager
 // ---------------------------------------------------------------------------
 
-var _ itemPager = &eventDeltaPager{}
-
-const (
-	eventBetaDeltaURLTemplate = "https://graph.microsoft.com/beta/users/%s/calendars/%s/events/delta"
-)
+var _ itemPager = &eventPager{}
 
 type eventPager struct {
 	gs      graph.Servicer
@@ -310,6 +310,12 @@ func (p *eventPager) setNext(nextLink string) {
 func (p *eventPager) valuesIn(pl api.PageLinker) ([]getIDAndAddtler, error) {
 	return toValues[models.Eventable](pl)
 }
+
+// ---------------------------------------------------------------------------
+// non-delta item pager
+// ---------------------------------------------------------------------------
+
+var _ itemPager = &eventDeltaPager{}
 
 type eventDeltaPager struct {
 	gs      graph.Servicer
