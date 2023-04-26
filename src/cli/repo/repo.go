bb642f9ece8437e14b4266d98d05977a699dd629
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-04-25 18:38:09 -0700

    Add maintenance CLI command

--- src/cli/repo/repo.go
+++ src/cli/repo/repo.go
@@ -1,12 +1,21 @@
 package repo
 
 import (
+	"strings"
+
+	"github.com/alcionai/clues"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/maps"
+
+	"github.com/alcionai/corso/src/cli/print"
+	"github.com/alcionai/corso/src/cli/utils"
+	"github.com/alcionai/corso/src/pkg/control"
 )
 
 const (
-	initCommand    = "init"
-	connectCommand = "connect"
+	initCommand        = "init"
+	connectCommand     = "connect"
+	maintenanceCommand = "maintenance"
 )
 
 var repoCommands = []func(cmd *cobra.Command) *cobra.Command{
@@ -18,14 +27,24 @@ func AddCommands(cmd *cobra.Command) {
 	var (
 		// Get new instances so that setting the context during tests works
 		// properly.
-		repoCmd    = repoCmd()
-		initCmd    = initCmd()
-		connectCmd = connectCmd()
+		repoCmd        = repoCmd()
+		initCmd        = initCmd()
+		connectCmd     = connectCmd()
+		maintenanceCmd = maintenanceCmd()
 	)
 
 	cmd.AddCommand(repoCmd)
 	repoCmd.AddCommand(initCmd)
 	repoCmd.AddCommand(connectCmd)
+
+	utils.AddCommand(
+		repoCmd,
+		maintenanceCmd,
+		utils.HideCommand(),
+		utils.MarkPreReleaseCommand())
+	utils.AddMaintenanceSafetyFlag(maintenanceCmd)
+	utils.AddQuickMaintenanceFlag(maintenanceCmd)
+	utils.AddForceMaintenanceFlag(maintenanceCmd)
 
 	for _, addRepoTo := range repoCommands {
 		addRepoTo(initCmd)
@@ -83,4 +102,66 @@ func connectCmd() *cobra.Command {
 // Handler for calls to `corso repo connect`.
 func handleConnectCmd(cmd *cobra.Command, args []string) error {
 	return cmd.Help()
+}
+
+func maintenanceCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   maintenanceCommand,
+		Short: "Run maintenance on an existing repository",
+		Long:  `Run maintenance on an existing repository to keep it running smoothly`,
+		RunE:  handleMaintenanceCmd,
+		Args:  cobra.NoArgs,
+	}
+}
+
+func handleMaintenanceCmd(cmd *cobra.Command, args []string) error {
+	ctx := cmd.Context()
+
+	s := control.Safety(utils.MaintenanceSafetyFV)
+	if err := validateMaintenanceFlags(s); err != nil {
+		return err
+	}
+
+	r, _, err := utils.GetAccountAndConnect(ctx)
+	if err != nil {
+		return print.Only(ctx, err)
+	}
+
+	defer utils.CloseRepo(ctx, r)
+
+	m, err := r.NewMaintenance(
+		ctx,
+		s,
+		utils.QuickMaintenanceFV,
+		utils.ForceMaintenanceFV)
+	if err != nil {
+		return print.Only(ctx, err)
+	}
+
+	err = m.Run(ctx)
+	if err != nil {
+		return print.Only(ctx, err)
+	}
+
+	return nil
+}
+
+func validateMaintenanceFlags(s control.Safety) error {
+	_, ok := control.SafetyValues[s]
+	if !ok {
+		values := maps.Keys(control.SafetyValues)
+		allButLast := []string{}
+
+		for i := 0; i < len(values)-1; i++ {
+			allButLast = append(allButLast, string(values[i]))
+		}
+
+		valuesStr := strings.Join(allButLast, ", ") + " or " + string(values[len(values)-1])
+
+		return clues.New(
+			string(s) + " is an unrecognized safety level; must be one of " +
+				valuesStr)
+	}
+
+	return nil
 }
