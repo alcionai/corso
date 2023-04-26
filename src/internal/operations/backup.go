alcionai/corso

commit 62daf10213ce29782752924bc1c53a44a65dfe01

migrate onedrive using prefix collection (#3122)
#### Does this PR need a docs update or release note?

- [x] :clock1: Yes, but in a later PR

#### Type of change

- [x] :sunflower: Feature

#### Issue(s)

* #2825

#### Test Plan

- [x] :muscle: Manual
- [x] :zap: Unit test
- [x] :green_heart: E2E

src/internal/operations/backup.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/alcionai/corso/src/internal/common"
 	"github.com/alcionai/corso/src/internal/common/crash"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/internal/diagnostics"
 	"github.com/alcionai/corso/src/internal/events"
@@ -18,6 +19,7 @@ import (
 	"github.com/alcionai/corso/src/internal/operations/inject"
 	"github.com/alcionai/corso/src/internal/stats"
 	"github.com/alcionai/corso/src/internal/streamstore"
+	"github.com/alcionai/corso/src/internal/version"
 	"github.com/alcionai/corso/src/pkg/account"
 	"github.com/alcionai/corso/src/pkg/backup"
 	"github.com/alcionai/corso/src/pkg/backup/details"
@@ -33,11 +35,18 @@ import (
 type BackupOperation struct {
 	operation
 
-	ResourceOwner common.IDNamer
+	ResourceOwner idname.Provider
 
 	Results   BackupResults      `json:"results"`
 	Selectors selectors.Selector `json:"selectors"`
 	Version   string             `json:"version"`
+
+	// backupVersion ONLY controls the value that gets persisted to the
+	// backup model after operation.  It does NOT modify the operation behavior
+	// to match the version.  Its inclusion here is, unfortunately, purely to
+	// facilitate integration testing that requires a certain backup version, and
+	// should be removed when we have a more controlled workaround.
+	backupVersion int
 
 	account account.Account
 	bp      inject.BackupProducer
@@ -62,7 +71,7 @@ func NewBackupOperation(
 	bp inject.BackupProducer,
 	acct account.Account,
 	selector selectors.Selector,
-	owner common.IDNamer,
+	owner idname.Provider,
 	bus events.Eventer,
 ) (BackupOperation, error) {
 	op := BackupOperation{
@@ -70,6 +79,7 @@ func NewBackupOperation(
 		ResourceOwner: owner,
 		Selectors:     selector,
 		Version:       "v0",
+		backupVersion: version.Backup,
 		account:       acct,
 		incremental:   useIncrementalBackup(selector, opts),
 		bp:            bp,
@@ -210,6 +220,7 @@ func (op *BackupOperation) Run(ctx context.Context) (err error) {
 		sstore,
 		opStats.k.SnapshotID,
 		op.Results.BackupID,
+		op.backupVersion,
 		deets.Details())
 	if err != nil {
 		op.Errors.Fail(clues.Wrap(err, "persisting backup"))
@@ -253,12 +264,18 @@ func (op *BackupOperation) do(
 		return nil, clues.Wrap(err, "producing manifests and metadata")
 	}
 
+	_, lastBackupVersion, err := lastCompleteBackups(ctx, op.store, mans)
+	if err != nil {
+		return nil, clues.Wrap(err, "retrieving prior backups")
+	}
+
 	cs, excludes, err := produceBackupDataCollections(
 		ctx,
 		op.bp,
 		op.ResourceOwner,
 		op.Selectors,
 		mdColls,
+		lastBackupVersion,
 		op.Options,
 		op.Errors)
 	if err != nil {
@@ -333,9 +350,10 @@ func useIncrementalBackup(sel selectors.Selector, opts control.Options) bool {
 func produceBackupDataCollections(
 	ctx context.Context,
 	bp inject.BackupProducer,
-	resourceOwner common.IDNamer,
+	resourceOwner idname.Provider,
 	sel selectors.Selector,
 	metadata []data.RestoreCollection,
+	lastBackupVersion int,
 	ctrlOpts control.Options,
 	errs *fault.Bus,
 ) ([]data.BackupCollection, map[string]map[string]struct{}, error) {
@@ -346,7 +364,7 @@ func produceBackupDataCollections(
 		closer()
 	}()
 
-	return bp.ProduceBackupCollections(ctx, resourceOwner, sel, metadata, ctrlOpts, errs)
+	return bp.ProduceBackupCollections(ctx, resourceOwner, sel, metadata, lastBackupVersion, ctrlOpts, errs)
 }
 
 // ---------------------------------------------------------------------------
@@ -585,6 +603,56 @@ func getNewPathRefs(
 	return newPath, newLoc, updated, nil
 }
 
+func lastCompleteBackups(
+	ctx context.Context,
+	ms *store.Wrapper,
+	mans []*kopia.ManifestEntry,
+) (map[string]*backup.Backup, int, error) {
+	var (
+		oldestVersion = version.NoBackup
+		result        = map[string]*backup.Backup{}
+	)
+
+	if len(mans) == 0 {
+		return result, -1, nil
+	}
+
+	for _, man := range mans {
+		// For now skip snapshots that aren't complete. We will need to revisit this
+		// when we tackle restartability.
+		if len(man.IncompleteReason) > 0 {
+			continue
+		}
+
+		var (
+			mctx    = clues.Add(ctx, "base_manifest_id", man.ID)
+			reasons = man.Reasons
+		)
+
+		bID, ok := man.GetTag(kopia.TagBackupID)
+		if !ok {
+			return result, oldestVersion, clues.New("no backup ID in snapshot manifest").WithClues(mctx)
+		}
+
+		mctx = clues.Add(mctx, "base_manifest_backup_id", bID)
+
+		bup, err := getBackupFromID(mctx, model.StableID(bID), ms)
+		if err != nil {
+			return result, oldestVersion, err
+		}
+
+		for _, r := range reasons {
+			result[r.Key()] = bup
+		}
+
+		if oldestVersion == -1 || bup.Version < oldestVersion {
+			oldestVersion = bup.Version
+		}
+	}
+
+	return result, oldestVersion, nil
+}
+
 func mergeDetails(
 	ctx context.Context,
 	ms *store.Wrapper,
@@ -627,7 +695,7 @@ func mergeDetails(
 			detailsStore,
 			errs)
 		if err != nil {
-			return clues.New("fetching base details for backup").WithClues(mctx)
+			return clues.New("fetching base details for backup")
 		}
 
 		for _, entry := range baseDeets.Items() {
@@ -749,6 +817,7 @@ func (op *BackupOperation) createBackupModels(
 	sscw streamstore.CollectorWriter,
 	snapID string,
 	backupID model.StableID,
+	backupVersion int,
 	deets *details.Details,
 ) error {
 	ctx = clues.Add(ctx, "snapshot_id", snapID, "backup_id", backupID)
@@ -783,6 +852,7 @@ func (op *BackupOperation) createBackupModels(
 	b := backup.New(
 		snapID, ssid,
 		op.Status.String(),
+		backupVersion,
 		backupID,
 		op.Selectors,
 		op.ResourceOwner.ID(),

src/cli/backup/backup.go
@@ -13,7 +13,7 @@ import (
 	"github.com/alcionai/corso/src/cli/options"
 	. "github.com/alcionai/corso/src/cli/print"
 	"github.com/alcionai/corso/src/cli/utils"
-	"github.com/alcionai/corso/src/internal/common"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/pkg/account"
 	"github.com/alcionai/corso/src/pkg/backup"
@@ -198,7 +198,7 @@ func runBackups(
 	r repository.Repository,
 	serviceName, resourceOwnerType string,
 	selectorSet []selectors.Selector,
-	ins common.IDNameSwapper,
+	ins idname.Cacher,
 ) error {
 	var (
 		bIDs []string

src/internal/version/backup.go
@@ -9,6 +9,9 @@ const Backup = 7
 // Labels should state their application, the backup version number,
 // and the colloquial purpose of the label.
 const (
+	// NoBackup should be used when we cannot find, or do not supply, prior backup metadata.
+	NoBackup = -1
+
 	// OneDrive1DataAndMetaFiles is the corso backup format version
 	// in which we split from storing just the data to storing both
 	// the data and metadata in two files.
@@ -39,4 +42,13 @@ const (
 	// OneDriveXLocationRef provides LocationRef information for Exchange,
 	// OneDrive, and SharePoint libraries.
 	OneDrive7LocationRef = 7
+
+	// AllXMigrateUserPNToID marks when we migrated repo refs from the user's
+	// PrincipalName to their ID for stability.
+	AllXMigrateUserPNToID = Backup + 1
 )
+
+// IsNoBackup returns true if the version implies that no prior backup exists.
+func IsNoBackup(version int) bool {
+	return version <= NoBackup
+}

src/pkg/backup/backup.go
@@ -13,7 +13,6 @@ import (
 	"github.com/alcionai/corso/src/internal/common"
 	"github.com/alcionai/corso/src/internal/model"
 	"github.com/alcionai/corso/src/internal/stats"
-	"github.com/alcionai/corso/src/internal/version"
 	"github.com/alcionai/corso/src/pkg/fault"
 	"github.com/alcionai/corso/src/pkg/selectors"
 )
@@ -67,6 +66,7 @@ var _ print.Printable = &Backup{}
 
 func New(
 	snapshotID, streamStoreID, status string,
+	version int,
 	id model.StableID,
 	selector selectors.Selector,
 	ownerID, ownerName string,
@@ -116,7 +116,7 @@ func New(
 		ResourceOwnerID:   ownerID,
 		ResourceOwnerName: ownerName,
 
-		Version:       version.Backup,
+		Version:       version,
 		SnapshotID:    snapshotID,
 		StreamStoreID: streamStoreID,
 

src/cli/backup/exchange_e2e_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/alcionai/corso/src/cli/config"
 	"github.com/alcionai/corso/src/cli/print"
 	"github.com/alcionai/corso/src/cli/utils"
-	"github.com/alcionai/corso/src/internal/common"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/connector/exchange"
 	"github.com/alcionai/corso/src/internal/operations"
 	"github.com/alcionai/corso/src/internal/tester"
@@ -256,13 +256,8 @@ func (suite *PreparedBackupExchangeE2ESuite) SetupSuite() {
 	suite.backupOps = make(map[path.CategoryType]string)
 
 	var (
-		users    = []string{suite.m365UserID}
-		idToName = map[string]string{suite.m365UserID: suite.m365UserID}
-		nameToID = map[string]string{suite.m365UserID: suite.m365UserID}
-		ins      = common.IDsNames{
-			IDToName: idToName,
-			NameToID: nameToID,
-		}
+		users = []string{suite.m365UserID}
+		ins   = idname.NewCache(map[string]string{suite.m365UserID: suite.m365UserID})
 	)
 
 	for _, set := range []path.CategoryType{email, contacts, events} {

src/cli/backup/onedrive_e2e_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/alcionai/corso/src/cli/config"
 	"github.com/alcionai/corso/src/cli/print"
 	"github.com/alcionai/corso/src/cli/utils"
-	"github.com/alcionai/corso/src/internal/common"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/operations"
 	"github.com/alcionai/corso/src/internal/tester"
 	"github.com/alcionai/corso/src/pkg/account"
@@ -171,12 +171,7 @@ func (suite *BackupDeleteOneDriveE2ESuite) SetupSuite() {
 	var (
 		m365UserID = tester.M365UserID(t)
 		users      = []string{m365UserID}
-		idToName   = map[string]string{m365UserID: m365UserID}
-		nameToID   = map[string]string{m365UserID: m365UserID}
-		ins        = common.IDsNames{
-			IDToName: idToName,
-			NameToID: nameToID,
-		}
+		ins        = idname.NewCache(map[string]string{m365UserID: m365UserID})
 	)
 
 	// some tests require an existing backup

src/cli/backup/sharepoint.go
@@ -12,7 +12,7 @@ import (
 	"github.com/alcionai/corso/src/cli/options"
 	. "github.com/alcionai/corso/src/cli/print"
 	"github.com/alcionai/corso/src/cli/utils"
-	"github.com/alcionai/corso/src/internal/common"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/pkg/backup/details"
 	"github.com/alcionai/corso/src/pkg/fault"
@@ -203,7 +203,7 @@ func validateSharePointBackupCreateFlags(sites, weburls, cats []string) error {
 // TODO: users might specify a data type, this only supports AllData().
 func sharePointBackupCreateSelectors(
 	ctx context.Context,
-	ins common.IDNameSwapper,
+	ins idname.Cacher,
 	sites, weburls, cats []string,
 ) (*selectors.SharePointBackup, error) {
 	if len(sites) == 0 && len(weburls) == 0 {
@@ -223,7 +223,7 @@ func sharePointBackupCreateSelectors(
 	return addCategories(sel, cats), nil
 }
 
-func includeAllSitesWithCategories(ins common.IDNameSwapper, categories []string) *selectors.SharePointBackup {
+func includeAllSitesWithCategories(ins idname.Cacher, categories []string) *selectors.SharePointBackup {
 	return addCategories(selectors.NewSharePointBackup(ins.IDs()), categories)
 }
 

src/cli/backup/sharepoint_e2e_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/alcionai/corso/src/cli/config"
 	"github.com/alcionai/corso/src/cli/print"
 	"github.com/alcionai/corso/src/cli/utils"
-	"github.com/alcionai/corso/src/internal/common"
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/operations"
 	"github.com/alcionai/corso/src/internal/tester"
 	"github.com/alcionai/corso/src/pkg/account"
@@ -135,12 +135,7 @@ func (suite *BackupDeleteSharePointE2ESuite) SetupSuite() {
 	var (
 		m365SiteID = tester.M365SiteID(t)
 		sites      = []string{m365SiteID}
-		idToName   = map[string]string{m365SiteID: m365SiteID}
-		nameToID   = map[string]string{m365SiteID: m365SiteID}
-		ins        = common.IDsNames{
-			IDToName: idToName,
-			NameToID: nameToID,
-		}
+		ins        = idname.NewCache(map[string]string{m365SiteID: m365SiteID})
 	)
 
 	// some tests require an existing backup

commit c436533966a844a521a507598778cc57432dc7ff

Don't try to fetch backup version if no compete backups available
Regression from https://github.com/alcionai/corso/commit/62daf10213ce29782752924bc1c53a44a65dfe01#diff-f088d3a5f56348ddaf6fbf485c2aeaa6d8b40a860de014f7084db6daf0753829R267

src/internal/operations/backup.go
@@ -241,8 +241,9 @@ func (op *BackupOperation) do(
 	backupID model.StableID,
 ) (*details.Builder, error) {
 	var (
-		reasons         = selectorToReasons(op.Selectors, false)
-		fallbackReasons = makeFallbackReasons(op.Selectors)
+		reasons           = selectorToReasons(op.Selectors, false)
+		fallbackReasons   = makeFallbackReasons(op.Selectors)
+		lastBackupVersion = -1
 	)
 
 	logger.Ctx(ctx).With(
@@ -264,9 +265,11 @@ func (op *BackupOperation) do(
 		return nil, clues.Wrap(err, "producing manifests and metadata")
 	}
 
-	_, lastBackupVersion, err := lastCompleteBackups(ctx, op.store, mans)
-	if err != nil {
-		return nil, clues.Wrap(err, "retrieving prior backups")
+	if canUseMetaData {
+		_, lastBackupVersion, err = lastCompleteBackups(ctx, op.store, mans)
+		if err != nil {
+			return nil, clues.Wrap(err, "retrieving prior backups")
+		}
 	}
 
 	cs, excludes, err := produceBackupDataCollections(