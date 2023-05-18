7c820f8fe98a70cbb7d9f3f2dccef3388fe87ddb
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-05-18 13:29:49 -0700

    Run selectors tests on all versions

--- src/pkg/selectors/selectors_reduce_test.go
+++ src/pkg/selectors/selectors_reduce_test.go
@@ -1,6 +1,7 @@
 package selectors_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -9,9 +10,11 @@ import (
 
 	"github.com/alcionai/corso/src/internal/common/dttm"
 	"github.com/alcionai/corso/src/internal/tester"
+	"github.com/alcionai/corso/src/internal/version"
 	"github.com/alcionai/corso/src/pkg/backup/details"
 	"github.com/alcionai/corso/src/pkg/backup/details/testdata"
 	"github.com/alcionai/corso/src/pkg/fault"
+	"github.com/alcionai/corso/src/pkg/path"
 	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
@@ -27,25 +30,31 @@ func (suite *SelectorReduceSuite) TestReduce() {
 	ctx, flush := tester.NewContext()
 	defer flush()
 
-	allDetails := testdata.GetDetailsSet()
 	table := []struct {
 		name     string
-		selFunc  func() selectors.Reducer
-		expected []details.Entry
+		selFunc  func(t *testing.T, wantVersion int) selectors.Reducer
+		expected func(t *testing.T, wantVersion int) []details.Entry
 	}{
 		{
 			name: "ExchangeAllMail",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.Mails(selectors.Any(), selectors.Any()))
 
 				return sel
 			},
-			expected: testdata.ExchangeEmailItems,
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					-1)
+			},
 		},
 		{
 			name: "ExchangeMailFolderPrefixMatch",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.MailFolders(
 					[]string{testdata.ExchangeEmailInboxPath.FolderLocation()},
@@ -53,48 +62,79 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: testdata.ExchangeEmailItems,
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					-1)
+			},
 		},
 		{
 			name: "ExchangeMailSubject",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Filter(sel.MailSubject("foo"))
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeMailSubjectExcludeItem",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
+				deets := testdata.GetDeetsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion)
+
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Filter(sel.MailSender("a-person"))
 				sel.Exclude(sel.Mails(
 					selectors.Any(),
-					[]string{testdata.ExchangeEmailItemPath2.RR.ShortRef()},
+					[]string{deets[1].ShortRef},
 				))
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeMailSender",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Filter(sel.MailSender("a-person"))
 
 				return sel
 			},
-			expected: []details.Entry{
-				testdata.ExchangeEmailItems[0],
-				testdata.ExchangeEmailItems[1],
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0, 1)
 			},
 		},
 		{
 			name: "ExchangeMailReceivedTime",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Filter(sel.MailReceivedBefore(
 					dttm.Format(testdata.Time1.Add(time.Second)),
@@ -102,11 +142,18 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeMailID",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.Mails(
 					selectors.Any(),
@@ -115,24 +162,44 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeMailShortRef",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
+				deets := testdata.GetDeetsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion)
+
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.Mails(
 					selectors.Any(),
-					[]string{testdata.ExchangeEmailItemPath1.RR.ShortRef()},
+					[]string{deets[0].ShortRef},
 				))
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeAllEventsAndMailWithSubject",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.Events(
 					selectors.Any(),
@@ -142,39 +209,62 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeEventsAndMailWithSubject",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Filter(sel.EventSubject("foo"))
 				sel.Filter(sel.MailSubject("foo"))
 
 				return sel
 			},
-			expected: []details.Entry{},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return []details.Entry{}
+			},
 		},
 		{
 			name: "ExchangeAll",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.AllData())
 
 				return sel
 			},
-			expected: append(
-				append(
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return append(
 					append(
-						[]details.Entry{},
-						testdata.ExchangeEmailItems...),
-					testdata.ExchangeContactsItems...),
-				testdata.ExchangeEventsItems...,
-			),
+						testdata.GetItemsForVersion(
+							t,
+							path.ExchangeService,
+							path.EmailCategory,
+							wantVersion,
+							-1),
+						testdata.GetItemsForVersion(
+							t,
+							path.ExchangeService,
+							path.EventsCategory,
+							wantVersion,
+							-1)...),
+					testdata.GetItemsForVersion(
+						t,
+						path.ExchangeService,
+						path.ContactsCategory,
+						wantVersion,
+						-1)...)
+			},
 		},
 		{
 			name: "ExchangeMailByFolder",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.MailFolders(
 					[]string{testdata.ExchangeEmailBasePath.FolderLocation()},
@@ -182,14 +272,21 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		// TODO (keepers): all folders are treated as prefix-matches at this time.
 		// so this test actually does nothing different.  In the future, we'll
 		// need to amend the non-prefix folder tests to expect non-prefix matches.
 		{
 			name: "ExchangeMailByFolderPrefix",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.MailFolders(
 					[]string{testdata.ExchangeEmailBasePath.FolderLocation()},
@@ -198,11 +295,18 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEmailItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeMailByFolderRoot",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.MailFolders(
 					[]string{testdata.ExchangeEmailInboxPath.FolderLocation()},
@@ -210,11 +314,18 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: testdata.ExchangeEmailItems,
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EmailCategory,
+					wantVersion,
+					-1)
+			},
 		},
 		{
 			name: "ExchangeContactByFolder",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.ContactFolders(
 					[]string{testdata.ExchangeContactsBasePath.FolderLocation()},
@@ -222,11 +333,18 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeContactsItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.ContactsCategory,
+					wantVersion,
+					0)
+			},
 		},
 		{
 			name: "ExchangeContactByFolderRoot",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.ContactFolders(
 					[]string{testdata.ExchangeContactsRootPath.FolderLocation()},
@@ -234,12 +352,19 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: testdata.ExchangeContactsItems,
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.ContactsCategory,
+					wantVersion,
+					-1)
+			},
 		},
 
 		{
 			name: "ExchangeEventsByFolder",
-			selFunc: func() selectors.Reducer {
+			selFunc: func(t *testing.T, wantVersion int) selectors.Reducer {
 				sel := selectors.NewExchangeRestore(selectors.Any())
 				sel.Include(sel.EventCalendars(
 					[]string{testdata.ExchangeEventsBasePath.FolderLocation()},
@@ -247,16 +372,28 @@ func (suite *SelectorReduceSuite) TestReduce() {
 
 				return sel
 			},
-			expected: []details.Entry{testdata.ExchangeEventsItems[0]},
+			expected: func(t *testing.T, wantVersion int) []details.Entry {
+				return testdata.GetItemsForVersion(
+					t,
+					path.ExchangeService,
+					path.EventsCategory,
+					wantVersion,
+					0)
+			},
 		},
 	}
 
-	for _, test := range table {
-		suite.Run(test.name, func() {
-			t := suite.T()
+	for v := 0; v <= version.Backup; v++ {
+		suite.Run(fmt.Sprintf("version%d", v), func() {
+			for _, test := range table {
+				suite.Run(test.name, func() {
+					t := suite.T()
 
-			output := test.selFunc().Reduce(ctx, allDetails, fault.New(true))
-			assert.ElementsMatch(t, test.expected, output.Entries)
+					allDetails := testdata.GetDetailsSetForVersion(t, v)
+					output := test.selFunc(t, v).Reduce(ctx, allDetails, fault.New(true))
+					assert.ElementsMatch(t, test.expected(t, v), output.Entries)
+				})
+			}
 		})
 	}
 }
