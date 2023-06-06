78e251f145819665a3dc96a971aa5b02fc7a25bb
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-06-06 16:45:28 -0700

    Update tests for new return type

--- src/internal/operations/backup_integration_test.go
+++ src/internal/operations/backup_integration_test.go
@@ -232,10 +232,8 @@ func checkBackupIsInManifests(
 			bf, err := kw.NewBaseFinder(bo.store)
 			require.NoError(t, err, clues.ToCore(err))
 
-			mans, err := bf.FindBases(ctx, reasons, tags)
-			require.NoError(t, err, clues.ToCore(err))
-
-			for _, man := range mans {
+			mans := bf.FindBases(ctx, reasons, tags)
+			for _, man := range mans.MergeBases() {
 				bID, ok := man.GetTag(kopia.TagBackupID)
 				if !assert.Truef(t, ok, "snapshot manifest %s missing backup ID tag", man.ID) {
 					continue
