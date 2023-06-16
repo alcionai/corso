d83c9b58eb5ca9853b7e69e7b0ff0380b9f73d72
ryanfkeepers <ryanfkeepers@gmail.com>
2023-06-16 11:36:01 -0600

    properly handle collision replace behavior
    completes the item collision handling behavior in
    exchange by turning replace handling into a post-delete
    process.

--- src/internal/m365/exchange/events_restore_test.go
+++ src/internal/m365/exchange/events_restore_test.go
@@ -21,22 +21,34 @@ import (
 	"github.com/alcionai/corso/src/pkg/services/m365/api"
 )
 
-var _ eventRestorer = &mockEventRestorer{}
+var _ eventRestorer = &eventRestoreMock{}
 
-type mockEventRestorer struct {
+type eventRestoreMock struct {
 	postItemErr       error
+	calledPost        bool
+	deleteItemErr     error
+	calledDelete      bool
 	postAttachmentErr error
 }
 
-func (m mockEventRestorer) PostItem(
-	ctx context.Context,
-	userID, containerID string,
-	body models.Eventable,
+func (m *eventRestoreMock) PostItem(
+	_ context.Context,
+	_, _ string,
+	_ models.Eventable,
 ) (models.Eventable, error) {
+	m.calledPost = true
 	return models.NewEvent(), m.postItemErr
 }
 
-func (m mockEventRestorer) PostSmallAttachment(
+func (m *eventRestoreMock) DeleteItem(
+	_ context.Context,
+	_, _ string,
+) error {
+	m.calledDelete = true
+	return m.deleteItemErr
+}
+
+func (m *eventRestoreMock) PostSmallAttachment(
 	_ context.Context,
 	_, _, _ string,
 	_ models.Attachmentable,
@@ -44,7 +56,7 @@ func (m mockEventRestorer) PostSmallAttachment(
 	return m.postAttachmentErr
 }
 
-func (m mockEventRestorer) PostLargeAttachment(
+func (m *eventRestoreMock) PostLargeAttachment(
 	_ context.Context,
 	_, _, _, _ string,
 	_ []byte,
@@ -133,63 +145,88 @@ func (suite *EventsRestoreIntgSuite) TestRestoreEvent() {
 
 	table := []struct {
 		name         string
-		apiMock      eventRestorer
+		apiMock      *eventRestoreMock
 		collisionMap map[string]string
 		onCollision  control.CollisionPolicy
 		expectErr    func(*testing.T, error)
+		expectMock   func(*testing.T, *eventRestoreMock)
 	}{
 		{
 			name:         "no collision: skip",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{},
 			onCollision:  control.Copy,
 			expectErr: func(t *testing.T, err error) {
 				assert.NoError(t, err, clues.ToCore(err))
+			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.True(t, m.calledPost, "new item posted")
+				assert.False(t, m.calledDelete, "old item deleted")
 			},
 		},
 		{
 			name:         "no collision: copy",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{},
 			onCollision:  control.Skip,
 			expectErr: func(t *testing.T, err error) {
 				assert.NoError(t, err, clues.ToCore(err))
 			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.True(t, m.calledPost, "new item posted")
+				assert.False(t, m.calledDelete, "old item deleted")
+			},
 		},
 		{
 			name:         "no collision: replace",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{},
 			onCollision:  control.Replace,
 			expectErr: func(t *testing.T, err error) {
 				assert.NoError(t, err, clues.ToCore(err))
 			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.True(t, m.calledPost, "new item posted")
+				assert.False(t, m.calledDelete, "old item deleted")
+			},
 		},
 		{
 			name:         "collision: skip",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{collisionKey: "smarf"},
 			onCollision:  control.Skip,
 			expectErr: func(t *testing.T, err error) {
 				assert.ErrorIs(t, err, graph.ErrItemAlreadyExistsConflict, clues.ToCore(err))
 			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.False(t, m.calledPost, "new item posted")
+				assert.False(t, m.calledDelete, "old item deleted")
+			},
 		},
 		{
 			name:         "collision: copy",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{collisionKey: "smarf"},
 			onCollision:  control.Copy,
 			expectErr: func(t *testing.T, err error) {
 				assert.NoError(t, err, clues.ToCore(err))
 			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.True(t, m.calledPost, "new item posted")
+				assert.False(t, m.calledDelete, "old item deleted")
+			},
 		},
 		{
 			name:         "collision: replace",
-			apiMock:      mockEventRestorer{},
+			apiMock:      &eventRestoreMock{},
 			collisionMap: map[string]string{collisionKey: "smarf"},
 			onCollision:  control.Replace,
 			expectErr: func(t *testing.T, err error) {
-				assert.ErrorIs(t, err, graph.ErrItemAlreadyExistsConflict, clues.ToCore(err))
+				assert.NoError(t, err, clues.ToCore(err))
+			},
+			expectMock: func(t *testing.T, m *eventRestoreMock) {
+				assert.True(t, m.calledPost, "new item posted")
+				assert.True(t, m.calledDelete, "old item deleted")
 			},
 		},
 	}
@@ -211,6 +248,7 @@ func (suite *EventsRestoreIntgSuite) TestRestoreEvent() {
 				fault.New(true))
 
 			test.expectErr(t, err)
+			test.expectMock(t, test.apiMock)
 		})
 	}
 }
