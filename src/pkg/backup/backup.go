dc98b64e2013ed345499b92f7bdd6eb25ce85310
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-07-12 09:13:55 -0700

    **Temporary** Add known lint issue

--- src/pkg/backup/backup.go
+++ src/pkg/backup/backup.go
@@ -21,7 +21,7 @@ import (
 // Backup represents the result of a backup operation
 type Backup struct {
 	model.BaseModel
-	CreationTime time.Time `json:"creationTime"`
+	CreationTime time.Time `josn:"creationTime"`
 
 	// SnapshotID is the kopia snapshot ID
 	SnapshotID string `json:"snapshotID"`
