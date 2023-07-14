80513b1678462319b8a3b4c3b4f904f5be5368e5
Abin <mail@meain.io>
2023-07-14 17:54:35 +0530

    Add test for getItemName

--- src/internal/m365/onedrive/export_test.go
+++ src/internal/m365/onedrive/export_test.go
@@ -1,8 +1,12 @@
 package onedrive
 
 import (
+	"bytes"
+	"context"
+	"io"
 	"testing"
 
+	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/internal/m365/onedrive/metadata"
 	"github.com/alcionai/corso/src/internal/tester"
 	"github.com/stretchr/testify/assert"
@@ -52,6 +56,92 @@ func (suite *ExportUnitSuite) TestIsMetadataFile() {
 	for _, test := range table {
 		suite.Run(test.name, func() {
 			assert.Equal(suite.T(), test.isMeta, isMetadataFile(test.id, test.version), "is metadata")
+		})
+	}
+}
+
+type metadataStream struct {
+	id   string
+	name string
+}
+
+func (ms metadataStream) ToReader() io.ReadCloser {
+	return io.NopCloser(bytes.NewBufferString(`{"filename": "` + ms.name + `"}`))
+}
+func (ms metadataStream) UUID() string  { return ms.id }
+func (ms metadataStream) Deleted() bool { return false }
+
+type finD struct {
+	id   string
+	name string
+	err  error
+}
+
+func (fd finD) FetchItemByName(ctx context.Context, name string) (data.Stream, error) {
+	if fd.err != nil {
+		return nil, fd.err
+	}
+
+	return metadataStream{id: fd.id, name: fd.name}, nil
+}
+
+func (suite *ExportUnitSuite) TestGetItemName() {
+	table := []struct {
+		tname   string
+		id      string
+		version int
+		name    string
+		fin     data.FetchItemByNamer
+		errFunc assert.ErrorAssertionFunc
+	}{
+		{
+			tname:   "legacy",
+			id:      "name",
+			version: 1,
+			name:    "name",
+			errFunc: assert.NoError,
+		},
+		{
+			tname:   "name in filename",
+			id:      "name.data",
+			version: 4,
+			name:    "name",
+			errFunc: assert.NoError,
+		},
+		{
+			tname:   "name in metadata",
+			id:      "name.data",
+			version: 5,
+			name:    "name",
+			fin:     finD{id: "name.data", name: "name"},
+			errFunc: assert.NoError,
+		},
+		{
+			tname:   "name in metadata but error",
+			id:      "name.data",
+			version: 5,
+			name:    "",
+			fin:     finD{err: assert.AnError},
+			errFunc: assert.Error,
+		},
+	}
+
+	for _, test := range table {
+		suite.Run(test.tname, func() {
+			t := suite.T()
+
+			ctx, flush := tester.NewContext(t)
+			defer flush()
+
+			name, err := getItemName(
+				ctx,
+				test.id,
+				test.version,
+				test.fin,
+			)
+			test.errFunc(t, err)
+
+			assert.Equal(t, test.name, name, "name")
 		})
 	}
 }
