dd325211f3bb851213b57d3ba890cc519d662764
neha_gupta <nehagupta161995@gmail.com>
2023-07-17 13:34:08 +0530

    Longevity test- delete backup after n days (#3790)
    <!-- PR description-->
    Longevity Test- Delete the backups older than n no of days. Currently n's value is 5. 
    
    ---
    
    #### Does this PR need a docs update or release note?
    - [x] :no_entry: No
    
    #### Type of change
    
    <!--- Please check the type of change your PR introduces: --->
    - [ ] :robot: Supportability/Tests
    #### Issue(s)
    
    <!-- Can reference multiple issues. Use one of the following "magic words" - "closes, fixes" to auto-close the Github issue. -->
    * https://github.com/alcionai/corso/issues/3679
    
    #### Test Plan
    
    <!-- How will this be tested prior to merging.-->
    - [x] :zap: Unit test

---
+++ src/cmd/longevity_test/longevity.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/alcionai/corso/src/cli/config"
+	"github.com/alcionai/corso/src/cli/utils"
+	"github.com/alcionai/corso/src/pkg/logger"
+	"github.com/alcionai/corso/src/pkg/path"
+	"github.com/alcionai/corso/src/pkg/store"
+)
+
+func main() {
+	var (
+		service path.ServiceType
+		cc      = cobra.Command{}
+	)
+
+	cc.SetContext(context.Background())
+
+	if err := config.InitFunc(&cc, []string{}); err != nil {
+		return
+	}
+
+	switch serviceName := os.Getenv("SERVICE"); serviceName {
+	case "exchange":
+		service = path.ExchangeService
+	case "onedrive":
+		service = path.OneDriveService
+	case "sharepoint":
+		service = path.SharePointService
+	default:
+		fatal(cc.Context(), "unknown service", nil)
+	}
+
+	r, _, _, err := utils.GetAccountAndConnect(cc.Context(), service, nil)
+	if err != nil {
+		fatal(cc.Context(), "unable to connect account", err)
+	}
+
+	defer r.Close(cc.Context())
+
+	backups, err := r.BackupsByTag(cc.Context(), store.Service(service))
+	if err != nil {
+		fatal(cc.Context(), "unable to find backups", err)
+	}
+
+	days, err := strconv.Atoi(os.Getenv("DELETION_DAYS"))
+	if err != nil {
+		fatal(cc.Context(), "invalid no of days provided", nil)
+	}
+
+	for _, backup := range backups {
+		if backup.StartAndEndTime.CompletedAt.Before(time.Now().AddDate(0, 0, -days)) {
+			if err := r.DeleteBackup(cc.Context(), backup.ID.String()); err != nil {
+				fatal(cc.Context(), "deleting backup", err)
+			}
+
+			logAndPrint(cc.Context(), "Deleted backup %s", backup.ID.String())
+		}
+	}
+}
+
+func fatal(ctx context.Context, msg string, err error) {
+	logger.CtxErr(ctx, err).Error("test failure: " + msg)
+	fmt.Println(msg+": ", err)
+	os.Exit(1)
+}
+
+func logAndPrint(ctx context.Context, tmpl string, vs ...any) {
+	logger.Ctx(ctx).Infof(tmpl, vs...)
+	fmt.Printf(tmpl+"\n", vs...)
+}
