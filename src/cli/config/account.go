alcionai/corso

commit 1ba103523ae43a2d5ed7b265df0d55f7289e2008

Fix nightly test (#3739)
<!-- PR description-->

---

#### Does this PR need a docs update or release note?

- [ ] :white_check_mark: Yes, it's included
- [ ] :clock1: Yes, but in a later PR
- [x] :no_entry: No

#### Type of change

<!--- Please check the type of change your PR introduces: --->
- [ ] :sunflower: Feature
- [ ] :bug: Bugfix
- [ ] :world_map: Documentation
- [ ] :robot: Supportability/Tests
- [ ] :computer: CI/Deployment
- [x] :broom: Tech Debt/Cleanup

#### Issue(s)

<!-- Can reference multiple issues. Use one of the following "magic words" - "closes, fixes" to auto-close the Github issue. -->
* #<issue>

#### Test Plan

<!-- How will this be tested prior to merging.-->
- [x] :muscle: Manual
- [x] :zap: Unit test
- [ ] :green_heart: E2E

src/cli/config/account.go
@@ -40,6 +40,7 @@ func m365Overrides(in map[string]string) map[string]string {
 func configureAccount(
 	vpr *viper.Viper,
 	readConfigFromViper bool,
+	matchFromConfig bool,
 	overrides map[string]string,
 ) (account.Account, error) {
 	var (
@@ -54,7 +55,9 @@ func configureAccount(
 		if err != nil {
 			return acct, clues.Wrap(err, "reading m365 configs from corso config file")
 		}
+	}
 
+	if matchFromConfig {
 		if err := mustMatchConfig(vpr, m365Overrides(overrides)); err != nil {
 			return acct, clues.Wrap(err, "verifying m365 configs in corso config file")
 		}

src/cli/config/config_test.go
@@ -270,7 +270,7 @@ func (suite *ConfigIntegrationSuite) TestGetStorageAndAccount() {
 	err = vpr.ReadInConfig()
 	require.NoError(t, err, "reading repo config", clues.ToCore(err))
 
-	config, err := getStorageAndAccountWithViper(vpr, true, false, nil)
+	config, err := getStorageAndAccountWithViper(vpr, true, true, nil)
 	require.NoError(t, err, "getting storage and account from config", clues.ToCore(err))
 
 	readS3Cfg, err := config.Storage.S3Config()

src/cli/config/config.go
@@ -284,7 +284,7 @@ func getStorageAndAccountWithViper(
 		config.RepoID = vpr.GetString(RepoID)
 	}
 
-	config.Account, err = configureAccount(vpr, readConfigFromViper, overrides)
+	config.Account, err = configureAccount(vpr, readConfigFromViper, mustMatchFromConfig, overrides)
 	if err != nil {
 		return config, clues.Wrap(err, "retrieving account configuration details")
 	}

commit fa79cd5a72302c451be9bceefa2692fb9e4b272c

Only match with viper config if it was read (#3860)
Fix a regression introduced in 1ba103523ae. The `matchFromConfig` block should only be entered if we read config from viper.

---

#### Does this PR need a docs update or release note?

- [x] :white_check_mark: Yes, it's included
- [ ] :clock1: Yes, but in a later PR
- [ ] :no_entry: No

#### Type of change

<!--- Please check the type of change your PR introduces: --->
- [ ] :sunflower: Feature
- [x] :bug: Bugfix
- [ ] :world_map: Documentation
- [ ] :robot: Supportability/Tests
- [ ] :computer: CI/Deployment
- [ ] :broom: Tech Debt/Cleanup

#### Issue(s)

<!-- Can reference multiple issues. Use one of the following "magic words" - "closes, fixes" to auto-close the Github issue. -->
* #3856 

#### Test Plan

<!-- How will this be tested prior to merging.-->
- [ ] :muscle: Manual
- [ ] :zap: Unit test
- [x] :green_heart: E2E

src/cli/config/account.go
@@ -50,16 +50,16 @@ func configureAccount(
 		if err != nil {
 			return acct, clues.Wrap(err, "reading m365 configs from corso config file")
 		}
-	}
 
-	if matchFromConfig {
-		providerType := vpr.GetString(AccountProviderTypeKey)
-		if providerType != account.ProviderM365.String() {
-			return acct, clues.New("unsupported account provider: " + providerType)
-		}
+		if matchFromConfig {
+			providerType := vpr.GetString(AccountProviderTypeKey)
+			if providerType != account.ProviderM365.String() {
+				return acct, clues.New("unsupported account provider: " + providerType)
+			}
 
-		if err := mustMatchConfig(vpr, m365Overrides(overrides)); err != nil {
-			return acct, clues.Wrap(err, "verifying m365 configs in corso config file")
+			if err := mustMatchConfig(vpr, m365Overrides(overrides)); err != nil {
+				return acct, clues.Wrap(err, "verifying m365 configs in corso config file")
+			}
 		}
 	}
 

src/cli/config/storage.go
@@ -75,16 +75,16 @@ func configureStorage(
 		if p, ok := overrides[storage.Prefix]; ok {
 			overrides[storage.Prefix] = common.NormalizePrefix(p)
 		}
-	}
 
-	if matchFromConfig {
-		providerType := vpr.GetString(StorageProviderTypeKey)
-		if providerType != storage.ProviderS3.String() {
-			return store, clues.New("unsupported storage provider: " + providerType)
-		}
+		if matchFromConfig {
+			providerType := vpr.GetString(StorageProviderTypeKey)
+			if providerType != storage.ProviderS3.String() {
+				return store, clues.New("unsupported storage provider: " + providerType)
+			}
 
-		if err := mustMatchConfig(vpr, s3Overrides(overrides)); err != nil {
-			return store, clues.Wrap(err, "verifying s3 configs in corso config file")
+			if err := mustMatchConfig(vpr, s3Overrides(overrides)); err != nil {
+				return store, clues.Wrap(err, "verifying s3 configs in corso config file")
+			}
 		}
 	}
 

src/cli/config/config_test.go
@@ -450,7 +450,7 @@ func (suite *ConfigIntegrationSuite) TestGetStorageAndAccount_noFileOnlyOverride
 		StorageProviderTypeKey: storage.ProviderS3.String(),
 	}
 
-	cfg, err := getStorageAndAccountWithViper(vpr, false, false, overrides)
+	cfg, err := getStorageAndAccountWithViper(vpr, false, true, overrides)
 	require.NoError(t, err, "getting storage and account from config", clues.ToCore(err))
 
 	readS3Cfg, err := cfg.Storage.S3Config()