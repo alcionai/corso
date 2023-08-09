b121d042cd8685c08c2f4cdfea15b2c034474093
Abhishek Pandey <abhishek@alcion.ai>
2023-08-09 13:29:15 +0530

    Remove duplicates

--- src/internal/kopia/mock_backup_base.go
+++ src/internal/kopia/mock_backup_base.go
@@ -16,7 +16,6 @@ func AssertBackupBasesEqual(t *testing.T, expect, got BackupBases) {
 		assert.Empty(t, got.MergeBases(), "merge bases")
 		assert.Empty(t, got.AssistBackups(), "assist backups")
 		assert.Empty(t, got.AssistBases(), "assist bases")
-		assert.Empty(t, got.AssistBackups(), "assist backups")
 
 		return
 	}
@@ -36,7 +35,6 @@ func AssertBackupBasesEqual(t *testing.T, expect, got BackupBases) {
 	assert.ElementsMatch(t, expect.MergeBases(), got.MergeBases(), "merge bases")
 	assert.ElementsMatch(t, expect.AssistBackups(), got.AssistBackups(), "assist backups")
 	assert.ElementsMatch(t, expect.AssistBases(), got.AssistBases(), "assist bases")
-	assert.ElementsMatch(t, expect.AssistBackups(), got.AssistBackups(), "assist backups")
 }
 
 func NewMockBackupBases() *MockBackupBases {
