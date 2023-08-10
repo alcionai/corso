b7aea6bab4656e7213d4ba49e2c8dd71a15ea062
ryanfkeepers <ryanfkeepers@gmail.com>
2023-08-09 18:37:11 -0600

    introduce service_resource tuple
    the service resource tuple will form the basis for
    having paths contain multiple nested service
    declarations, by allowing it to swap out the
    Service() and ResourceOwner() funcs in favor of
    a func that returns the ordered list of service-resource
    tuples found in the path.

--- src/pkg/path/service_type.go
+++ src/pkg/path/service_type.go
@@ -29,6 +29,8 @@ const (
 	ExchangeMetadataService               // exchangeMetadata
 	OneDriveMetadataService               // onedriveMetadata
 	SharePointMetadataService             // sharepointMetadata
+	GroupsService                         // groups
+	GroupsMetadataService                 // groupsMetadata
 )
 
 func toServiceType(service string) ServiceType {
@@ -50,4 +52,37 @@ func toServiceType(service string) ServiceType {
 	default:
 		return UnknownService
 	}
+}
+
+// subServices is a mapping of all valid service/subService pairs.
+// a subService pair occurs when one service contains a reference
+// to a protected resource of another service type, and the resource
+// for that second service is the identifier which is used to discover
+// data.  A subService relationship may imply that the subservice data
+// is wholly replicated/owned by the primary service, or it may not,
+// each case differs.
+//
+// Ex:
+//   - groups/<gID>/sharepoint/<siteID> => each team in groups contains a
+//     complete sharepoint site.
+//   - groups/<gID>/member/<userID> => each user in a team can own one or
+//     more Chats.  But the group does not contain the complete user data.
+var subServices = map[ServiceType]map[ServiceType]struct{}{
+	GroupsService: {
+		SharePointService: {},
+	},
+}
+
+func ValidateServiceAndSubService(service, subService ServiceType) error {
+	subs, ok := subServices[service]
+	if !ok {
+		return clues.New("unsupported service").With("service", service)
+	}
+
+	if _, ok := subs[subService]; !ok {
+		return clues.New("unknown service/subService combination").
+			With("service", service, "subService", subService)
+	}
+
+	return nil
 }
