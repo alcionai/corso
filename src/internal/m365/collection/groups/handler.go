8d4277b1c7035722f708d98cd462a3c5136ebabd
neha-Gupta1 <nehagupta161995@gmail.com>
2023-08-17 16:24:38 +0530

    add handers for channels

---
+++ src/internal/m365/collection/groups/handler.go
@@ -0,0 +1,23 @@
+package groups
+
+import (
+	"context"
+
+	"github.com/alcionai/corso/src/pkg/services/m365/api"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+)
+
+type BackupChannelHandler interface {
+	GetChannel(ctx context.Context, teamID, channelID string) (models.Channelable, error)
+	NewChannelPager(teamID, channelID string) api.ChannelItemDeltaEnumerator
+}
+
+type BackupMessagesHandler interface {
+	GetItem(ctx context.Context, teamID, channelID, itemID string) (models.ChatMessageable, error)
+	NewItemPager(teamID, channelID string) api.MessageItemDeltaEnumerator
+}
+
+type BackupReplyHandler interface {
+	GetItem(ctx context.Context, teamID, channelID, messageID string) (serialization.Parsable, error)
+}
