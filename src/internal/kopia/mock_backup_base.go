b3c6be5b770455546bd225aa3095511a7c1129a3
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-08-31 08:46:48 -0700

    Update mock code for new API

--- src/internal/kopia/mock_backup_base.go
+++ src/internal/kopia/mock_backup_base.go
@@ -14,17 +14,19 @@ func AssertBackupBasesEqual(t *testing.T, expect, got BackupBases) {
 	if expect == nil {
 		assert.Empty(t, got.Backups(), "backups")
 		assert.Empty(t, got.MergeBases(), "merge bases")
-		assert.Empty(t, got.AssistBackups(), "assist backups")
-		assert.Empty(t, got.AssistBases(), "assist bases")
+		assert.Empty(t, got.UniqueAssistBackups(), "assist backups")
+		assert.Empty(t, got.UniqueAssistBases(), "assist bases")
+		assert.Empty(t, got.SnapshotAssistBases(), "snapshot assist bases")
 
 		return
 	}
 
 	if got == nil {
-		if len(expect.Backups()) > 0 &&
-			len(expect.MergeBases()) > 0 &&
-			len(expect.AssistBackups()) > 0 &&
-			len(expect.AssistBases()) > 0 {
+		if len(expect.Backups()) > 0 ||
+			len(expect.MergeBases()) > 0 ||
+			len(expect.UniqueAssistBackups()) > 0 ||
+			len(expect.UniqueAssistBases()) > 0 ||
+			len(expect.SnapshotAssistBases()) > 0 {
 			assert.Fail(t, "got was nil but expected non-nil result %v", expect)
 		}
 
@@ -33,8 +35,9 @@ func AssertBackupBasesEqual(t *testing.T, expect, got BackupBases) {
 
 	assert.ElementsMatch(t, expect.Backups(), got.Backups(), "backups")
 	assert.ElementsMatch(t, expect.MergeBases(), got.MergeBases(), "merge bases")
-	assert.ElementsMatch(t, expect.AssistBackups(), got.AssistBackups(), "assist backups")
-	assert.ElementsMatch(t, expect.AssistBases(), got.AssistBases(), "assist bases")
+	assert.ElementsMatch(t, expect.UniqueAssistBackups(), got.UniqueAssistBackups(), "assist backups")
+	assert.ElementsMatch(t, expect.UniqueAssistBases(), got.UniqueAssistBases(), "assist bases")
+	assert.ElementsMatch(t, expect.SnapshotAssistBases(), got.SnapshotAssistBases(), "snapshot assist bases")
 }
 
 func NewMockBackupBases() *MockBackupBases {
@@ -52,22 +55,22 @@ func (bb *MockBackupBases) WithBackups(b ...BackupEntry) *MockBackupBases {
 
 func (bb *MockBackupBases) WithMergeBases(m ...ManifestEntry) *MockBackupBases {
 	bb.backupBases.mergeBases = append(bb.MergeBases(), m...)
-	bb.backupBases.assistBases = append(bb.AssistBases(), m...)
+	bb.backupBases.assistBases = append(bb.UniqueAssistBases(), m...)
 
 	return bb
 }
 
 func (bb *MockBackupBases) WithAssistBackups(b ...BackupEntry) *MockBackupBases {
-	bb.backupBases.assistBackups = append(bb.AssistBackups(), b...)
+	bb.backupBases.assistBackups = append(bb.UniqueAssistBackups(), b...)
 	return bb
 }
 
 func (bb *MockBackupBases) WithAssistBases(m ...ManifestEntry) *MockBackupBases {
-	bb.backupBases.assistBases = append(bb.AssistBases(), m...)
+	bb.backupBases.assistBases = append(bb.UniqueAssistBases(), m...)
 	return bb
 }
 
 func (bb *MockBackupBases) ClearMockAssistBases() *MockBackupBases {
-	bb.backupBases.ClearAssistBases()
+	bb.backupBases.DisableAssistBases()
 	return bb
 }
