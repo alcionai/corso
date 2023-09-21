ef86c1f181fe6787335fb471b672d5a345347277
ryanfkeepers <ryanfkeepers@gmail.com>
2023-09-21 17:25:50 -0600

    add key descripttions

--- src/pkg/count/keys.go
+++ src/pkg/count/keys.go
@@ -3,12 +3,18 @@ package count
 type key string
 
 const (
+	// count of bucket-tokens consumed by api calls.
 	APICallTokensConsumed key = "api-call-tokens-consumed"
-	CollisionReplace      key = "collision-replace"
-	CollisionSkip         key = "collision-skip"
+	// count of times that items had collisions during restore,
+	// and that collision was solved by replacing the item.
+	CollisionReplace key = "collision-replace"
+	// count of times that items had collisions during restore,
+	// and that collision was solved by skipping the item.
+	CollisionSkip key = "collision-skip"
 	// NewItemCreated should be used for non-skip, non-replace,
 	// non-meta item creation counting.  IE: use it specifically
 	// for counting new items (no collision) or copied items.
-	NewItemCreated    key = "new-item-created"
+	NewItemCreated key = "new-item-created"
+	// count of api calls that resulted in failure due to throttling.
 	ThrottledAPICalls key = "throttled-api-calls"
 )
