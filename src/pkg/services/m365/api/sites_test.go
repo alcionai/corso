a8a2aee99ddbcca3bceb9c86e922bcc7008b40ce
Keepers <ryanfkeepers@gmail.com>
2023-09-26 16:32:15 -0600

    remove expand drive from root site (#4377)
    missed the root site call usage of expand drive
    on the last fix.
    
    ---
    
    #### Does this PR need a docs update or release note?
    
    - [x] :no_entry: No
    
    #### Type of change
    
    - [x] :bug: Bugfix
    
    #### Issue(s)
    
    * #4337
    
    #### Test Plan
    
    - [x] :muscle: Manual
    - [x] :zap: Unit test
    - [x] :green_heart: E2E

--- src/pkg/services/m365/api/sites_test.go
+++ src/pkg/services/m365/api/sites_test.go
@@ -256,7 +256,7 @@ func (suite *SitesIntgSuite) TestGetRoot() {
 	ctx, flush := tester.NewContext(t)
 	defer flush()
 
-	result, err := suite.its.ac.Sites().GetRoot(ctx)
+	result, err := suite.its.ac.Sites().GetRoot(ctx, api.CallConfig{Expand: []string{"drive"}})
 	require.NoError(t, err)
 	require.NotNil(t, result, "must find the root site")
 	require.NotEmpty(t, ptr.Val(result.GetId()), "must have an id")
