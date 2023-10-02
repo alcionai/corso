8bdef88a8bb7e8d155795d1770e99548e00dd1ee
Abin Simon <abinsimon10@gmail.com>
2023-10-02 20:03:15 +0530

    Return tombstone instead of canUsePreviousBackup (#4394)
    This is necessary to fix the correctness of the backup.
    
    ---
    
    #### Does this PR need a docs update or release note?
    
    - [ ] :white_check_mark: Yes, it's included
    - [ ] :clock1: Yes, but in a later PR
    - [x] :no_entry: No
    
    #### Type of change
    
    <!--- Please check the type of change your PR introduces: --->
    - [ ] :sunflower: Feature
    - [x] :bug: Bugfix
    - [ ] :world_map: Documentation
    - [ ] :robot: Supportability/Tests
    - [ ] :computer: CI/Deployment
    - [ ] :broom: Tech Debt/Cleanup
    
    #### Issue(s)
    
    <!-- Can reference multiple issues. Use one of the following "magic words" - "closes, fixes" to auto-close the Github issue. -->
    * fixes https://github.com/alcionai/corso/issues/4371
    
    #### Test Plan
    
    <!-- How will this be tested prior to merging.-->
    - [ ] :muscle: Manual
    - [ ] :zap: Unit test
    - [x] :green_heart: E2E

--- src/internal/m365/collection/drive/collections.go
+++ src/internal/m365/collection/drive/collections.go
@@ -135,11 +135,6 @@ func deserializeMetadata(
 					continue
 				}
 
-				if err == nil {
-					// Successful decode.
-					continue
-				}
-
 				// This is conservative, but report an error if either any of the items
 				// for any of the deserialized maps have duplicate drive IDs or there's
 				// some other problem deserializing things. This will cause the entire
@@ -147,7 +142,9 @@ func deserializeMetadata(
 				// these cases. We can make the logic for deciding when to continue vs.
 				// when to fail less strict in the future if needed.
 				if err != nil {
-					return nil, nil, false, clues.Stack(err).WithClues(ictx)
+					errs.Fail(clues.Stack(err).WithClues(ictx))
+
+					return map[string]string{}, map[string]map[string]string{}, false, nil
 				}
 			}
 		}
