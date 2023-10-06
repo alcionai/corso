97953f76db1b7aea5aeed3d5345d8112912d942a
Abhishek Pandey <abhishek@alcion.ai>
2023-10-06 16:55:43 +0530

    Remove pyroscope

--- src/corso.go
+++ src/corso.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"github.com/alcionai/corso/src/cli"
-	prof "github.com/alcionai/corso/src/internal/profile"
+	//prof "github.com/alcionai/corso/src/internal/profile"
 )
 
 func main() {
-	prof.Profiler()
+	//prof.Profiler()
 	cli.Handle()
 }
