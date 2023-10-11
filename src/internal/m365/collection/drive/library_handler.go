2aafa4b293bcaf2c9aebd4f513b9bb70e8a37895
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-10-11 10:41:21 -0700

    Update names of item and library handlers
    item -> userDrive
    library -> site
    
    This just updates the files defining those handlers.

--- src/internal/m365/collection/drive/library_handler.go
+++ src/internal/m365/collection/drive/library_handler.go
@@ -18,18 +18,18 @@ import (
 	"github.com/alcionai/corso/src/pkg/services/m365/api"
 )
 
-type baseLibraryHandler struct {
+type baseSiteHandler struct {
 	ac api.Drives
 }
 
-func (h baseLibraryHandler) NewDrivePager(
+func (h baseSiteHandler) NewDrivePager(
 	resourceOwner string,
 	fields []string,
 ) api.Pager[models.Driveable] {
 	return h.ac.NewSiteDrivePager(resourceOwner, fields)
 }
 
-func (h baseLibraryHandler) AugmentItemInfo(
+func (h baseSiteHandler) AugmentItemInfo(
 	dii details.ItemInfo,
 	resource idname.Provider,
 	item models.DriveItemable,
@@ -62,23 +62,23 @@ func (h baseLibraryHandler) AugmentItemInfo(
 	return dii
 }
 
-var _ BackupHandler = &libraryBackupHandler{}
+var _ BackupHandler = &siteBackupHandler{}
 
-type libraryBackupHandler struct {
-	baseLibraryHandler
+type siteBackupHandler struct {
+	baseSiteHandler
 	siteID  string
 	scope   selectors.SharePointScope
 	service path.ServiceType
 }
 
-func NewLibraryBackupHandler(
+func NewSiteBackupHandler(
 	ac api.Drives,
 	siteID string,
 	scope selectors.SharePointScope,
 	service path.ServiceType,
-) libraryBackupHandler {
-	return libraryBackupHandler{
-		baseLibraryHandler: baseLibraryHandler{
+) siteBackupHandler {
+	return siteBackupHandler{
+		baseSiteHandler: baseSiteHandler{
 			ac: ac,
 		},
 		siteID:  siteID,
@@ -87,7 +87,7 @@ func NewLibraryBackupHandler(
 	}
 }
 
-func (h libraryBackupHandler) Get(
+func (h siteBackupHandler) Get(
 	ctx context.Context,
 	url string,
 	headers map[string]string,
@@ -95,7 +95,7 @@ func (h libraryBackupHandler) Get(
 	return h.ac.Get(ctx, url, headers)
 }
 
-func (h libraryBackupHandler) PathPrefix(
+func (h siteBackupHandler) PathPrefix(
 	tenantID, driveID string,
 ) (path.Path, error) {
 	return path.Build(
@@ -109,7 +109,7 @@ func (h libraryBackupHandler) PathPrefix(
 		odConsts.RootPathDir)
 }
 
-func (h libraryBackupHandler) MetadataPathPrefix(
+func (h siteBackupHandler) MetadataPathPrefix(
 	tenantID string,
 ) (path.Path, error) {
 	p, err := path.BuildMetadata(
@@ -125,54 +125,54 @@ func (h libraryBackupHandler) MetadataPathPrefix(
 	return p, nil
 }
 
-func (h libraryBackupHandler) CanonicalPath(
+func (h siteBackupHandler) CanonicalPath(
 	folders *path.Builder,
 	tenantID string,
 ) (path.Path, error) {
 	return folders.ToDataLayerPath(tenantID, h.siteID, h.service, path.LibrariesCategory, false)
 }
 
-func (h libraryBackupHandler) ServiceCat() (path.ServiceType, path.CategoryType) {
+func (h siteBackupHandler) ServiceCat() (path.ServiceType, path.CategoryType) {
 	return h.service, path.LibrariesCategory
 }
 
-func (h libraryBackupHandler) FormatDisplayPath(
+func (h siteBackupHandler) FormatDisplayPath(
 	driveName string,
 	pb *path.Builder,
 ) string {
 	return "/" + driveName + "/" + pb.String()
 }
 
-func (h libraryBackupHandler) NewLocationIDer(
+func (h siteBackupHandler) NewLocationIDer(
 	driveID string,
 	elems ...string,
 ) details.LocationIDer {
 	return details.NewSharePointLocationIDer(driveID, elems...)
 }
 
-func (h libraryBackupHandler) GetItemPermission(
+func (h siteBackupHandler) GetItemPermission(
 	ctx context.Context,
 	driveID, itemID string,
 ) (models.PermissionCollectionResponseable, error) {
 	return h.ac.GetItemPermission(ctx, driveID, itemID)
 }
 
-func (h libraryBackupHandler) GetItem(
+func (h siteBackupHandler) GetItem(
 	ctx context.Context,
 	driveID, itemID string,
 ) (models.DriveItemable, error) {
 	return h.ac.GetItem(ctx, driveID, itemID)
 }
 
-func (h libraryBackupHandler) IsAllPass() bool {
+func (h siteBackupHandler) IsAllPass() bool {
 	return h.scope.IsAny(selectors.SharePointLibraryFolder)
 }
 
-func (h libraryBackupHandler) IncludesDir(dir string) bool {
+func (h siteBackupHandler) IncludesDir(dir string) bool {
 	return h.scope.Matches(selectors.SharePointLibraryFolder, dir)
 }
 
-func (h libraryBackupHandler) EnumerateDriveItemsDelta(
+func (h siteBackupHandler) EnumerateDriveItemsDelta(
 	ctx context.Context,
 	driveID, prevDeltaLink string,
 	selectProps []string,
@@ -184,17 +184,17 @@ func (h libraryBackupHandler) EnumerateDriveItemsDelta(
 // Restore
 // ---------------------------------------------------------------------------
 
-var _ RestoreHandler = &libraryRestoreHandler{}
+var _ RestoreHandler = &siteRestoreHandler{}
 
-type libraryRestoreHandler struct {
-	baseLibraryHandler
+type siteRestoreHandler struct {
+	baseSiteHandler
 	ac      api.Client
 	service path.ServiceType
 }
 
-func NewLibraryRestoreHandler(ac api.Client, service path.ServiceType) libraryRestoreHandler {
-	return libraryRestoreHandler{
-		baseLibraryHandler: baseLibraryHandler{
+func NewSiteRestoreHandler(ac api.Client, service path.ServiceType) siteRestoreHandler {
+	return siteRestoreHandler{
+		baseSiteHandler: baseSiteHandler{
 			ac: ac.Drives(),
 		},
 		ac:      ac,
@@ -202,28 +202,28 @@ func NewLibraryRestoreHandler(ac api.Client, service path.ServiceType) libraryRe
 	}
 }
 
-func (h libraryRestoreHandler) PostDrive(
+func (h siteRestoreHandler) PostDrive(
 	ctx context.Context,
 	siteID, driveName string,
 ) (models.Driveable, error) {
 	return h.ac.Lists().PostDrive(ctx, siteID, driveName)
 }
 
-func (h libraryRestoreHandler) DeleteItem(
+func (h siteRestoreHandler) DeleteItem(
 	ctx context.Context,
 	driveID, itemID string,
 ) error {
 	return h.ac.Drives().DeleteItem(ctx, driveID, itemID)
 }
 
-func (h libraryRestoreHandler) DeleteItemPermission(
+func (h siteRestoreHandler) DeleteItemPermission(
 	ctx context.Context,
 	driveID, itemID, permissionID string,
 ) error {
 	return h.ac.Drives().DeleteItemPermission(ctx, driveID, itemID, permissionID)
 }
 
-func (h libraryRestoreHandler) GetItemsInContainerByCollisionKey(
+func (h siteRestoreHandler) GetItemsInContainerByCollisionKey(
 	ctx context.Context,
 	driveID, containerID string,
 ) (map[string]api.DriveItemIDType, error) {
@@ -235,14 +235,14 @@ func (h libraryRestoreHandler) GetItemsInContainerByCollisionKey(
 	return m, nil
 }
 
-func (h libraryRestoreHandler) NewItemContentUpload(
+func (h siteRestoreHandler) NewItemContentUpload(
 	ctx context.Context,
 	driveID, itemID string,
 ) (models.UploadSessionable, error) {
 	return h.ac.Drives().NewItemContentUpload(ctx, driveID, itemID)
 }
 
-func (h libraryRestoreHandler) PostItemPermissionUpdate(
+func (h siteRestoreHandler) PostItemPermissionUpdate(
 	ctx context.Context,
 	driveID, itemID string,
 	body *drives.ItemItemsItemInvitePostRequestBody,
@@ -250,7 +250,7 @@ func (h libraryRestoreHandler) PostItemPermissionUpdate(
 	return h.ac.Drives().PostItemPermissionUpdate(ctx, driveID, itemID, body)
 }
 
-func (h libraryRestoreHandler) PostItemLinkShareUpdate(
+func (h siteRestoreHandler) PostItemLinkShareUpdate(
 	ctx context.Context,
 	driveID, itemID string,
 	body *drives.ItemItemsItemCreateLinkPostRequestBody,
@@ -258,7 +258,7 @@ func (h libraryRestoreHandler) PostItemLinkShareUpdate(
 	return h.ac.Drives().PostItemLinkShareUpdate(ctx, driveID, itemID, body)
 }
 
-func (h libraryRestoreHandler) PostItemInContainer(
+func (h siteRestoreHandler) PostItemInContainer(
 	ctx context.Context,
 	driveID, parentFolderID string,
 	newItem models.DriveItemable,
@@ -267,14 +267,14 @@ func (h libraryRestoreHandler) PostItemInContainer(
 	return h.ac.Drives().PostItemInContainer(ctx, driveID, parentFolderID, newItem, onCollision)
 }
 
-func (h libraryRestoreHandler) GetFolderByName(
+func (h siteRestoreHandler) GetFolderByName(
 	ctx context.Context,
 	driveID, parentFolderID, folderName string,
 ) (models.DriveItemable, error) {
 	return h.ac.Drives().GetFolderByName(ctx, driveID, parentFolderID, folderName)
 }
 
-func (h libraryRestoreHandler) GetRootFolder(
+func (h siteRestoreHandler) GetRootFolder(
 	ctx context.Context,
 	driveID string,
 ) (models.DriveItemable, error) {
