a39cd6b810ad0968c3d432f9ed2afebe601536a9
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-10-11 09:39:59 -0700

    Add helper functions for extracting info
    Helper functions to extract info like item creator or drive name/ID
    given an item.

--- src/internal/m365/collection/drive/handler_utils.go
+++ src/internal/m365/collection/drive/handler_utils.go
@@ -11,6 +11,38 @@ import (
 	"github.com/alcionai/corso/src/pkg/path"
 )
 
+func getItemCreator(item models.DriveItemable) string {
+	if item.GetCreatedBy() == nil || item.GetCreatedBy().GetUser() == nil {
+		return ""
+	}
+
+	// User is sometimes not available when created via some
+	// external applications (like backup/restore solutions)
+	additionalData := item.GetCreatedBy().GetUser().GetAdditionalData()
+
+	ed, ok := additionalData["email"]
+	if !ok {
+		ed = additionalData["displayName"]
+	}
+
+	if ed == nil {
+		return ""
+	}
+
+	return *ed.(*string)
+}
+
+func getItemDriveInfo(item models.DriveItemable) (string, string) {
+	if item.GetParentReference() == nil {
+		return "", ""
+	}
+
+	driveName := strings.TrimSpace(ptr.Val(item.GetParentReference().GetName()))
+	driveID := ptr.Val(item.GetParentReference().GetDriveId())
+
+	return driveName, driveID
+}
+
 func augmentItemInfo(
 	dii details.ItemInfo,
 	resource idname.Provider,
