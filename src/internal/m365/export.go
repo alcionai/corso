dd4523edad5787ff299c6cb93cbb82013be91d20
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-10-12 16:32:27 -0700

    Remove old export function

--- src/internal/m365/export.go
+++ src/internal/m365/export.go
@@ -1,24 +1,12 @@
 package m365
 
 import (
-	"context"
-
-	"github.com/alcionai/clues"
-
-	"github.com/alcionai/corso/src/internal/data"
-	"github.com/alcionai/corso/src/internal/diagnostics"
-	"github.com/alcionai/corso/src/internal/m365/graph"
 	"github.com/alcionai/corso/src/internal/m365/service/groups"
 	"github.com/alcionai/corso/src/internal/m365/service/onedrive"
 	"github.com/alcionai/corso/src/internal/m365/service/sharepoint"
-	"github.com/alcionai/corso/src/internal/m365/support"
 	"github.com/alcionai/corso/src/internal/operations/inject"
-	"github.com/alcionai/corso/src/pkg/backup/details"
 	"github.com/alcionai/corso/src/pkg/control"
-	"github.com/alcionai/corso/src/pkg/export"
-	"github.com/alcionai/corso/src/pkg/fault"
 	"github.com/alcionai/corso/src/pkg/path"
-	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
 // NewServiceHandler returns an instance of a struct capable of running various
@@ -39,73 +27,4 @@ func (ctrl *Controller) NewServiceHandler(
 	}
 
 	return &inject.BaseServiceHandler{}
-}
-
-// ProduceExportCollections exports data from the specified collections
-func (ctrl *Controller) ProduceExportCollections(
-	ctx context.Context,
-	backupVersion int,
-	sels selectors.Selector,
-	exportCfg control.ExportConfig,
-	opts control.Options,
-	dcs []data.RestoreCollection,
-	stats *data.ExportStats,
-	errs *fault.Bus,
-) ([]export.Collectioner, error) {
-	ctx, end := diagnostics.Span(ctx, "m365:export")
-	defer end()
-
-	ctx = graph.BindRateLimiterConfig(ctx, graph.LimiterCfg{Service: sels.PathService()})
-	ctx = clues.Add(ctx, "export_config", exportCfg)
-
-	var (
-		expCollections []export.Collectioner
-		status         *support.ControllerOperationStatus
-		deets          = &details.Builder{}
-		err            error
-	)
-
-	switch sels.Service {
-	case selectors.ServiceOneDrive:
-		expCollections, err = onedrive.ProduceExportCollections(
-			ctx,
-			backupVersion,
-			exportCfg,
-			opts,
-			dcs,
-			deets,
-			stats,
-			errs)
-	case selectors.ServiceSharePoint:
-		expCollections, err = sharepoint.ProduceExportCollections(
-			ctx,
-			backupVersion,
-			exportCfg,
-			opts,
-			dcs,
-			ctrl.backupDriveIDNames,
-			deets,
-			stats,
-			errs)
-	case selectors.ServiceGroups:
-		expCollections, err = groups.ProduceExportCollections(
-			ctx,
-			backupVersion,
-			exportCfg,
-			opts,
-			dcs,
-			ctrl.backupDriveIDNames,
-			ctrl.backupSiteIDWebURL,
-			deets,
-			stats,
-			errs)
-
-	default:
-		err = clues.Wrap(clues.New(sels.Service.String()), "service not supported")
-	}
-
-	ctrl.incrementAwaitingMessages()
-	ctrl.UpdateStatus(status)
-
-	return expCollections, err
 }
