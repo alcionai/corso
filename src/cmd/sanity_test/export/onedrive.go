7419faab23a1a53856d8752f63481c085a20d9db
Keepers <ryanfkeepers@gmail.com>
2023-10-13 16:55:35 -0600

    refactor drive restore & export to use sanitree (#4425)
    refactors the common drive sanity checks to use the sanitree data container.  Also expands the sanitree in two ways:
    1. adds leaves (individual items) to nodes for granular data comparison
    2. adds multi-type support for comparing nodes of different types.
    
    ---
    
    #### Does this PR need a docs update or release note?
    
    - [x] :no_entry: No
    
    #### Type of change
    
    - [x] :robot: Supportability/Tests
    
    #### Issue(s)
    
    * #3988
    
    #### Test Plan
    
    - [x] :muscle: Manual
    - [x] :green_heart: E2E

--- src/cmd/sanity_test/export/onedrive.go
+++ src/cmd/sanity_test/export/onedrive.go
@@ -2,13 +2,9 @@ package export
 
 import (
 	"context"
-	"fmt"
-	"path/filepath"
-	"time"
 
 	"github.com/alcionai/corso/src/cmd/sanity_test/common"
-	"github.com/alcionai/corso/src/cmd/sanity_test/restore"
-	"github.com/alcionai/corso/src/internal/common/ptr"
+	"github.com/alcionai/corso/src/cmd/sanity_test/driveish"
 	"github.com/alcionai/corso/src/pkg/services/m365/api"
 )
 
@@ -22,42 +18,9 @@ func CheckOneDriveExport(
 		common.Fatal(ctx, "getting the drive:", err)
 	}
 
-	// map itemID -> item size
-	var (
-		fileSizes       = make(map[string]int64)
-		exportFileSizes = make(map[string]int64)
-		startTime       = time.Now()
-	)
-
-	err = filepath.Walk(
-		envs.FolderName,
-		common.FilepathWalker(envs.FolderName, exportFileSizes, startTime))
-	if err != nil {
-		fmt.Println("Error walking the path:", err)
-	}
-
-	_ = restore.PopulateDriveDetails(
+	driveish.CheckExport(
 		ctx,
 		ac,
-		ptr.Val(drive.GetId()),
-		envs.FolderName,
-		envs.DataFolder,
-		fileSizes,
-		map[string][]common.PermissionInfo{},
-		startTime)
-
-	for fileName, expected := range fileSizes {
-		common.LogAndPrint(ctx, "checking for file: %s", fileName)
-
-		got := exportFileSizes[fileName]
-
-		common.Assert(
-			ctx,
-			func() bool { return expected == got },
-			fmt.Sprintf("different file size: %s", fileName),
-			expected,
-			got)
-	}
-
-	fmt.Println("Success")
+		drive,
+		envs)
 }
