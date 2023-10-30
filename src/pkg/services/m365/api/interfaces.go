4eb49b8a715ba67c1ca124c66a8a504a23ddebf6
ryanfkeepers <ryanfkeepers@gmail.com>
2023-10-30 12:40:08 -0600

    fix broken type check

---
+++ src/pkg/services/m365/api/interfaces.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"context"
+
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+
+	"github.com/alcionai/corso/src/pkg/fault"
+)
+
+type GetAndSerializeItemer[INFO any] interface {
+	GetItemer[INFO]
+	Serializer
+}
+
+type GetItemer[INFO any] interface {
+	GetItem(
+		ctx context.Context,
+		user, itemID string,
+		immutableIDs bool,
+		errs *fault.Bus,
+	) (serialization.Parsable, *INFO, error)
+}
+
+type Serializer interface {
+	Serialize(
+		ctx context.Context,
+		item serialization.Parsable,
+		protectedResource, itemID string,
+	) ([]byte, error)
+}
