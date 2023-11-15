7436179841f56b1e6ce0ffb91475672b3d3dc0c3
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-11-15 15:03:28 -0800

    Add exchange service-level handler

--- src/internal/m365/service/exchange/export.go
+++ src/internal/m365/service/exchange/export.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/alcionai/clues"
 
+	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/data"
 	"github.com/alcionai/corso/src/internal/m365/collection/exchange"
+	"github.com/alcionai/corso/src/internal/m365/resource"
 	"github.com/alcionai/corso/src/internal/operations/inject"
 	"github.com/alcionai/corso/src/pkg/backup/details"
 	"github.com/alcionai/corso/src/pkg/control"
@@ -14,18 +16,31 @@ import (
 	"github.com/alcionai/corso/src/pkg/fault"
 	"github.com/alcionai/corso/src/pkg/logger"
 	"github.com/alcionai/corso/src/pkg/path"
+	"github.com/alcionai/corso/src/pkg/services/m365/api"
 )
 
 var _ inject.ServiceHandler = &baseExchangeHandler{}
 
 func NewExchangeHandler(
 	opts control.Options,
-) *baseExchangeHandler {
-	return &baseExchangeHandler{
-		opts: opts,
+	apiClient api.Client,
+	resourceClient idname.GetResourceIDAndNamer,
+) *exchangeHandler {
+	return &exchangeHandler{
+		baseExchangeHandler: baseExchangeHandler{
+			opts: opts,
+		},
+		apiClient:      apiClient,
+		resourceClient: resourceClient,
 	}
 }
 
+// ========================================================================== //
+//                        baseExchangeHandler
+// ========================================================================== //
+
+// baseExchangeHandler contains logic for tracking data and doing operations
+// (e.x. export) that don't require contact with external M356 services.
 type baseExchangeHandler struct {
 	opts control.Options
 }
@@ -71,4 +86,40 @@ func (h *baseExchangeHandler) ProduceExportCollections(
 	}
 
 	return ec, el.Failure()
+}
+
+// ========================================================================== //
+//                            exchangeHandler
+// ========================================================================== //
+
+// exchangeHandler contains logic for handling data and performing operations
+// (e.x. restore) regardless of whether they require contact with external M365
+// services or not.
+type exchangeHandler struct {
+	baseExchangeHandler
+	apiClient      api.Client
+	resourceClient idname.GetResourceIDAndNamer
+}
+
+func (h *exchangeHandler) IsServiceEnabled(
+	ctx context.Context,
+	resourceID string,
+) (bool, error) {
+	// TODO(ashmrtn): Move free function implementation to this function.
+	res, err := IsServiceEnabled(ctx, h.apiClient.Users(), resourceID)
+	return res, clues.Stack(err).OrNil()
+}
+
+func (h *exchangeHandler) PopulateProtectedResourceIDAndName(
+	ctx context.Context,
+	resourceID string, // Can be either ID or name.
+	ins idname.Cacher,
+) (idname.Provider, error) {
+	if h.resourceClient == nil {
+		return nil, clues.StackWC(ctx, resource.ErrNoResourceLookup)
+	}
+
+	pr, err := h.resourceClient.GetResourceIDAndNameFrom(ctx, resourceID, ins)
+
+	return pr, clues.Wrap(err, "identifying resource owner").OrNil()
 }
