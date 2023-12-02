4ab4d53325e33ce98b1240cd8f0f5bc2c8139e06
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-12-02 12:43:38 +0530

    add lists and associated relationships retrieval from M365 to /api/lists

--- src/pkg/services/m365/api/lists_test.go
+++ src/pkg/services/m365/api/lists_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/alcionai/clues"
+	"github.com/h2non/gock"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -13,6 +15,7 @@ import (
 	"github.com/alcionai/corso/src/internal/tester/tconfig"
 	"github.com/alcionai/corso/src/pkg/control/testdata"
 	"github.com/alcionai/corso/src/pkg/services/m365/api/graph"
+	graphTD "github.com/alcionai/corso/src/pkg/services/m365/api/graph/testdata"
 )
 
 type ListsAPIIntgSuite struct {
@@ -54,4 +57,185 @@ func (suite *ListsAPIIntgSuite) TestLists_PostDrive() {
 	// second post, same name, should error on name conflict]
 	_, err = acl.PostDrive(ctx, siteID, driveName)
 	require.ErrorIs(t, err, graph.ErrItemAlreadyExistsConflict, clues.ToCore(err))
+}
+
+func (suite *ListsAPIIntgSuite) TestLists_GetListByID() {
+	var (
+		listID            = "fake-list-id"
+		siteID            = suite.its.site.id
+		textColumnDefID   = "fake-text-column-id"
+		textColumnDefName = "itemName"
+		numColumnDefID    = "fake-num-column-id"
+		numColumnDefName  = "itemSize"
+		colLinkID         = "fake-collink-id"
+		cTypeID           = "fake-ctype-id"
+		listItemID        = "fake-list-item-id"
+	)
+
+	tests := []struct {
+		name   string
+		setupf func()
+		expect assert.ErrorAssertionFunc
+	}{
+		{
+			name: "",
+			setupf: func() {
+				list := models.NewList()
+				list.SetId(&listID)
+
+				txtColumnDef := models.NewColumnDefinition()
+				txtColumnDef.SetId(&textColumnDefID)
+				txtColumnDef.SetName(&textColumnDefName)
+				textColumn := models.NewTextColumn()
+				txtColumnDef.SetText(textColumn)
+				columnDefCol := models.NewColumnDefinitionCollectionResponse()
+				columnDefCol.SetValue([]models.ColumnDefinitionable{txtColumnDef})
+
+				numColumnDef := models.NewColumnDefinition()
+				numColumnDef.SetId(&numColumnDefID)
+				numColumnDef.SetName(&numColumnDefName)
+				numColumn := models.NewNumberColumn()
+				numColumnDef.SetNumber(numColumn)
+				columnDefCol2 := models.NewColumnDefinitionCollectionResponse()
+				columnDefCol2.SetValue([]models.ColumnDefinitionable{numColumnDef})
+
+				colLink := models.NewColumnLink()
+				colLink.SetId(&colLinkID)
+				colLinkCol := models.NewColumnLinkCollectionResponse()
+				colLinkCol.SetValue([]models.ColumnLinkable{colLink})
+
+				cTypes := models.NewContentType()
+				cTypes.SetId(&cTypeID)
+				cTypesCol := models.NewContentTypeCollectionResponse()
+				cTypesCol.SetValue([]models.ContentTypeable{cTypes})
+
+				listItem := models.NewListItem()
+				listItem.SetId(&listItemID)
+				listItemCol := models.NewListItemCollectionResponse()
+				listItemCol.SetValue([]models.ListItemable{listItem})
+
+				fields := models.NewFieldValueSet()
+				fieldsData := map[string]any{
+					"itemName": "item1",
+					"itemSize": 4,
+				}
+				fields.SetAdditionalData(fieldsData)
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID).
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), list))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"columns").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), columnDefCol))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"items").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), listItemCol))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"items",
+					listItemID,
+					"fields").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), fields))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"contentTypes").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), cTypesCol))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"contentTypes",
+					cTypeID,
+					"columns").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), columnDefCol2))
+
+				interceptV1Path(
+					"sites",
+					siteID,
+					"lists",
+					listID,
+					"contentTypes",
+					cTypeID,
+					"columnLinks").
+					Reply(200).
+					JSON(graphTD.ParseableToMap(suite.T(), colLinkCol))
+			},
+			expect: assert.NoError,
+		},
+	}
+
+	for _, test := range tests {
+		suite.Run(test.name, func() {
+			t := suite.T()
+
+			ctx, flush := tester.NewContext(t)
+			defer flush()
+
+			defer gock.Off()
+			test.setupf()
+
+			list, err := suite.its.gockAC.Lists().GetListByID(ctx, siteID, listID)
+			test.expect(t, err)
+			assert.Equal(t, listID, *list.GetId())
+
+			items := list.GetItems()
+			assert.NotEmpty(t, items)
+			assert.Equal(t, 1, len(items))
+			assert.Equal(t, listItemID, *items[0].GetId())
+
+			expectedItemData := map[string]any{"itemName": ptr.To[string]("item1"), "itemSize": ptr.To[float64](float64(4))}
+			itemFields := items[0].GetFields()
+			itemData := itemFields.GetAdditionalData()
+			assert.Equal(t, expectedItemData, itemData)
+
+			columns := list.GetColumns()
+			assert.NotEmpty(t, columns)
+			assert.Equal(t, 1, len(columns))
+			assert.Equal(t, textColumnDefID, *columns[0].GetId())
+
+			cTypes := list.GetContentTypes()
+			assert.NotEmpty(t, cTypes)
+			assert.Equal(t, 1, len(cTypes))
+			assert.Equal(t, cTypeID, *cTypes[0].GetId())
+
+			colLinks := cTypes[0].GetColumnLinks()
+			assert.NotEmpty(t, colLinks)
+			assert.Equal(t, 1, len(colLinks))
+			assert.Equal(t, colLinkID, *colLinks[0].GetId())
+
+			columns = cTypes[0].GetColumns()
+			assert.NotEmpty(t, columns)
+			assert.Equal(t, 1, len(columns))
+			assert.Equal(t, numColumnDefID, *columns[0].GetId())
+		})
+	}
 }
