94b7672a57035cd8bbd8c867e4fbd8749d239386
ryanfkeepers <ryanfkeepers@gmail.com>
2023-12-06 20:05:54 -0700

    fix bad rebase

--- src/internal/m365/collection/drive/collections_tree.go
+++ src/internal/m365/collection/drive/collections_tree.go
@@ -323,8 +323,6 @@ func (c *Collections) populateTree(
 				pageCount = 0
 				pageItemCount = 0
 				countDeltas = 0
-			} else {
-				counter.Inc(count.TotalPagesEnumerated)
 			}
 
 			err = c.enumeratePageOfItems(
