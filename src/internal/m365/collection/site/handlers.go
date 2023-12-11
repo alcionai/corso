302dd23ad9d71bd449b5902c6ec15f2f180b412c
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-12-11 20:01:18 +0530

    declare lists restore handlers

--- src/internal/m365/collection/site/handlers.go
+++ src/internal/m365/collection/site/handlers.go
@@ -13,3 +13,24 @@ type backupHandler interface {
 type getItemByIDer interface {
 	GetItemByID(ctx context.Context, itemID string) (models.Listable, error)
 }
+
+type restoreHandler interface {
+	PostLister
+	PostListItemer
+}
+
+type PostLister interface {
+	PostList(
+		ctx context.Context,
+		listName string,
+		oldListByteArray []byte,
+	) (models.Listable, error)
+}
+
+type PostListItemer interface {
+	PostListItem(
+		ctx context.Context,
+		listID string,
+		oldListByteArray []byte,
+	) ([]models.ListItemable, error)
+}
--- src/internal/m365/collection/site/lists_handler.go
+++ src/internal/m365/collection/site/lists_handler.go
@@ -25,3 +25,33 @@ func NewListsBackupHandler(protectedResource string, ac api.Lists) listsBackupHa
 func (bh listsBackupHandler) GetItemByID(ctx context.Context, itemID string) (models.Listable, error) {
 	return bh.ac.GetListByID(ctx, bh.protectedResource, itemID)
 }
+
+var _ restoreHandler = &listsRestoreHandler{}
+
+type listsRestoreHandler struct {
+	ac                api.Lists
+	protectedResource string
+}
+
+func NewListsRestoreHandler(protectedResource string, ac api.Lists) listsRestoreHandler {
+	return listsRestoreHandler{
+		ac:                ac,
+		protectedResource: protectedResource,
+	}
+}
+
+func (rh listsRestoreHandler) PostList(
+	ctx context.Context,
+	listName string,
+	oldListByteArray []byte,
+) (models.Listable, error) {
+	return rh.ac.PostList(ctx, rh.protectedResource, listName, oldListByteArray)
+}
+
+func (rh listsRestoreHandler) PostListItem(
+	ctx context.Context,
+	listID string,
+	oldListByteArray []byte,
+) ([]models.ListItemable, error) {
+	return rh.ac.PostListItem(ctx, rh.protectedResource, listID, oldListByteArray)
+}
