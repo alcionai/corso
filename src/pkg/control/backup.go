e57d70c4ca5af47112db0f2c164a77ea6eadf384
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-12-14 08:43:24 -0800

    Create separate config structs for backup
    To avoid passing in backup-specific config options during repo connect,
    create a new set of config structs that contain options only used during
    backup. The API for creating a backup op can then be expanded to take
    this config struct.

--- src/pkg/control/backup.go
+++ src/pkg/control/backup.go
@@ -1,22 +1,34 @@
 package control
 
 import (
-	"github.com/alcionai/corso/src/pkg/control/repository"
 	"github.com/alcionai/corso/src/pkg/extensions"
 )
 
-// Options holds the optional configurations for a process
-type Options struct {
+// DefaultBackupOptions provides a Backup with the default values set.
+func DefaultBackupOptions() Backup {
+	return Backup{
+		FailureHandling: FailAfterRecovery,
+		DeltaPageSize:   500,
+		Parallelism: Parallelism{
+			CollectionBuffer: 4,
+			ItemFetch:        4,
+		},
+	}
+}
+
+// Backup is the set of options used for backup operations. Each set of options
+// is only applied to the backup operation it's passed to. To use the same set
+// of options for multiple backup operations pass the struct to all operations.
+type Backup struct {
 	// DeltaPageSize controls the quantity of items fetched in each page
 	// during multi-page queries, such as graph api delta endpoints.
 	DeltaPageSize        int32                              `json:"deltaPageSize"`
-	DisableMetrics       bool                               `json:"disableMetrics"`
 	FailureHandling      FailurePolicy                      `json:"failureHandling"`
 	ItemExtensionFactory []extensions.CreateItemExtensioner `json:"-"`
 	Parallelism          Parallelism                        `json:"parallelism"`
-	Repo                 repository.Options                 `json:"repo"`
-	SkipReduce           bool                               `json:"skipReduce"`
-	ToggleFeatures       Toggles                            `json:"toggleFeatures"`
+	ToggleFeatures       BackupToggles                      `json:"toggleFeatures"`
+	ServiceRateLimiter   RateLimiter                        `json:"serviceRateLimiter"`
+
 	// PreviewItemLimits defines the number of items and/or amount of data to
 	// fetch on a best-effort basis. Right now it's used for preview backups.
 	//
@@ -29,54 +41,40 @@ type Options struct {
 	PreviewLimits PreviewItemLimits `json:"previewItemLimits"`
 }
 
-// RateLimiter is the set of options applied to any external service facing rate
-// limiters Corso may use during backups or restores.
-type RateLimiter struct {
-	DisableSlidingWindowLimiter bool `json:"disableSlidingWindowLimiter"`
+type Parallelism struct {
+	// CollectionBuffer sets the number of items in a collection to buffer before
+	// blocking.
+	CollectionBuffer int
+
+	// ItemFetch sets the number of items to fetch in parallel when populating
+	// items within a collection.
+	ItemFetch int
 }
 
-type FailurePolicy string
-
-const (
-	// fails and exits the run immediately
-	FailFast FailurePolicy = "fail-fast"
-	// recovers whenever possible, reports non-zero recoveries as a failure
-	FailAfterRecovery FailurePolicy = "fail-after-recovery"
-	// recovers whenever possible, does not report recovery as failure
-	BestEffort FailurePolicy = "best-effort"
-)
-
-// DefaultOptions provides an Options with the default values set.
-func DefaultOptions() Options {
-	return Options{
-		FailureHandling: FailAfterRecovery,
-		DeltaPageSize:   500,
-		ToggleFeatures:  Toggles{},
-		Parallelism: Parallelism{
-			CollectionBuffer: 4,
-			ItemFetch:        4,
-		},
-	}
+// PreviewItemLimits describes best-effort maximum values to attempt to reach in
+// this backup. Preview backups are used to demonstrate value by being quick to
+// create.
+type PreviewItemLimits struct {
+	MaxItems             int
+	MaxItemsPerContainer int
+	MaxContainers        int
+	MaxBytes             int64
+	MaxPages             int
+	Enabled              bool
 }
 
-// ---------------------------------------------------------------------------
-// Feature Flags and Toggles
-// ---------------------------------------------------------------------------
-
-// Toggles allows callers to force corso to behave in ways that deviate from
-// the default expectations by turning on or shutting off certain features.
-// The default state for every toggle is false; toggles are only turned on
-// if specified by the caller.
-type Toggles struct {
+type BackupToggles struct {
 	// DisableIncrementals prevents backups from using incremental lookups,
 	// forcing a new, complete backup of all data regardless of prior state.
 	DisableIncrementals bool `json:"exchangeIncrementals,omitempty"`
+
 	// ForceItemDataDownload disables finding cached items in previous failed
 	// backups (i.e. kopia-assisted incrementals). Data dedupe will still occur
 	// since that is based on content hashes. Items that have not changed since
 	// the previous backup (i.e. in the merge base) will not be redownloaded. Use
 	// DisableIncrementals to control that behavior.
 	ForceItemDataDownload bool `json:"forceItemDataDownload,omitempty"`
+
 	// DisableDelta prevents backups from using delta based lookups,
 	// forcing a backup by enumerating all items. This is different
 	// from DisableIncrementals in that this does not even makes use of
@@ -85,18 +83,13 @@ type Toggles struct {
 	// user as Microsoft prevents the API from being able to make calls
 	// to delta endpoints.
 	DisableDelta bool `json:"exchangeDelta,omitempty"`
+
 	// ExchangeImmutableIDs denotes whether Corso should store items with
 	// immutable Exchange IDs. This is only safe to set if the previous backup for
 	// incremental backups used immutable IDs or if a full backup is being done.
 	ExchangeImmutableIDs bool `json:"exchangeImmutableIDs,omitempty"`
 
 	RunMigrations bool `json:"runMigrations"`
-
-	// DisableSlidingWindowLimiter disables the experimental sliding window rate
-	// limiter for graph API requests. This is only relevant for exchange backups.
-	// Setting this flag switches exchange backups to fallback to the default token
-	// bucket rate limiter.
-	DisableSlidingWindowLimiter bool `json:"disableSlidingWindowLimiter"`
 
 	// see: https://github.com/alcionai/corso/issues/4688
 	UseDeltaTree bool `json:"useDeltaTree"`
