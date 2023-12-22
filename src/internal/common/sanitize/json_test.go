1d2e242c8d9181fa9169478039c955064b2e190c
Ashlie Martinez <ashmrtnz@alcion.ai>
2023-12-22 16:32:14 -0700

    Fix tests for current implementation

--- src/internal/common/sanitize/json_test.go
+++ src/internal/common/sanitize/json_test.go
@@ -33,10 +33,19 @@ func generateCharacterTests() []jsonTest {
 
 		baseTestName = "Escape0x%02X"
 		baseTestData = `{"foo":"ba%sr"}`
-		expect       = `{"foo":"ba%s\u00%02Xr"}`
+		// The current implementation tranforms most characters to the encoding
+		// \u0000 where the 4 0's are hex digits. This is according to the JSON spec
+		// in RFC 8259 section 7.
+		expect = `{"foo":"ba%s\u00%02Xr"}`
 	)
 
 	for i := 0; i < 0x20; i++ {
+		// Whitespace characters are tested with manually written tests because they
+		// have different handling.
+		if i == '\n' || i == '\t' || i == '\r' {
+			continue
+		}
+
 		res = append(
 			res,
 			jsonTest{
@@ -57,12 +66,54 @@ func generateCharacterTests() []jsonTest {
 	return res
 }
 
-func (suite *SanitizeJSONUnitSuite) TestJSONString() {
+func (suite *SanitizeJSONUnitSuite) TestJSONBytes() {
 	table := []jsonTest{
 		{
 			name:        "AlreadyValid NoSpecialCharacters",
 			input:       []byte(`{"foo":"bar"}`),
 			expect:      []byte(`{"foo":"bar"}`),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid EscapedTab",
+			input:       []byte("{\"foo\":\"ba\\tr\\\"\"}"),
+			expect:      []byte("{\"foo\":\"ba\\tr\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid TabOutsideString",
+			input:       []byte("{\"foo\":\t\"bar\\\"\"}"),
+			expect:      []byte("{\"foo\":\t\"bar\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid EscapedLinefeed",
+			input:       []byte("{\"foo\":\"ba\\nr\\\"\"}"),
+			expect:      []byte("{\"foo\":\"ba\\nr\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid LinefeedOutsideString",
+			input:       []byte("{\"foo\":\n\"bar\\\"\"}"),
+			expect:      []byte("{\"foo\":\n\"bar\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid EscapedCarriageReturn",
+			input:       []byte("{\"foo\":\"ba\\rr\\\"\"}"),
+			expect:      []byte("{\"foo\":\"ba\\rr\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid CarriageReturnOutsideString",
+			input:       []byte("{\"foo\":\r\"bar\\\"\"}"),
+			expect:      []byte("{\"foo\":\r\"bar\\\"\"}"),
+			expectValid: assert.True,
+		},
+		{
+			name:        "AlreadyValid UnicodeSequence",
+			input:       []byte(`{"foo":"ba\u0008r\""}`),
+			expect:      []byte(`{"foo":"ba\u0008r\""}`),
 			expectValid: assert.True,
 		},
 		{
