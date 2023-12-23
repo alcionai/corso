30650e04e67d3a8ddf8e8ab08bbd0e53b9d55ac4
Abhishek Pandey <abhishek@alcion.ai>
2023-12-22 17:07:39 -0800

    Disable export and restore for sharepoint lists

--- src/cli/export/sharepoint.go
+++ src/cli/export/sharepoint.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/alcionai/corso/src/cli/flags"
 	"github.com/alcionai/corso/src/cli/utils"
+	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
 // called by export.go to map subcommands to provider-specific handling.
@@ -93,6 +94,9 @@ func exportSharePointCmd(cmd *cobra.Command, args []string) error {
 
 	sel := utils.IncludeSharePointRestoreDataSelectors(ctx, opts)
 	utils.FilterSharePointRestoreInfoSelectors(sel, opts)
+
+	// Exclude lists from exports since they are not supported yet.
+	sel.Exclude(sel.Lists(selectors.Any()))
 
 	return runExport(
 		ctx,
