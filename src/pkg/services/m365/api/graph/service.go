bf11b048518d71fd3355bac9f94ed21bbb1f22d1
ryanfkeepers <ryanfkeepers@gmail.com>
2024-01-09 12:08:03 -0700

    minor handling cleanup

--- src/pkg/services/m365/api/graph/service.go
+++ src/pkg/services/m365/api/graph/service.go
@@ -13,13 +13,11 @@ import (
 	khttp "github.com/microsoft/kiota-http-go"
 	msgraphsdkgo "github.com/microsoftgraph/msgraph-sdk-go"
 	msgraphgocore "github.com/microsoftgraph/msgraph-sdk-go-core"
-	"github.com/pkg/errors"
 
 	"github.com/alcionai/corso/src/internal/common/crash"
 	"github.com/alcionai/corso/src/internal/common/idname"
 	"github.com/alcionai/corso/src/internal/events"
 	"github.com/alcionai/corso/src/pkg/count"
-	"github.com/alcionai/corso/src/pkg/errs/core"
 	"github.com/alcionai/corso/src/pkg/filters"
 	"github.com/alcionai/corso/src/pkg/logger"
 	"github.com/alcionai/corso/src/pkg/path"
@@ -394,12 +392,6 @@ func (aw *adapterWrap) Send(
 		}
 
 		err = stackWithCoreErr(ictx, err, 1)
-
-		// force an early exit on throttling issues.
-		// those retries are already handled in middleware.
-		if errors.Is(err, core.ErrApplicationThrottled) {
-			return nil, err
-		}
 
 		// exit most errors without retry
 		switch {
