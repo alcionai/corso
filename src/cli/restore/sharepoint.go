3d52ef7104a211794eac74a539cfdeacbbbcec36
hiteshrepo <pattanayak.hitesh03@gmail.com>
2024-01-17 14:51:38 +0530

    enables sharepoint lists backup, restore and export

--- src/cli/restore/sharepoint.go
+++ src/cli/restore/sharepoint.go
@@ -7,7 +7,6 @@ import (
 	"github.com/alcionai/corso/src/cli/flags"
 	"github.com/alcionai/corso/src/cli/utils"
 	"github.com/alcionai/corso/src/pkg/dttm"
-	"github.com/alcionai/corso/src/pkg/selectors"
 )
 
 // called by restore.go to map subcommands to provider-specific handling.
@@ -94,9 +93,6 @@ func restoreSharePointCmd(cmd *cobra.Command, args []string) error {
 
 	sel := utils.IncludeSharePointRestoreDataSelectors(ctx, opts)
 	utils.FilterSharePointRestoreInfoSelectors(sel, opts)
-
-	// Exclude lists from restore since they are not supported yet.
-	sel.Exclude(sel.Lists(selectors.Any()))
 
 	return runRestore(
 		ctx,
