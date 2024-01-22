deead262aecc2500a4ca579d30a038992a6fa234
Abin Simon <abinsimon10@gmail.com>
2024-01-23 03:41:39 +0530

    Allow for timezone from TZ database (#5078)
    <!-- PR description-->
    
    Graph sometimes just send timezone value which are available in TZ
    database. This PR makes sure that we don't always try to convert, but
    check if the timezone is already in the format that we need first.
    
    ---
    
    #### Does this PR need a docs update or release note?
    
    - [ ] :white_check_mark: Yes, it's included
    - [ ] :clock1: Yes, but in a later PR
    - [x] :no_entry: No
    
    #### Type of change
    
    <!--- Please check the type of change your PR introduces: --->
    - [ ] :sunflower: Feature
    - [x] :bug: Bugfix
    - [ ] :world_map: Documentation
    - [ ] :robot: Supportability/Tests
    - [ ] :computer: CI/Deployment
    - [ ] :broom: Tech Debt/Cleanup
    
    #### Issue(s)
    
    <!-- Can reference multiple issues. Use one of the following "magic
    words" - "closes, fixes" to auto-close the Github issue. -->
    * #<issue>
    
    #### Test Plan
    
    <!-- How will this be tested prior to merging.-->
    - [ ] :muscle: Manual
    - [x] :zap: Unit test
    - [ ] :green_heart: E2E
    
    ---------
    
    Co-authored-by: zackrossman <zrossman@alcion.ai>

--- src/internal/converters/ics/ics_test.go
+++ src/internal/converters/ics/ics_test.go
@@ -139,6 +139,13 @@ func (suite *ICSUnitSuite) TestGetUTCTime() {
 			errCheck:  require.NoError,
 		},
 		{
+			name:      "timezone from TZ database",
+			timestamp: "2021-01-01T12:00:00Z",
+			timezone:  "America/Los_Angeles",
+			time:      time.Date(2021, 1, 1, 20, 0, 0, 0, time.UTC),
+			errCheck:  require.NoError,
+		},
+		{
 			name:      "invalid time",
 			timestamp: "invalid",
 			timezone:  "UTC",
