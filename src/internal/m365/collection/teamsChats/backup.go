2eecc908c5ff044e2fe3452d1fcaf228b0be2947
ryanfkeepers <ryanfkeepers@gmail.com>
2024-01-22 16:46:53 -0700

    boilerplate teamschat collection package
    seems like a lot of code, but this is 95% boilerplate additions copied
    from other similar collections packages.  Some noteworthy differences:
    * teamsChats does not handle metadata, so all metadata, delta, and previous
    path handling was removed
    * teamsChats does not produce tombstones
    * chats are never deleted, so no "removed" items are tracked
    * all chats gets stored at the prefix root, so no "containers" are iterated, and therefore only one collection is ever produced.
    
    This means that, overall, the boilerplate here is much reduced compared to
    boilerplate in similar packages.

---
+++ src/internal/m365/collection/teamsChats/backup.go
@@ -0,0 +1,166 @@
+package teamschats
+
+import (
+	"context"
+
+	"github.com/alcionai/clues"
+
+	"github.com/alcionai/corso/src/internal/data"
+	"github.com/alcionai/corso/src/internal/m365/support"
+	"github.com/alcionai/corso/src/internal/operations/inject"
+	"github.com/alcionai/corso/src/pkg/backup/metadata"
+	"github.com/alcionai/corso/src/pkg/control"
+	"github.com/alcionai/corso/src/pkg/count"
+	"github.com/alcionai/corso/src/pkg/fault"
+	"github.com/alcionai/corso/src/pkg/path"
+	"github.com/alcionai/corso/src/pkg/selectors"
+	"github.com/alcionai/corso/src/pkg/services/m365/api"
+	"github.com/alcionai/corso/src/pkg/services/m365/api/graph"
+)
+
+func CreateCollections[I chatsItemer](
+	ctx context.Context,
+	bpc inject.BackupProducerConfig,
+	bh backupHandler[I],
+	tenantID string,
+	scope selectors.TeamsChatsScope,
+	statusUpdater support.StatusUpdater,
+	useLazyReader bool,
+	counter *count.Bus,
+	errs *fault.Bus,
+) ([]data.BackupCollection, bool, error) {
+	var (
+		category = scope.Category().PathType()
+		qp       = graph.QueryParams{
+			Category:          category,
+			ProtectedResource: bpc.ProtectedResource,
+			TenantID:          tenantID,
+		}
+	)
+
+	cc := api.CallConfig{
+		CanMakeDeltaQueries: false,
+	}
+
+	container, err := bh.getContainer(ctx, cc)
+	if err != nil {
+		return nil, false, clues.Stack(err)
+	}
+
+	counter.Add(count.Containers, 1)
+
+	collection, err := populateCollection[I](
+		ctx,
+		qp,
+		bh,
+		statusUpdater,
+		container,
+		scope,
+		useLazyReader,
+		bpc.Options,
+		counter,
+		errs)
+	if err != nil {
+		return nil, false, clues.Wrap(err, "filling collections")
+	}
+
+	collections := []data.BackupCollection{collection}
+
+	metadataPrefix, err := path.BuildMetadata(
+		qp.TenantID,
+		qp.ProtectedResource.ID(),
+		path.TeamsChatsService,
+		qp.Category,
+		false)
+	if err != nil {
+		return nil, false, clues.WrapWC(ctx, err, "making metadata path prefix").
+			Label(count.BadPathPrefix)
+	}
+
+	metadataCollection, err := graph.MakeMetadataCollection(
+		metadataPrefix,
+		// no deltas or previousPaths are used here; we store empty files instead
+		[]graph.MetadataCollectionEntry{
+			graph.NewMetadataEntry(metadata.PreviousPathFileName, map[string]string{}),
+			graph.NewMetadataEntry(metadata.DeltaURLsFileName, map[string]string{}),
+		},
+		statusUpdater,
+		counter.Local())
+	if err != nil {
+		return nil, false, clues.WrapWC(ctx, err, "making metadata collection")
+	}
+
+	collections = append(collections, metadataCollection)
+
+	// no deltas involved in this category, so canUsePrevBackups is always true.
+	return collections, true, nil
+}
+
+func populateCollection[I chatsItemer](
+	ctx context.Context,
+	qp graph.QueryParams,
+	bh backupHandler[I],
+	statusUpdater support.StatusUpdater,
+	container container[I],
+	scope selectors.TeamsChatsScope,
+	useLazyReader bool,
+	ctrlOpts control.Options,
+	counter *count.Bus,
+	errs *fault.Bus,
+) (data.BackupCollection, error) {
+	var (
+		cl         = counter.Local()
+		collection data.BackupCollection
+		err        error
+	)
+
+	ctx = clues.AddLabelCounter(ctx, cl.PlainAdder())
+	cc := api.CallConfig{
+		CanMakeDeltaQueries: false,
+	}
+
+	items, err := bh.getItemIDs(ctx, cc)
+	if err != nil {
+		errs.AddRecoverable(ctx, clues.Stack(err))
+		return collection, clues.Stack(errs.Failure()).OrNil()
+	}
+
+	// Only create a collection if the path matches the scope.
+	includedItems := []I{}
+
+	for _, item := range items {
+		if !bh.includeItem(item, scope) {
+			cl.Inc(count.SkippedItems)
+		} else {
+			includedItems = append(includedItems, item)
+		}
+	}
+
+	cl.Add(count.ItemsAdded, int64(len(includedItems)))
+	cl.Add(count.ItemsRemoved, 0)
+
+	p, err := bh.canonicalPath()
+	if err != nil {
+		err = clues.StackWC(ctx, err).Label(count.BadCollPath)
+		errs.AddRecoverable(ctx, err)
+
+		return collection, clues.Stack(errs.Failure()).OrNil()
+	}
+
+	collection = NewCollection(
+		data.NewBaseCollection(
+			p,
+			p,
+			container.humanLocation.Builder(),
+			ctrlOpts,
+			false,
+			cl),
+		bh,
+		qp.ProtectedResource.ID(),
+		includedItems,
+		container,
+		statusUpdater,
+		useLazyReader)
+
+	return collection, clues.Stack(errs.Failure()).OrNil()
+}
---
+++ src/internal/m365/collection/teamsChats/chat_handler.go
@@ -0,0 +1,95 @@
+package teamschats
+
+import (
+	"context"
+
+	"github.com/alcionai/clues"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+
+	"github.com/alcionai/corso/src/internal/common/ptr"
+	"github.com/alcionai/corso/src/pkg/backup/details"
+	"github.com/alcionai/corso/src/pkg/path"
+	"github.com/alcionai/corso/src/pkg/selectors"
+	"github.com/alcionai/corso/src/pkg/services/m365/api"
+)
+
+var _ backupHandler[models.Chatable] = &usersChatsBackupHandler{}
+
+type usersChatsBackupHandler struct {
+	ac                  api.Chats
+	protectedResourceID string
+	tenantID            string
+}
+
+func NewUsersChatsBackupHandler(
+	tenantID, protectedResourceID string,
+	ac api.Chats,
+) usersChatsBackupHandler {
+	return usersChatsBackupHandler{
+		ac:                  ac,
+		protectedResourceID: protectedResourceID,
+		tenantID:            tenantID,
+	}
+}
+
+// chats have no containers.  Everything is stored at the root.
+//
+//lint:ignore U1000 required for interface compliance
+func (bh usersChatsBackupHandler) getContainer(
+	ctx context.Context,
+	_ api.CallConfig,
+) (container[models.Chatable], error) {
+	return chatContainer(), nil
+}
+
+//lint:ignore U1000 required for interface compliance
+func (bh usersChatsBackupHandler) getItemIDs(
+	ctx context.Context,
+	cc api.CallConfig,
+) ([]models.Chatable, error) {
+	return bh.ac.GetChats(
+		ctx,
+		bh.protectedResourceID,
+		cc)
+}
+
+//lint:ignore U1000 required for interface compliance
+func (bh usersChatsBackupHandler) includeItem(
+	ch models.Chatable,
+	scope selectors.TeamsChatsScope,
+) bool {
+	return scope.Matches(selectors.TeamsChatsChat, ptr.Val(ch.GetTopic()))
+}
+
+func (bh usersChatsBackupHandler) canonicalPath() (path.Path, error) {
+	return path.BuildPrefix(
+		bh.tenantID,
+		bh.protectedResourceID,
+		path.TeamsChatsService,
+		path.ChatsCategory)
+}
+
+//lint:ignore U1000 false linter issue due to generics
+func (bh usersChatsBackupHandler) getItem(
+	ctx context.Context,
+	userID string,
+	chatID string,
+) (models.Chatable, *details.TeamsChatsInfo, error) {
+	// FIXME: should retrieve and populate all messages in the chat.
+	return nil, nil, clues.New("not implemented")
+}
+
+//lint:ignore U1000 false linter issue due to generics
+func (bh usersChatsBackupHandler) augmentItemInfo(
+	dgi *details.TeamsChatsInfo,
+	c models.Chatable,
+) {
+	// no-op
+}
+
+func chatContainer() container[models.Chatable] {
+	return container[models.Chatable]{
+		storageDirFolders: path.Elements{},
+		humanLocation:     path.Elements{},
+	}
+}
