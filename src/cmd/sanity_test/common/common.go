07da7f16a127e260e67b9fbaf6c80050b45b1187
Hitesh Pattanayak <48874082+HiteshRepo@users.noreply.github.com>
2024-01-23 17:38:16 +0530

    adds local sanity tests for lists (#5031)
    adds local sanity tests for lists
    
    for reference:
    - `lists` do not have `folders` concept, they are directly created in a site. As a result cannot be restored to a different folder to test. Therefore they need to be created in adjacent to the 'backed-up' lists. Due to this there is a requirement of `SANITY_TEST_RESTORE_CONTAINER_PREFIX`. Using this, during restore check, this env-var is used to find restored lists.
    - Similarly, `SANITY_TEST_SOURCE_CONTAINER` is also used a prefix matcher to filter created test lists
    - When exported, lists are stored in local into the provided directory as `<SANITY_TEST_RESTORE_CONTAINER>/Lists/<list-id>/<list-id>.json`
    
    should go in after:
    - https://github.com/alcionai/corso/pull/5024
    - https://github.com/alcionai/corso/pull/5026
    
    #### Does this PR need a docs update or release note?
    - [x] :no_entry: No
    
    #### Type of change
    
    <!--- Please check the type of change your PR introduces: --->
    - [x] :robot: Supportability/Tests
    
    #### Issue(s)
    #4754
    
    #### Test Plan
    
    <!-- How will this be tested prior to merging.-->
    - [x] :muscle: Manual
    - [x] :zap: Unit test
    - [x] :green_heart: E2E

--- src/cmd/sanity_test/common/common.go
+++ src/cmd/sanity_test/common/common.go
@@ -19,33 +19,40 @@ type PermissionInfo struct {
 }
 
 const (
-	sanityBackupID             = "SANITY_BACKUP_ID"
-	sanityTestSourceContainer  = "SANITY_TEST_SOURCE_CONTAINER"
-	sanityTestRestoreContainer = "SANITY_TEST_RESTORE_CONTAINER"
-	sanityTestUser             = "SANITY_TEST_USER"
+	sanityBackupID                   = "SANITY_BACKUP_ID"
+	sanityTestSourceContainer        = "SANITY_TEST_SOURCE_CONTAINER"
+	sanityTestRestoreContainer       = "SANITY_TEST_RESTORE_CONTAINER"
+	sanityTestRestoreContainerPrefix = "SANITY_TEST_RESTORE_CONTAINER_PREFIX"
+	sanityTestUser                   = "SANITY_TEST_USER"
+	sanityTestCategory               = "SANITY_TEST_CATEGORY"
 )
 
 type Envs struct {
 	BackupID         string
 	SourceContainer  string
 	RestoreContainer string
-	GroupID          string
-	SiteID           string
-	UserID           string
-	TeamSiteID       string
+	// applies for sharepoint lists only
+	RestoreContainerPrefix string
+	Category               string
+	GroupID                string
+	SiteID                 string
+	UserID                 string
+	TeamSiteID             string
 }
 
 func EnvVars(ctx context.Context) Envs {
 	folder := strings.TrimSpace(os.Getenv(sanityTestRestoreContainer))
 
 	e := Envs{
-		BackupID:         os.Getenv(sanityBackupID),
-		SourceContainer:  os.Getenv(sanityTestSourceContainer),
-		RestoreContainer: folder,
-		GroupID:          tconfig.GetM365TeamID(ctx),
-		SiteID:           tconfig.GetM365SiteID(ctx),
-		UserID:           tconfig.GetM365UserID(ctx),
-		TeamSiteID:       tconfig.GetM365TeamSiteID(ctx),
+		BackupID:               os.Getenv(sanityBackupID),
+		SourceContainer:        os.Getenv(sanityTestSourceContainer),
+		RestoreContainer:       folder,
+		Category:               os.Getenv(sanityTestCategory),
+		RestoreContainerPrefix: os.Getenv(sanityTestRestoreContainerPrefix),
+		GroupID:                tconfig.GetM365TeamID(ctx),
+		SiteID:                 tconfig.GetM365SiteID(ctx),
+		UserID:                 tconfig.GetM365UserID(ctx),
+		TeamSiteID:             tconfig.GetM365TeamSiteID(ctx),
 	}
 
 	if len(os.Getenv(sanityTestUser)) > 0 {
