8a7cbbadcfb644f20e9e9033fee8e3ea75aaa9f1
Ashlie Martinez <ashmrtnz@alcion.ai>
2024-01-22 17:11:50 -0800

    Use unique data to source info for kopia snapshots
    Use unique info for the kopia snapshot source information to help
    isolate snapshots for different resources from each other. This will
    come in handy both for checkpoint snapshots and regular snapshots if
    kopia's retention policy happens to look for things.

--- src/internal/kopia/wrapper_test.go
+++ src/internal/kopia/wrapper_test.go
@@ -3,6 +3,7 @@ package kopia
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	stdpath "path"
 	"strings"
@@ -373,6 +374,113 @@ func (suite *BasicKopiaIntegrationSuite) TestUpdatePersistentConfig() {
 		t,
 		ptr.Val(config.MinEpochDuration),
 		mutableParams.EpochParameters.MinEpochDuration)
+}
+
+func (suite *BasicKopiaIntegrationSuite) TestConsumeBackupCollections_SetsSourceInfo() {
+	table := []struct {
+		name        string
+		reasons     []identity.Reasoner
+		expectError assert.ErrorAssertionFunc
+		expectUser  string
+		expectHost  string
+	}{
+		{
+			name: "DifferentDataTypesInService",
+			reasons: []identity.Reasoner{
+				identity.NewReason(testTenant, testUser, path.ExchangeService, path.EmailCategory),
+				identity.NewReason(testTenant, testUser, path.ExchangeService, path.ContactsCategory),
+			},
+			expectError: assert.NoError,
+			expectUser:  testTenant + "-" + testUser,
+			expectHost: path.ExchangeService.String() +
+				path.ContactsCategory.String() + "-" + path.ExchangeService.String() +
+				path.EmailCategory.String(),
+		},
+		{
+			name: "DifferentServices",
+			reasons: []identity.Reasoner{
+				identity.NewReason(testTenant, testUser, path.ExchangeService, path.EmailCategory),
+				identity.NewReason(testTenant, testUser, path.OneDriveService, path.FilesCategory),
+			},
+			expectError: assert.NoError,
+			expectUser:  testTenant + "-" + testUser,
+			expectHost: path.ExchangeService.String() +
+				path.EmailCategory.String() + "-" + path.OneDriveService.String() +
+				path.FilesCategory.String(),
+		},
+		{
+			name: "DifferentTenants Errors",
+			reasons: []identity.Reasoner{
+				identity.NewReason(testTenant+"1", testUser, path.ExchangeService, path.EmailCategory),
+				identity.NewReason(testTenant, testUser, path.ExchangeService, path.ContactsCategory),
+			},
+			expectError: assert.Error,
+		},
+		{
+			name: "DifferentResources Errors",
+			reasons: []identity.Reasoner{
+				identity.NewReason(testTenant, testUser+"1", path.ExchangeService, path.EmailCategory),
+				identity.NewReason(testTenant, testUser, path.ExchangeService, path.ContactsCategory),
+			},
+			expectError: assert.Error,
+		},
+	}
+
+	for _, test := range table {
+		suite.Run(test.name, func() {
+			t := suite.T()
+
+			ctx, flush := tester.NewContext(t)
+			defer flush()
+
+			var cols []data.BackupCollection
+
+			for i, reason := range test.reasons {
+				colPath, err := path.Build(
+					reason.Tenant(),
+					reason.ProtectedResource(),
+					reason.Service(),
+					reason.Category(),
+					false,
+					fmt.Sprintf("%d", i))
+				require.NoError(t, err, clues.ToCore(err))
+
+				cols = append(cols, exchMock.NewCollection(colPath, colPath, 0))
+			}
+
+			c, err := openLocalKopiaRepo(t, ctx)
+			require.NoError(t, err, clues.ToCore(err))
+
+			wrapper := &Wrapper{c}
+
+			defer wrapper.Close(ctx)
+
+			stats, _, _, err := wrapper.ConsumeBackupCollections(
+				ctx,
+				test.reasons,
+				nil,
+				cols,
+				nil,
+				nil,
+				true,
+				count.New(),
+				fault.New(true))
+			test.expectError(t, err, clues.ToCore(err))
+
+			if err != nil {
+				return
+			}
+
+			snap, err := snapshot.LoadSnapshot(
+				ctx,
+				wrapper.c,
+				manifest.ID(stats.SnapshotID))
+			require.NoError(t, err, clues.ToCore(err))
+
+			assert.Equal(t, test.expectHost, snap.Source.Host, "source host")
+			assert.Equal(t, test.expectUser, snap.Source.UserName, "source user")
+		})
+	}
 }
 
 // ---------------
