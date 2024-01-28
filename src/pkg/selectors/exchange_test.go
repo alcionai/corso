9665a2401cb2097ca18c8610137858efca1c5435
HiteshRepo <pattanayak.hitesh03@gmail.com>
2024-01-28 14:29:51 +0530

    check effect of failfast flag - 2nd run

--- src/pkg/selectors/exchange_test.go
+++ src/pkg/selectors/exchange_test.go
@@ -1652,11 +1652,6 @@ func (suite *ExchangeSelectorSuite) Test_MoreTest() {
 			path1: "/path/to/dir",
 			path2: "/path/to/dir",
 		},
-		{
-			name:  "same linux normal path",
-			path1: "/path/to/dir",
-			path2: "/path/to/dir",
-		},
 	}
 
 	for _, test := range tests {
