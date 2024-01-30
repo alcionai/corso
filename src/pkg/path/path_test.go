0144eebb55d774845c7cd376207919b255f7e806
HiteshRepo <pattanayak.hitesh03@gmail.com>
2024-01-30 17:54:04 +0530

    re run

--- src/pkg/path/path_test.go
+++ src/pkg/path/path_test.go
@@ -721,3 +721,23 @@ func (suite *PathUnitSuite) TestArePathsEquivalent() {
 		})
 	}
 }
+
+func (suite *PathUnitSuite) Test_Stuff() {
+	t := suite.T()
+
+	tests := []struct {
+		name  string
+		path1 string
+		path2 string
+	}{
+		{
+			name:  "same linux normal path",
+			path1: "/path/to/dir",
+			path2: "/path/to/dir",
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.path1, test.path2)
+	}
+}
